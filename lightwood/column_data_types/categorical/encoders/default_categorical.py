07e3e5847c53d15391ed75c573d94c7d3b212fe5
Jorge Torres <jorge.torres.maldonado@gmail.com>
2019-04-22 10:39:11 -0400

    add support for None

--- lightwood/column_data_types/categorical/encoders/default_categorical.py
+++ lightwood/column_data_types/categorical/encoders/default_categorical.py
@@ -17,15 +17,17 @@ class DefaultCategoricalEncoder:
             self._lang.index2word =  {UNCOMMON_TOKEN: UNCOMMON_WORD}
             self._lang.n_words = 1
             for word in column_data:
-                self._lang.addWord(word)
+                if word != None:
+                    self._lang.addWord(word)
 
         ret = []
         v_len = self._lang.n_words
 
         for word in column_data:
             encoded_word = [0]*v_len
-            index = self._lang.word2index[word] if word in self._lang.word2index else UNCOMMON_TOKEN
-            encoded_word[index] = 1
+            if word != None:
+                index = self._lang.word2index[word] if word in self._lang.word2index else UNCOMMON_TOKEN
+                encoded_word[index] = 1
 
             ret += [encoded_word]
 
@@ -38,24 +40,29 @@ class DefaultCategoricalEncoder:
 
         ret = []
 
+
         for vector in encoded_data_list:
+            found = False
             for i, val in enumerate(vector):
                 if int(val) == 1:
+                    found = True
                     ret += [self._lang.index2word[i]]
                     break
-
+            if not found:
+                ret += [None]
+                
         return ret
 
 
 if __name__ == "__main__":
 
-    data = 'once upon a time there where some tokens'.split(' ')
+    data = 'once upon a time there where some tokens'.split(' ') + [None]
 
     enc = DefaultCategoricalEncoder()
 
     print (enc.encode(data))
 
-    print(enc.decode(enc.encode(['not there', 'time', 'tokens'])))
+    print(enc.decode(enc.encode(['not there', 'time', 'tokens', None])))
 
 
 
