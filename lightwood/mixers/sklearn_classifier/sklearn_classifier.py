50d2c4e91360aa3d6af1a1fdd9bde6b650ef0a15
Jorge Torres <jorge.torres.maldonado@gmail.com>
2019-04-26 12:55:29 -0400

    classifier template and test code

--- lightwood/mixers/sklearn_classifier/sklearn_classifier.py
+++ lightwood/mixers/sklearn_classifier/sklearn_classifier.py
@@ -39,4 +39,57 @@ class SklearnClassifier:
 
 
 
+if __name__ == "__main__":
+    import random
+    import pandas
+    from lightwood.api.data_source import DataSource
+
+
+    ###############
+    # GENERATE DATA
+    ###############
+
+    config = {
+        'name': 'test',
+        'input_features': [
+            {
+                'name': 'x',
+                'type': 'numeric',
+                'encoder_path': 'lightwood.encoders.numeric.numeric'
+            },
+            {
+                'name': 'y',
+                'type': 'numeric',
+                #'encoder_path': 'lightwood.encoders.numeric.numeric'
+            }
+        ],
+
+        'output_features': [
+            {
+                'name': 'z',
+                'type': 'categorical',
+                #'encoder_path': 'lightwood.encoders.categorical.categorical'
+            }
+        ]
+    }
+
+    data = {'x':[i for i in range(10)], 'y':[random.randint(i,i+20) for i in range(10)]}
+    nums = [data['x'][i] * data['y'][i] for i in range(10)]
+
+    data['z'] = ['low' if i< 50 else 'high' for i in nums]
+
+    data_frame = pandas.DataFrame(data)
+
+    print(data_frame)
+
+    ds = DataSource(data_frame, config)
+    ####################
+
+
+
+    mixer = SklearnClassifier(input_column_names = ['x', 'y'], output_column_names=['y'])
+
+    mixer.fit(data)
+
+    
 
