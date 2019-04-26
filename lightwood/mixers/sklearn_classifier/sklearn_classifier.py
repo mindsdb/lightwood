cf7db00968396f24dacfd22512daed335814b9b9
Jorge Torres <jorge.torres.maldonado@gmail.com>
2019-04-26 12:40:37 -0400

    defaults and mixer

--- lightwood/mixers/sklearn_classifier/sklearn_classifier.py
+++ lightwood/mixers/sklearn_classifier/sklearn_classifier.py
@@ -1,7 +1,42 @@
 
 class SklearnClassifier:
 
-    def __init__(self, ):
+    def __init__(self, input_column_names, output_column_names ):
+        """
+        :param input_column_names: is a list [col_name1, col_name2]
+        :param output_column_names: is a list [col_name1, col_name2]
+        """
+
+
+        self.input_column_names = input_column_names
+        self.output_column_names = output_column_names
+
+        self.model = None
+
         pass
+
+
+    def fit(self, data_source):
+        '''
+        :param data: is a DataSource object
+        :return:
+        '''
+
+        pass
+
+
+    def predict(self, when_data_source):
+        '''
+
+        :param when_data: is a DataSource object
+        :return:
+        '''
+
+        pass
+
+
+
+
+
 
 
