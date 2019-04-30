57e13ea130053edd00334cccb8a046bc03068885
Jorge Torres <jorge.torres.maldonado@gmail.com>
2019-04-30 13:32:08 -0400

    updating the config definition so we support a specific mixer if user wants to

--- lightwood/data_schemas/definition.py
+++ lightwood/data_schemas/definition.py
@@ -1,20 +1,27 @@
 from schema import Schema, And, Use, Optional
 from lightwood.constants.lightwood import COLUMN_DATA_TYPES, HISTOGRAM_TYPES
 
-feature_schema = Schema({
+input_feature_schema = Schema({
                 'name': str,
                 'type': And(str, Use(str.lower), lambda s: s in COLUMN_DATA_TYPES.get_attributes().values()),
                 Optional('encoder_path'): str,
                 Optional('encoder_args'): dict
             })
 
+output_feature_schema = Schema({
+                'name': str,
+                'type': And(str, Use(str.lower), lambda s: s in COLUMN_DATA_TYPES.get_attributes().values()),
+                Optional('mixer_path'): str,
+                Optional('mixer_args'): dict
+            })
+
 definition_schema = Schema({
         'name': str,
         'input_features': [
-            feature_schema
+            input_feature_schema
         ],
         'output_features': [
-            feature_schema
+            output_feature_schema
         ]
 
     })
