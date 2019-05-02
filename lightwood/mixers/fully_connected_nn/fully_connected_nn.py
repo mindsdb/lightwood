16b06e9829e54fbfc9e29360fed63bb6c909f42d
Jorge Torres <jorge.torres.maldonado@gmail.com>
2019-05-02 15:55:50 -0700

    fully connected template

--- lightwood/mixers/fully_connected_nn/fully_connected_nn.py
+++ lightwood/mixers/fully_connected_nn/fully_connected_nn.py
@@ -12,3 +12,105 @@ class FullyConnectedNnMixer:
     def predict(self, when_data_source):
 
         pass
+
+
+
+
+
+if __name__ == "__main__":
+    import random
+    import pandas
+    from lightwood.api.data_source import DataSource
+
+    ###############
+    # GENERATE DATA
+    ###############
+
+    config = {
+        'name': 'test',
+        'input_features': [
+            {
+                'name': 'x',
+                'type': 'numeric',
+                'encoder_path': 'lightwood.encoders.numeric.numeric'
+            },
+            {
+                'name': 'y',
+                'type': 'numeric',
+                # 'encoder_path': 'lightwood.encoders.numeric.numeric'
+            }
+        ],
+
+        'output_features': [
+            {
+                'name': 'z',
+                'type': 'categorical',
+                # 'encoder_path': 'lightwood.encoders.categorical.categorical'
+            }
+        ]
+    }
+
+    ##For Classification
+    data = {'x': [i for i in range(10)], 'y': [random.randint(i, i + 20) for i in range(10)]}
+    nums = [data['x'][i] * data['y'][i] for i in range(10)]
+
+    data['z'] = ['low' if i < 50 else 'high' for i in nums]
+
+    data_frame = pandas.DataFrame(data)
+
+    # print(data_frame)
+
+    ds = DataSource(data_frame, config)
+    predict_input_ds = DataSource(data_frame[['x', 'y']], config)
+    ####################
+
+    mixer = FullyConnectedNnMixer(input_column_names=['x', 'y'], output_column_names=['z'])
+
+    data_encoded = mixer.fit(ds)
+    predictions = mixer.predict(predict_input_ds)
+    print(predictions)
+
+    ##For Regression
+
+    # GENERATE DATA
+    ###############
+
+    config = {
+        'name': 'test',
+        'input_features': [
+            {
+                'name': 'x',
+                'type': 'numeric',
+                'encoder_path': 'lightwood.encoders.numeric.numeric'
+            },
+            {
+                'name': 'y',
+                'type': 'numeric',
+                # 'encoder_path': 'lightwood.encoders.numeric.numeric'
+            }
+        ],
+
+        'output_features': [
+            {
+                'name': 'z',
+                'type': 'numeric',
+                # 'encoder_path': 'lightwood.encoders.categorical.categorical'
+            }
+        ]
+    }
+
+    data = {'x': [i for i in range(10)], 'y': [random.randint(i, i + 20) for i in range(10)]}
+    nums = [data['x'][i] * data['y'][i] for i in range(10)]
+
+    data['z'] = [i + 0.5 for i in range(10)]
+
+    data_frame = pandas.DataFrame(data)
+    ds = DataSource(data_frame, config)
+    predict_input_ds = DataSource(data_frame[['x', 'y']], config)
+    ####################
+
+    mixer = FullyConnectedNnMixer(input_column_names=['x', 'y'], output_column_names=['z'])
+
+    data_encoded = mixer.fit(ds)
+    predictions = mixer.predict(predict_input_ds)
+    print(predictions)
