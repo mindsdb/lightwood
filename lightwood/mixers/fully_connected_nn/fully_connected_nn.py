3186e29e34d67497895308ead4a3fb4de318741b
Jorge Torres <jorge.torres.maldonado@gmail.com>
2019-05-02 15:34:31 -0700

    added a fully connected neural network template

---
+++ lightwood/mixers/fully_connected_nn/fully_connected_nn.py
@@ -0,0 +1,14 @@
+
+class FullyConnectedNnMixer:
+
+    def __init__(self):
+
+        pass
+
+    def fit(self, data_source):
+
+        pass
+
+    def predict(self, when_data_source):
+
+        pass
