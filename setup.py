a24a25f54bb8424432429dc1ee00d9b5a2a745f1
George3d6 <george@cerebralab.com>
2019-07-18 16:55:58 +0000

    trying some more stuff before giving up

--- setup.py
+++ setup.py
@@ -34,11 +34,15 @@ elif os == 'Darwin':
 elif os == 'Windows':
     requirements = remove_requirement(requirements,'torch')
     requirements = remove_requirement(requirements,'torchvision')
-    requirements.append('torch == 1.1.0')
-    requirements.append('torchvision == 0.3.0')
 
-    dependency_links.append('https://download.pytorch.org/whl/cu100/torch-1.1.0-cp37-cp37m-win_amd64.whl#egg=torch-1.1.0')
-    dependency_links.append('https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp37-cp37m-win_amd64.whl#egg=torchvision-0.3.0')
+    requirements.append('torch @ https://download.pytorch.org/whl/cu100/torch-1.1.0-cp37-cp37m-win_amd64.whl')
+    requirements.append('torchvision @ https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp37-cp37m-win_amd64.whl')
+
+    #requirements.append('torch == 1.1.0')
+    #requirements.append('torchvision == 0.3.0')
+
+    #dependency_links.append('https://download.pytorch.org/whl/cu100/torch-1.1.0-cp37-cp37m-win_amd64.whl#egg=torch-1.1.0')
+    #dependency_links.append('https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp37-cp37m-win_amd64.whl#egg=torchvision-0.3.0')
 
 # Docker and other unknown OS-es
 else:
