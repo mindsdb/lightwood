c71eae04a91d96c4cb2df2749157ae0d3dea81b2
George3d6 <george@cerebralab.com>
2019-07-23 18:43:58 +0000

    only building source for pypi for now

--- install.sh
+++ install.sh
@@ -14,8 +14,9 @@ if [ "$mode" = "prod" ]; then
     python3 setup.py build
     #python3 setup.py install
     #python3 setup.py bdist_egg -p win32
-    python3 setup.py sdist bdist_wheel
-
+    #python3 setup.py sdist bdist_wheel
+    python3 setup.py sdist
+    
     echo "Do you want to publish this version (yes/no)?"
 
     read publish
@@ -35,6 +36,3 @@ if [ "$mode" = "dev" ]; then
     python3 setup.py develop --uninstall
     python3 setup.py develop
 fi
-
-
-
