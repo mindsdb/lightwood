95fc35f3da2de280e76bf303a9825e2b0cd1216a
George3d6 <george@cerebralab.com>
2019-08-09 04:26:31 +0000

    dynamically setting gpu cstatus if not set

--- lightwood/api/predictor.py
+++ lightwood/api/predictor.py
@@ -10,7 +10,7 @@ import numpy as np
 
 from lightwood.api.data_source import DataSource
 from lightwood.data_schemas.predictor_config import predictor_config_schema
-
+from lightwood.config.config import CONFIG
 from lightwood.mixers.sk_learn.sk_learn import SkLearnMixer
 from lightwood.mixers.nn.nn import NnMixer
 from sklearn.metrics import accuracy_score
@@ -60,6 +60,8 @@ class Predictor:
         self.train_accuracy = None
 
     def learn(self, from_data, test_data=None, callback_on_iter = None, eval_every_x_epochs = 20, stop_training_after_seconds=3600 * 24 * 5):
+        print(CONFIG.USE_CUDA)
+        exit()
         """
         Train and save a model (you can use this to retrain model from data)
 
