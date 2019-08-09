3006a5f1685354c11193d1b6712af38a26f9c794
George3d6 <george@cerebralab.com>
2019-08-09 00:55:08 +0000

    made functions shorter

--- lightwood/mixers/nn/helpers/shapes.py
+++ lightwood/mixers/nn/helpers/shapes.py
@@ -23,15 +23,14 @@ def rectangle(in_size,out_size,depth):
 
 def rombus(in_size,out_size,depth,max_size=None):
     if max_size is None:
-        max_size = in_size*2
+        max_size = max(in_size,out_size)*2
     if depth < 3:
         print('Depth must be at least 3 for the rombus function to work correctly, setting it to 3')
         depth = 3
 
     funnel_size = math.ceil(depth/2)
 
-    first_funnel = funnel(max_size,in_size,funnel_size)
-    first_funnel.reverse()
+    first_funnel = funnel(in_size, max_size,funnel_size)
 
     if depth % 2 == 1:
         first_funnel = first_funnel[:-1]
