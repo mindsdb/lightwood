06017089b11eabbd05fbe7a1d0c3de312e4ac8be
George3d6 <george@cerebralab.com>
2019-08-12 21:16:08 +0000

    updated requirements

--- setup.py
+++ setup.py
@@ -33,12 +33,14 @@ dependency_links = []
 
 # Linux specific requirements
 if sys_platform == 'linux' or sys_platform.startswith('linux'):
-    requirements = remove_requirements(requirements,'torch',replace='torch >= 1.1.0')
+    pass
+    #requirements = remove_requirements(requirements,'torch',replace='torch >= 1.1.0')
 
 
 # OSX specific requirements
 elif sys_platform == 'darwin':
-    requirements = remove_requirements(requirements,'torch',replace='torch >= 1.1.0.post2')
+    pass
+    #requirements = remove_requirements(requirements,'torch',replace='torch >= 1.1.0.post2')
 
 # Windows specific requirements
 elif sys_platform in ['win32','cygwin','windows']:
