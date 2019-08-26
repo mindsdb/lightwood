c92792bea352cfb60655e26aab0d2c917bfeb437
George3d6 <george@cerebralab.com>
2019-08-26 04:58:49 +0000

    version update

--- lightwood/__about__.py
+++ lightwood/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'lightwood'
 __package_name__ = 'mindsdb'
-__version__ = '0.8.6'
+__version__ = '0.8.7'
 __description__ = "Lightwood's goal is to make it very simple for developers to use the power of artificial neural networks in their projects."
 __email__ = "jorge@mindsdb.com"
 __author__ = 'MindsDB Inc'
