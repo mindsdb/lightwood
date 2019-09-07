401faac21daaffb387cdaac5bf9adb8424878bb6
George3d6 <george@cerebralab.com>
2019-09-07 18:20:46 +0000

    finished implementation of Ranger optimizer

--- lightwood/model_building/basic_ax_optimizer/basic_ax_optimizer.py
+++ lightwood/model_building/basic_ax_optimizer/basic_ax_optimizer.py
@@ -6,6 +6,14 @@ class BasicAxOptimizer:
 
     def evaluate(self, error_yielding_function):
 
+
+        #parameter comments:
+        # beta1 (momentum) of .95 seems to work better than .90...
+        #N_sma_threshold of 5 seems better in testing than 4.
+        #In both cases, worth testing on your dataset (.90 vs .95, 4 vs 5) to make sure which works best for you.
+        # @TODO Implement the above testing with AX ^
+        # lr=1e-3
+
         best_parameters, values, experiment, model = ax.optimize(
             parameters=[
                 {'name': 'base_lr', 'type': 'range', 'bounds': [0.0003,0.003]}, # , 'log_scale': True ?
