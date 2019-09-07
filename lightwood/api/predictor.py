454232430df8864f5a96a6bedddb993c91b239f6
George3d6 <george@cerebralab.com>
2019-09-07 23:23:14 +0000

    removed unused print and parameters

--- lightwood/api/predictor.py
+++ lightwood/api/predictor.py
@@ -197,10 +197,9 @@ class Predictor:
             training_time_per_iteration = stop_model_building_after_seconds/optimizer.total_trials
 
             best_parameters = optimizer.evaluate(lambda dynamic_parameters: Predictor.evaluate_mixer(mixer_class, mixer_params, from_data_ds, test_data_ds, dynamic_parameters, is_categorical_output, max_training_time=training_time_per_iteration, max_epochs=None))
-            print(best_parameters)
         else:
             # Run a bunch of models through AX and figure out some decent values to put in here
-            best_parameters = {'base_lr': 0.002, 'max_lr': 0.01, 'weight_decay': 0.01, 'network_depth': 5, 'scheduler_mode': 'triangular'}
+            best_parameters = {}
 
         mixer = mixer_class(best_parameters, is_categorical_output=is_categorical_output)
         self._mixer = mixer
