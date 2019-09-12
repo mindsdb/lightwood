e1b545defff4d6acecd26e565b48e6083a10fa12
George3d6 <george@cerebralab.com>
2019-09-12 06:19:52 +0000

    adding models to cpu before saving and setting them to whatever when loading

--- lightwood/encoders/image/img_2_vec.py
+++ lightwood/encoders/image/img_2_vec.py
@@ -4,6 +4,7 @@ import requests
 from io import BytesIO
 
 from lightwood.encoders.image.helpers.img_to_vec import Img2Vec
+from lightwood.config.config import CONFIG
 
 
 class Img2VecEncoder:
@@ -23,13 +24,13 @@ class Img2VecEncoder:
         """
         if self._model is None:
             if self.aim == 'speed':
-                self._model = Img2Vec(model='resnet-18')
+                self._model = Img2Vec(model='resnet-18', cuda=CONFIG.USE_CUDA)
             elif self.aim == 'balance':
-                self._model = Img2Vec(model='resnext-50-small')#(model='resnet-18')
+                self._model = Img2Vec(model='resnext-50-small', cuda=CONFIG.USE_CUDA)
             elif self.aim == 'accuracy':
-                self._model = Img2Vec(model='resnext-50')
+                self._model = Img2Vec(model='resnext-50', cuda=CONFIG.USE_CUDA)
             else:
-                self._model = Img2Vec()
+                self._model = Img2Vec(cuda=CONFIG.USE_CUDA)
 
         pics = []
         for image in images:
