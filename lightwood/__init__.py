3222ddee852f5bc7597914aa397482864f203c91
George3d6 <george@cerebralab.com>
2019-10-19 18:41:26 +0000

    exposing version

--- lightwood/__init__.py
+++ lightwood/__init__.py
@@ -9,6 +9,7 @@ import lightwood.model_building
 from lightwood.api.predictor import Predictor
 from lightwood.mixers import BUILTIN_MIXERS
 from lightwood.encoders import BUILTIN_ENCODERS
+from lightwood.__about__ import __package_name__ as name, __version__
 
 
 COLUMN_DATA_TYPES = CONST.COLUMN_DATA_TYPES
