0f1b15bf3cec90149f7fce5e74f03509cdf7a3ab
George3d6 <george@cerebralab.com>
2019-10-27 19:41:10 +0000

    added tests and interface export for Bayesian NN Mixer

--- lightwood/mixers/bayesian_nn/bayesian_nn.py
+++ lightwood/mixers/bayesian_nn/bayesian_nn.py
@@ -23,8 +23,6 @@ class BayesianNnMixer:
         self.data_loader = None
         self.transformer = None
         self.encoders = None
-        self.optimizer_class = None
-        self.optimizer_args = None
         self.criterion = None
 
         self.batch_size = 200
@@ -222,18 +220,6 @@ class BayesianNnMixer:
             else:
                 self.criterion = torch.nn.MSELoss()
 
-        self.optimizer_class = Ranger
-        if self.optimizer_args is None:
-            self.optimizer_args = {}
-
-        if 'beta1' in self.dynamic_parameters:
-            self.optimizer_args['betas'] = (self.dynamic_parameters['beta1'],0.999)
-
-        for optimizer_arg_name in ['lr','k','N_sma_threshold']:
-            if optimizer_arg_name in self.dynamic_parameters:
-                self.optimizer_args[optimizer_arg_name] = self.dynamic_parameters[optimizer_arg_name]
-
-        #self.optimizer = self.optimizer_class(self.net.parameters(), **self.optimizer_args)
         self.optimizer = pyro.optim.Adam({"lr": 0.01})
         svi = pyro.infer.SVI(self.pyro_model, self.pyro_guide, self.optimizer, loss=pyro.infer.Trace_ELBO())
 
@@ -255,136 +241,5 @@ class BayesianNnMixer:
                 mi = inputs
                 running_loss += svi.step(mi[0], labels)
                 error = running_loss / (i + 1)
-                continue
-
-                # zero the parameter gradients
-                self.optimizer.zero_grad()
-
-                # forward + backward + optimize
-                outputs = self.net(inputs)
-
-                if self.is_categorical_output:
-                    target = labels.cpu().numpy()
-                    target_indexes = np.where(target>0)[1]
-                    targets_c = torch.LongTensor(target_indexes)
-                    labels = targets_c.to(self.net.device)
-
-                loss = self.criterion(outputs, labels)
-                loss.backward()
-
-                self.optimizer.step()
-                # Maybe make this a scheduler later
-                # Start flat and then go into cosine annealing
-
-                """
-                if total_iterations > 600 and epoch > 20:
-                    for group in self.optimizer.param_groups:
-                        if self.optimizer.initial_lr * 1/100 < group['lr']:
-                            group['lr'] = group['lr'] - self.optimizer.initial_lr * 1/400
-                """
-
-                running_loss += loss.item()
-                error = running_loss / (i + 1)
 
             yield error
-
-
-
-if __name__ == "__main__":
-    import random
-    import pandas
-    from lightwood.api.data_source import DataSource
-
-    ###############
-    # GENERATE DATA
-    ###############
-
-    config = {
-        'name': 'test',
-        'input_features': [
-            {
-                'name': 'x',
-                'type': 'numeric',
-                'encoder_path': 'lightwood.encoders.numeric.numeric'
-            },
-            {
-                'name': 'y',
-                'type': 'numeric',
-                # 'encoder_path': 'lightwood.encoders.numeric.numeric'
-            }
-        ],
-
-        'output_features': [
-            {
-                'name': 'z',
-                'type': 'categorical',
-                # 'encoder_path': 'lightwood.encoders.categorical.categorical'
-            }
-        ]
-    }
-
-    ##For Classification
-    data = {'x': [i for i in range(10)], 'y': [random.randint(i, i + 20) for i in range(10)]}
-    nums = [data['x'][i] * data['y'][i] for i in range(10)]
-
-    data['z'] = ['low' if i < 50 else 'high' for i in nums]
-
-    data_frame = pandas.DataFrame(data)
-
-    # print(data_frame)
-
-    ds = DataSource(data_frame, config)
-    predict_input_ds = DataSource(data_frame[['x', 'y']], config)
-    ####################
-
-    mixer = BayesianNnMixer(dynamic_parameters={'network_depth':4})
-
-    data_encoded = mixer.fit(ds)
-    predictions = mixer.predict(predict_input_ds)
-    print(predictions)
-
-    ##For Regression
-
-    # GENERATE DATA
-    ###############
-
-    config = {
-        'input_features': [
-            {
-                'name': 'x',
-                'type': 'numeric',
-                #'encoder_path': 'lightwood.encoders.numeric.numeric'
-            },
-            {
-                'name': 'y',
-                'type': 'numeric',
-                # 'encoder_path': 'lightwood.encoders.numeric.numeric'
-            }
-        ],
-
-        'output_features': [
-            {
-                'name': 'z',
-                'type': 'numeric',
-                # 'encoder_path': 'lightwood.encoders.categorical.categorical'
-            }
-        ]
-    }
-
-    data = {'x': [i for i in range(10)], 'y': [random.randint(i, i + 20) for i in range(10)]}
-    nums = [data['x'][i] * data['y'][i] for i in range(10)]
-
-    data['z'] = [i + 0.5 for i in range(10)]
-
-    data_frame = pandas.DataFrame(data)
-    ds = DataSource(data_frame, config)
-    predict_input_ds = DataSource(data_frame[['x', 'y']], config)
-    ####################
-
-    mixer = BayesianNnMixer(dynamic_parameters={})
-
-    for i in  mixer.iter_fit(ds):
-        print(i)
-
-    predictions = mixer.predict(predict_input_ds)
-    print(predictions)
