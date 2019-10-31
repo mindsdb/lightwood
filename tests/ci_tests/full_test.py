1ac98143b305600f9e5f3cfd64565413d05be6cd
Jorge Torres <jorge.torres.maldonado@gmail.com>
2019-10-31 01:40:00 -0500

    plinear working, results not that high, needs some evaluation regarding consistency

--- tests/ci_tests/full_test.py
+++ tests/ci_tests/full_test.py
@@ -38,4 +38,6 @@ def iter_function(epoch, error, test_error, test_error_gradient, test_accuracy):
 
 predictor.learn(from_data=df, callback_on_iter=iter_function, eval_every_x_epochs=2, stop_training_after_seconds=120)
 
-print(predictor.predict(when={'number_of_rooms':3, 'number_of_bathrooms':2, 'sqft':700, 'location':'great'}))
+print(predictor.predict(when={ 'number_of_bathrooms':2, 'sqft':700, 'location':'great'}))
+
+predictor.save('/tmp/test.lw')
