9ada43001d5133e72157b2c46c89f14a09102711
Jorge Torres <jorge.torres.maldonado@gmail.com>
2019-11-07 17:54:29 -0800

    config based plinear

--- lightwood/mixers/helpers/default_net.py
+++ lightwood/mixers/helpers/default_net.py
@@ -75,17 +75,23 @@ class DefaultNet(torch.nn.Module):
 
         layers = []
         for ind in range(len(shape) - 1):
-            layers.append(PLinear(shape[ind],shape[ind+1]))
+            linear_function = PLinear  if CONFIG.USE_PROBABILISTIC_LINEAR else torch.nn.Linear
+            layers.append(linear_function(shape[ind],shape[ind+1]))
             if ind < len(shape) - 2:
                 layers.append(rectifier())
 
 
         self.net = torch.nn.Sequential(*layers)
 
-        for layer in self.net:
-            if isinstance(layer, torch.nn.Linear):
-                torch.nn.init.normal_(layer.weight, mean=0., std=1 / math.sqrt(layer.out_features))
-                torch.nn.init.normal_(layer.bias, mean=0., std=0.1)
+        if CONFIG.DETERMINISTIC: # set initial weights based on a specific distribution if we have deterministic enabled
+            for layer in self.net:
+                if isinstance(layer, torch.nn.Linear):
+                    torch.nn.init.normal_(layer.weight, mean=0., std=1 / math.sqrt(layer.out_features))
+                    torch.nn.init.normal_(layer.bias, mean=0., std=0.1)
+
+                elif isinstance(layer, PLinear):
+                    torch.nn.init.normal_(layer.mean, mean=0., std=1 / math.sqrt(layer.out_features))
+                    torch.nn.init.normal_(layer.bias, mean=0., std=0.1)
 
         self.net = self.net.to(self.device)
 
