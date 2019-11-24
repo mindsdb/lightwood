ed50c349021e92b0ab794a77825d85a18c6c87f9
George3d6 <george@cerebralab.com>
2019-11-24 08:30:35 +0000

    added autoencoder for cateogries

--- lightwood/encoders/categorical/__init__.py
+++ lightwood/encoders/categorical/__init__.py
@@ -1,2 +1,6 @@
 from lightwood.encoders.categorical.categorical import CategoricalEncoder
+from lightwood.encoders.categorical.autoencoder import AutoEncoder
+
 default = CategoricalEncoder
+oh = CategoricalEncoder
+ae = AutoEncoder
