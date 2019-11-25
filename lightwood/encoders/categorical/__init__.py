df65a9a902190b991b07c71a0db1b7fda05447b4
George3d6 <george@cerebralab.com>
2019-11-25 18:14:48 +0000

    switched autoencoder it to be the default for cateogircal

--- lightwood/encoders/categorical/__init__.py
+++ lightwood/encoders/categorical/__init__.py
@@ -1,6 +1,6 @@
 from lightwood.encoders.categorical.categorical import CategoricalEncoder
 from lightwood.encoders.categorical.autoencoder import CategoricalAutoEncoder
 
-default = CategoricalEncoder
+default = CategoricalAutoEncoder
 oh = CategoricalEncoder
 ae = CategoricalAutoEncoder
