a93487c867012022944fe4a45325ade43caeeeef
George3d6 <george@cerebralab.com>
2019-11-27 17:39:44 +0000

    added selfaware option to the config

--- lightwood/config/config.py
+++ lightwood/config/config.py
@@ -11,8 +11,9 @@ class CONFIG:
     CACHE_ENCODED_DATA = True
     # Enable deterministic cuda flag and use seeds everywhere (static or based on features of the dataset)
     DETERMINISTIC = True
-    OVERSAMPLE = True
-
+    OVERSAMPLE = False
+    SELFAWARE = True
+    
     """Probabilistic FC layers"""
     USE_PROBABILISTIC_LINEAR = False # change weights in mixer to be probabilistic
 
