f3ee2f76b34ca92bc218443b885c8f61492fb634
Zoran Pandovski <zoran.pandovski@keitaro.com>
2019-12-02 11:07:09 +0100

    Initial version that works, fixed dependency issue for windows

--- setup.py
+++ setup.py
@@ -4,15 +4,7 @@ import setuptools
 import subprocess
 
 
-# @TODO: Figure out a way to check for this
-is_installed_from_pypi = False
-
-about = {}
-with open("lightwood/__about__.py") as fp:
-    exec(fp.read(), about)
-
-
-def remove_requirements(requirements, name, replace=None):
+def remove_requirements(requirements, name, replace=''):
     new_requirements = []
     for requirement in requirements:
         if requirement.split(' ')[0] != name:
@@ -23,55 +15,27 @@ def remove_requirements(requirements, name, replace=None):
 
 sys_platform = sys.platform
 
+about = {}
+with open("lightwood/__about__.py") as fp:
+    exec(fp.read(), about)
+    
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 with open('requirements.txt') as req_file:
     requirements = [req.strip() for req in req_file.read().splitlines()]
 
-# Linux specific requirements
-if sys_platform == 'linux' or sys_platform.startswith('linux'):
-    pass
-    #requirements = remove_requirements(requirements,'torch',replace='torch >= 1.1.0')
-
-
-# OSX specific requirements
-elif sys_platform == 'darwin':
-    pass
-    #requirements = remove_requirements(requirements,'torch',replace='torch >= 1.1.0.post2')
-
 # Windows specific requirements
-elif sys_platform in ['win32','cygwin','windows']:
-    if is_installed_from_pypi:
-        requirements = remove_requirements(requirements,'torch')
-        requirements = remove_requirements(requirements,'torchvision')
-
-    else:
-        if sys.version_info < (3,7):
-            requirements = remove_requirements(requirements,'torch',replace='torch @ https://download.pytorch.org/whl/cu100/torch-1.2.0-cp36-cp36m-win_amd64.whl')
-            requirements = remove_requirements(requirements,'torchvision',replace='torchvision @ https://download.pytorch.org/whl/cu100/torchvision-0.4.0-cp36-cp36m-win_amd64.whl')
-        else:
-            requirements = remove_requirements(requirements,'torch',replace='torch @ https://download.pytorch.org/whl/cu100/torch-1.2.0-cp37-cp37m-win_amd64.whl')
-            requirements = remove_requirements(requirements,'torchvision',replace='torchvision @ https://download.pytorch.org/whl/cu100/torchvision-0.4.0-cp37-cp37m-win_amd64.whl')
-
-    requirements.append('cwrap')
-
-# For stuff like freebsd
-else:
-    print('\n\n====================\n\nError, platform {sys_platform} not recognized, proceeding to install anyway, but lightwood might not work properly !\n\n====================\n\n')
-
-if is_installed_from_pypi and (sys_platform in ['win32','cygwin','windows']):
+if sys_platform in ['win32','cygwin','windows']:
+    requirements = remove_requirements(requirements,'torch')
+    requirements = remove_requirements(requirements,'torchvision')
     try:
-        subprocess.call(['pip','install','https://download.pytorch.org/whl/cu100/torch-1.1.0-cp37-cp37m-win_amd64.whl'])
-        print('Successfully installed pytorch !')
+        print('Installing pytorch and torchvision!')
+        subprocess.call(['pip','install','torch===1.2.0', 'torchvision===0.4.0', '-f', 'https://download.pytorch.org/whl/torch_stable.html'])
+        print('Successfully installed pytorch and torchvision!')
     except:
         print('Failed to install pytroch, please install pytroch and torchvision manually be following the simple instructions over at: https://pytorch.org/get-started/locally/')
 
-    try:
-        subprocess.call(['pip','install','https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp37-cp37m-win_amd64.whl'])
-        print('Successfully installed Torchvision !')
-    except:
-        print('Failed to install torchvision, please install pytroch and torchvision manually be following the simple instructions over at: https://pytorch.org/get-started/locally/')
 
 setuptools.setup(
     name=about['__title__'],
