76fc0e9191be1027c77f58ff32b1bdf66c16c8d8
George3d6 <george@cerebralab.com>
2019-12-15 10:42:05 +0000

    added numerical prediction head

--- lightwood/encoders/text/distilbert.py
+++ lightwood/encoders/text/distilbert.py
@@ -9,6 +9,7 @@ from transformers import DistilBertModel, DistilBertForSequenceClassification, D
 
 from lightwood.config.config import CONFIG
 from lightwood.constants.lightwood import COLUMN_DATA_TYPES, ENCODER_AIM
+from lightwood.mixers.helpers.default_net import DefaultNet
 
 
 class DistilBertEncoder:
@@ -56,12 +57,108 @@ class DistilBertEncoder:
             raise Exception('You can only call "prepare_encoder" once for a given encoder.')
 
         priming_data = [x if x is not None else '' for x in priming_data]
-        
+
         self._max_len = min(max([len(x) for x in priming_data]),self._model_max_len)
         self._tokenizer = self._tokenizer_class.from_pretrained(self._pretrained_model_name)
         self._pad_id = self._tokenizer.convert_tokens_to_ids([self._tokenizer.pad_token])[0]
         # @TODO: Support multiple targets if they are all categorical or train for the categorical target if it's a mix (maybe ?)
         # @TODO: Attach a language modeling head and/or use GPT2 and/or provide outputs better suited to a LM head (which will be the mixer) if the output if text
+
+        if training_data['targets'][0]['output_type'] == COLUMN_DATA_TYPES.NUMERIC and CONFIG.TRAIN_TO_PREDICT_TARGET:
+            self._model_type = 'classifier'
+            self._model = self._classifier_model_class.from_pretrained(self._pretrained_model_name).to(self.device)
+
+            if self.aim == ENCODER_AIM.SPEED:
+                batch_size = 10
+            else:
+                batch_size = 10
+
+            self._head = DefaultNet(ds=None, dynamic_parameters={},shape=[128,64,1], selfaware=False)
+            no_decay = ['bias', 'LayerNorm.weight']
+            optimizer_grouped_parameters = [
+                {'params': [p for n, p in self._head.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.000001},
+                {'params': [p for n, p in self._head.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+            ]
+
+            optimizer = AdamW(optimizer_grouped_parameters, lr=5e-5, eps=1e-8)
+            # num_training_steps is kind of an estimation
+            scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=10, num_training_steps=len(priming_data) * 15/20)
+
+            criterion = torch.nn.MSELoss()
+
+            self._model = torch.nn.Sequential(self._model, self._head)
+            self._model.train()
+            error_buffer = []
+            started = time.time()
+
+            best_error = None
+            best_model = None
+            best_epoch = None
+
+            random.seed(len(priming_data))
+
+            for epoch in range(5000):
+                running_loss = 0
+                error = None
+
+                merged_data = list(zip(priming_data, training_data['targets'][0]['encoded_output']))
+                random.shuffle(merged_data)
+
+                randomized_priming_data, randomized_target_data = zip(*merged_data)
+
+                itterable_priming_data = zip(*[iter(randomized_priming_data)]*batch_size)
+
+                for i, data in enumerate(itterable_priming_data):
+                    inputs = []
+                    for text in data:
+                        input = self._tokenizer.encode(text[:self._max_len], add_special_tokens=True)
+                        inputs.append(input)
+
+                    labels =  randomized_target_data[i*batch_size:(i+1)*batch_size]]
+
+                    max_input = max([len(x) for x in inputs])
+                    inputs = [x + [self._pad_id] * (max_input - len(x)) for x in inputs]
+                    inputs = torch.tensor(inputs).to(self.device)
+
+                    outputs = self._model(inputs)
+                    print(outputs)
+
+                    loss = self.criterion(outputs, labels)
+                    loss.backward()
+                    running_loss += loss.item()
+
+                    optimizer.step()
+                    scheduler.step()
+
+                    self._model.zero_grad()
+
+                    error = running_loss/(i + 1)
+
+                    if i % 200 == 0:
+                        logging.info(f'Intermediate text encoder error: {error}')
+
+                logging.info(f'Text encoder training error: {error}')
+                if best_error is None or best_error > error:
+                    best_error = error
+                    # Move to CPU to save GPU memory, move back to the origianl device if we end up using it
+                    self._model = self._model.cpu()
+                    best_model = copy.deepcopy(self._model)
+                    self._model = self._model.to(self.device)
+                    best_epoch = epoch
+
+                error_buffer.append(error)
+
+                if len(error_buffer) > 3:
+                    error_buffer.append(error)
+                    error_buffer = error_buffer[-3:]
+                    delta_mean = np.mean(error_buffer)
+                    if delta_mean < 0 or error < self.desired_error or best_epoch < epoch - 5:
+                        self._model = best_model.to(self.device)
+                        break
+
+                if started + self.max_training_time < time.time():
+                    self._model = best_model.to(self.device)
+                    break
 
         if training_data is not None and 'targets' in training_data and len(training_data['targets']) ==1 and training_data['targets'][0]['output_type'] == COLUMN_DATA_TYPES.CATEGORICAL and CONFIG.TRAIN_TO_PREDICT_TARGET:
             self._model_type = 'classifier'
@@ -182,3 +279,31 @@ class DistilBertEncoder:
     def decode(self, encoded_values_tensor, max_length = 100):
         # When test is an output... a bit trickier to handle this case, thinking on it
         pass
+
+
+if __name__ == "__main__":
+    # Generate some tests data
+    import random
+    from sklearn.metrics import accuracy_score
+
+    random.seed(2)
+    priming_data = []
+    primting_target = []
+    test_data = []
+    test_target = []
+    for i in range(0,2000):
+        if random.randint(1,5) %  == 3:
+            test_data.append(str(i))
+            test_target.append(i)
+        else:
+            priming_data.append(str(i))
+            primting_target.append(i)
+
+    enc = DistilBertEncoder()
+
+    enc.prepare_encoder(priming_data, training_data={'targets': [{'output_type': COLUMN_DATA_TYPES.NUMERIC, 'encoded_output': primting_target}]})
+    predicted_target = enc.encode(test_data)
+
+    encoder_accuracy = accuracy_score(list(predicted_target), test_target)
+    print(f'Categorial encoder accuracy for: {encoder_accuracy} on testing dataset')
+    assert(encoder_accuracy > 0.98)
