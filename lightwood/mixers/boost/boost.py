647a6d3577e96ea2992bc03ef1a563adde4ee40c
George3d6 <george@cerebralab.com>
2020-01-14 18:56:57 +0200

    added xgdboost mixer

--- lightwood/mixers/boost/boost.py
+++ lightwood/mixers/boost/boost.py
@@ -1,21 +1,53 @@
+import numpy as np
 import xgboost as xgb
+
+from lightwood.constants.lightwood import COLUMN_DATA_TYPES
 
 
 class BoostMixer():
 
     def __init__(self):
-        pass
+        self.targets = None
 
-    def fit(self, data_source):
-        input_features = data_source.configuration['input_features']
-        output_features data_source.configuration['output_features']
+    def train(self, data_source):
+        output_features = data_source.configuration['output_features']
 
-        print(input_features)
-        print(output_features)
+        self.targets = {}
+        for output_feature in output_features:
+            self.targets[output_feature['name']] = {
+                'type': output_feature['type']
+            }
 
+        X = []
         for row in data_source:
-            print(row)
+            X.append(np.array(row[0]))
 
+        X = np.array(X)
+        for target_col_name in self.targets:
+            Y = data_source.get_column_original_data(target_col_name)
             
+            if self.targets[target_col_name]['type'] == COLUMN_DATA_TYPES.CATEGORICAL:
+                self.targets[target_col_name]['model'] = xgb.XGBClassifier()
+                self.targets[target_col_name]['model'].fit(X,Y)
+
+            elif self.targets[target_col_name]['type'] == COLUMN_DATA_TYPES.NUMERIC:
+                self.targets[target_col_name]['model'] = xgb.XGBRegressor()
+                self.targets[target_col_name]['model'].fit(X,Y)
+
+            else:
+                self.targets[target_col_name]['model'] = None
+
+
     def predict(self, when_data_source):
-        pass
+        X = []
+        for row in when_data_source:
+            X.append(np.array(row[0]))
+
+        predictions = {}
+        for target_col_name in self.targets:
+            if self.targets[target_col_name]['model'] is None:
+                return False
+
+            predictions[target_col_name] = self.targets[target_col_name]['model'].predict(X)
+
+        return predictions
