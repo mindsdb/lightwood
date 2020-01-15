c5722eb61834e945fea5d085b5d1900209c0898b
George3d6 <george@cerebralab.com>
2020-01-15 23:32:02 +0200

    fixed some magic numbers

--- lightwood/api/predictor.py
+++ lightwood/api/predictor.py
@@ -365,7 +365,7 @@ class Predictor:
 
         for output_column in main_mixer_predictions:
             if output_column in self._helper_mixers:
-                if self._helper_mixers[output_column]['accuracy'] > 1.4 * self.train_accuracy[output_column]['value']:
+                if self._helper_mixers[output_column]['accuracy'] > 1.05 * self.train_accuracy[output_column]['value']:
                     helper_mixer_predictions = self._helper_mixers[output_column]['model'].predict(when_data_ds, output_column)
                     main_mixer_predictions[output_column] = {'predictions': list(helper_mixer_predictions[output_column])}
 
