0bfcd820938cbbe893d540567fd057fa382123c9
Zoran Pandovski <zoran.pandovski@keitaro.com>
2020-01-16 18:36:12 +0100

    Call tests from ci

--- tests/ci_tests/ci_tests.py
+++ tests/ci_tests/ci_tests.py
@@ -1,23 +1,30 @@
 import runpy
 
+pdir = '../../lightwood/'
+encoders_path = pdir + 'encoders/'
+mixers_path = pdir + 'mixers/'
 MODULES = [
-    './lightwood/encoders/categorical/onehot.py',
-    './lightwood/encoders/datetime/datetime.py',
+    f'{encoders_path}categorical/onehot.py',
+    f'{encoders_path}datetime/datetime.py',
+    f'{mixers_path}helpers/plinear.py',
+    f'{pdir}api/data_source.py',
+    f'{mixers_path}nn/nn.py',
+    f'{encoders_path}categorical/autoencoder.py',
+    #f'{encoders_path}text/rnn.py'.format(encoders_path),
     #'./lightwood/encoders/image/nn.py',
     #'./lightwood/encoders/image/img_2_vec.py',
-    './lightwood/encoders/text/rnn.py',
-    './lightwood/encoders/numeric/numeric.py',
-    './lightwood/mixers/helpers/plinear.py',
-    #'./lightwood/encoders/categorical/autoencoder.py',
-    './lightwood/encoders/time_series/cesium_ts.py',
-   # './lightwood/mixers/sk_learn/sk_learn.py',
-    './lightwood/mixers/nn/nn.py',
+    #'./lightwood/encoders/numeric/numeric.py',
+    #'./lightwood/encoders/time_series/cesium_ts.py',
+    #'./lightwood/mixers/sk_learn/sk_learn.py',
     #'./lightwood/encoders/text/infersent.py',
-    './lightwood/api/data_source.py'
+    #'./lightwood/encoders/text/distilbert.py'
 ]
 
+
 def run_tests(modules):
-    
+    '''
+    Run modules as scripts to execute main function
+    '''    
     for module in modules:
         runpy.run_path(module, run_name='__main__')
 
