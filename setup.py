5445a6699e9c452f554eb3bac9ca59ade1208101
George3d6 <george@cerebralab.com>
2020-01-18 20:57:05 +0200

    bumped to latest torch version, changed window requirements

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ sys_platform = sys.platform
 about = {}
 with open("lightwood/__about__.py") as fp:
     exec(fp.read(), about)
-    
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
@@ -26,6 +26,7 @@ with open('requirements.txt') as req_file:
     requirements = [req.strip() for req in req_file.read().splitlines()]
 
 # Windows specific requirements
+'''
 if sys_platform in ['win32','cygwin','windows']:
     requirements = remove_requirements(requirements,'torch')
     requirements = remove_requirements(requirements,'torchvision')
@@ -35,7 +36,7 @@ if sys_platform in ['win32','cygwin','windows']:
         print('Successfully installed pytorch and torchvision!')
     except:
         print('Failed to install pytroch, please install pytroch and torchvision manually be following the simple instructions over at: https://pytorch.org/get-started/locally/')
-
+'''
 
 setuptools.setup(
     name=about['__title__'],
