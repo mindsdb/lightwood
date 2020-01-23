0ada9f6926076bc8657b3730bfc3e27ffb6ebf12
George3d6 <george@cerebralab.com>
2020-01-23 16:15:54 +0200

    added scaffolding for audio encoder

---
+++ lightwood/encoders/audio/amplitude_ts.py
@@ -0,0 +1,38 @@
+from pydub import AudioSegment
+import requests
+import numpy as np
+
+from lightwood.encoders.categorical.onehot import CesiumTsEncoder
+
+
+
+class AmplitudeTsEncoder:
+
+    def __init__(self, is_target = False):
+        self._pytorch_wrapper = torch.FloatTensor
+        self._ts_encoder = CesiumTsEncoder()
+
+    def prepare_encoder(self, priming_data):
+        pass
+
+    def encode(self, column_data):
+        encoded_audio_arr = []
+        for path in  column_data:
+            if path.startswith('http'):
+                response = requests.get(path)
+                audio = AudioSegment.from_raw(BytesIO(response.content))
+            else:
+                audio = AudioSegment.from_file(path)
+            # For now convert all (usually will be stereo) to mono by adding up and averging the amplitudes
+            audio = audio.set_channels(1)
+            audio_arr = np.array(audio.get_array_of_samples())
+            encoded_audio_arr = self._ts_encoder.encode(audio_arr)
+            encoded_audio_arr.append(encoded_audio_arr)
+
+        return self._pytorch_wrapper(encoded_audio_arr)
+
+    def decode(self, encoded_values_tensor):
+        raise Exception('This encoder is not bi-directional')
+
+if __name__ == "__main__":
+    encoder = AmplitudeTsEncoder()
