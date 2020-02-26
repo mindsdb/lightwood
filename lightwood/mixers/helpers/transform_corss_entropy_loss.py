20abb8f8df5b077b7ab7b2219339ee2424792cd1
George3d6 <george@cerebralab.com>
2020-02-26 18:29:19 +0200

    fixed some naming

--- lightwood/mixers/helpers/transform_corss_entropy_loss.py
+++ lightwood/mixers/helpers/transform_corss_entropy_loss.py
@@ -3,8 +3,9 @@ import torch
 # Basically cross entropy loss that does the one hot decoding of the targets inside of it... useful for code-logic reasons to have it setup like this
 class TransformCrossEntropyLoss(torch.nn.Module):
     def __init__(self, **kwargs):
+        super().__init__()
         self.cross_entropy_loss = torch.nn.CrossEntropyLoss(**kwargs)
 
-    def forward(self, input, target):
+    def forward(self, preds, target):
         cat_labels = target.max(1).indices
-        return self.cross_entropy_loss(outputs, cat_labels)
+        return self.cross_entropy_loss(preds, cat_labels)
