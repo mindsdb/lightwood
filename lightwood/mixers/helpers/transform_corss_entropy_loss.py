473d83413d037325751bb3db776665cf45917d9f
George3d6 <george@cerebralab.com>
2020-02-26 18:27:39 +0200

    added kwargs passing as named arguments

--- lightwood/mixers/helpers/transform_corss_entropy_loss.py
+++ lightwood/mixers/helpers/transform_corss_entropy_loss.py
@@ -2,8 +2,8 @@ import torch
 
 # Basically cross entropy loss that does the one hot decoding of the targets inside of it... useful for code-logic reasons to have it setup like this
 class TransformCrossEntropyLoss(torch.nn.Module):
-    def __init__(self, *kwargs):
-        sefl.cross_entropy_loss = torch.nn.CrossEntropyLoss(*kwargs)
+    def __init__(self, **kwargs):
+        sefl.cross_entropy_loss = torch.nn.CrossEntropyLoss(**kwargs)
 
     def forward(self, input, target):
         cat_labels = target.max(1).indices
