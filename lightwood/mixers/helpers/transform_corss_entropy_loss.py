enabled qunatiles in numerical encoder, fixed max confidence logic

lightwood/mixers/helpers/transform_corss_entropy_loss.py
@@ -10,10 +10,11 @@ class TransformCrossEntropyLoss(torch.nn.Module):
         cat_labels = target.max(1).indices
         return self.cross_entropy_loss(preds, cat_labels)
 
-    def estimate_confidence(self, preds, maximum_confidence):
+    def estimate_confidence(self, preds, maximum_confidence=None):
         confidences = []
         for pred in preds:
             conf = float(pred.max(0).values)/float(sum([x if x > 0 else 0.000001 for x in preds.sum(0)]))
-            conf = conf/maximum_confidence
+            if maximum_confidence is not None:
+                conf = conf/maximum_confidence
             confidences.append(conf)
         return confidences