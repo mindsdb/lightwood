da3def4f6c10073ae4ea6d47e7c8e499e58ff309
George3d6 <george@cerebralab.com>
2020-04-06 19:47:55 +0300

    moved ax to optional requirements

--- lightwood/model_building/__init__.py
+++ lightwood/model_building/__init__.py
@@ -1,1 +1,5 @@
-from lightwood.model_building.basic_ax_optimizer.basic_ax_optimizer import BasicAxOptimizer
+try:
+    from lightwood.model_building.basic_ax_optimizer.basic_ax_optimizer import BasicAxOptimizer
+except:
+    # Optimizer is optional
+    pass
