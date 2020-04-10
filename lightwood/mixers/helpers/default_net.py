d6dde445fccd8ba29efa07f813127d4a96e1ae30
George3d6 <george@cerebralab.com>
2020-04-10 16:49:53 +0300

    removed ds usage from default net fully

--- lightwood/mixers/helpers/default_net.py
+++ lightwood/mixers/helpers/default_net.py
@@ -27,7 +27,7 @@ class DefaultNet(torch.nn.Module):
                 In order to make sure pytorch randomly generate number will be the same every time
                 when training on the same dataset
             '''
-            torch.manual_seed(2)
+            torch.manual_seed(66)
 
             if device_str == 'cuda':
                 torch.backends.cudnn.deterministic = True
