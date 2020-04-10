36d1d0c283d72ff908bf028a86579a63d98c68b5
George3d6 <george@cerebralab.com>
2020-04-10 16:30:02 +0300

    removed quantile loss issue

--- lightwood/data_schemas/predictor_config.py
+++ lightwood/data_schemas/predictor_config.py
@@ -20,18 +20,15 @@ mixer_graph_schema = Schema({
 })
 
 mixer_schema = Schema({
-    'class': object,
+    Optional('class'): object,
     Optional('attrs'): dict,
-    Optional('mixer_graph'): [mixer_graph_schema]
+    Optional('deterministic', default=True): bool,
+    Optional('selfaware', default=True): bool
 })
 
 data_source_schema = Schema({
     Optional('cache_transformed_data', default=True): bool,
 })
-
-default_data_source_config = {
-    'cache_transformed_data': True
-}
 
 predictor_config_schema = Schema({
     'input_features': [
@@ -40,7 +37,7 @@ predictor_config_schema = Schema({
     'output_features': [
         feature_schema
     ],
-    Optional('data_source', default=default_data_source_config): data_source_schema,
-    Optional('mixer'): mixer_schema,
+    Optional('data_source', default=data_source_schema.validate({})): data_source_schema,
+    Optional('mixer', default=mixer_schema.validate({})): mixer_schema,
     Optional('optimizer'): object
 })
