7d64550a5d1c5581c7db5e3f51f4a1689799b1c8
maximlopin <wzzhz13@gmail.com>
2020-07-02 16:31:08 +0300

    rename

--- lightwood/encoders/text/short.py
+++ lightwood/encoders/text/short.py
@@ -11,26 +11,31 @@ def _sum(embed_list):
     return torch.sum(*embed_list)
 
 
-class AutoEncoder():
-    def __init__(self, combine='concat'):
+class ShortTextEncoder():
+    def __init__(self, is_target=False, onehot=False, combine='concat'):
         if combine == 'concat':
             self._combine_fn = _cat
         elif combine == 'sum':
             self._combine_fn = _sum
         else:
             raise ValueError('expected combine to be "concat" or "sum"')
+        
+        if onehot:
+            raise NotImplementedError
+        else:
+            self._model = WordEmbeddings('glove')
 
-        self._model = WordEmbeddings('glove')
+    def prepare_encoder(self, _):
+        pass
 
     def encode(self, column_data):
         output = []
         for cell in column_data:
             sent = Sentence(cell)
             self._model.embed(sent)
-            vector = self.combine_fn([tok.embedding for tok in sent.tokens])
+            vector = self._combine_fn([tok.embedding for tok in sent.tokens])
             output.append(vector)
         return output
 
     def decoder(self, vectors):
-        # TODO
         raise NotImplementedError
