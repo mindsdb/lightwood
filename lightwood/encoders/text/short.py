0556d1bdbf91cc89824d835c7b2fb062bf24c8d5
maximlopin <wzzhz13@gmail.com>
2020-07-06 00:08:52 +0300

    concat

--- lightwood/encoders/text/short.py
+++ lightwood/encoders/text/short.py
@@ -1,48 +1,82 @@
 import torch
 from flair.data import Sentence
+from torch.nn.functional import pad
 from lightwood.encoders.categorical import CategoricalAutoEncoder
 
 
-def _cat(embed_list):
-    assert len(embed_list) > 0
-    return torch.cat(embed_list, dim=0)
+def _get_tokens(text):
+    if len(text) > 0:
+        return [tok.text for tok in Sentence(text).tokens]
+    else:
+        return ['']
 
 
-def _sum(embed_list):
-    assert len(embed_list) > 0
-    return torch.cat([emb[None] for emb in embed_list], dim=0).mean(0)
+def _concat(vec_list, max_):
+    assert len(vec_list) > 0
+    assert len(vec_list) <= max_
+    assert max_ > 0
+
+    cat_vec = torch.cat(list(vec_list), dim=0)
+
+    pad_size = max_ - len(vec_list)
+    padding = (0, pad_size * vec_list[0].size(0))
+    padded = pad(cat_vec[None], padding, 'constant', 0)[0]
+
+    return padded
+
+
+def _mean(vec_list):
+    assert len(vec_list) > 0
+    return torch.cat([emb[None] for emb in vec_list], dim=0).mean(0)
 
 
 class TextAutoEncoder(CategoricalAutoEncoder):
-    def __init__(self, is_target=False, combine='sum'):
+    def __init__(self, is_target=False, combine='mean'):
         super().__init__(is_target)
 
-        if combine == 'concat':
-            self._combine_fn = _cat
-        elif combine == 'sum':
-            self._combine_fn = _sum
-        else:
-            raise ValueError('expected combine to be "concat" or "sum"')
+        if combine not in ['mean', 'concat']:
+            self._unexpected_combine()
+        
+        self._combine = combine
+        self._combine_fn = None
+    
+    def _unexpected_combine(self):
+        raise ValueError('unexpected combine value (must be "mean" or "concat")')
         
     def prepare_encoder(self, column_data):
         no_null_sentences = (x if x is not None else '' for x in column_data)
-        unique_words = set()
-        for sent in map(Sentence, no_null_sentences):
-            for tok in sent.tokens:
-                unique_words.add(tok.text)
-        super().prepare_encoder(unique_words)
+        unique_tokens = set()
+        max_words_per_sent = 0
+        for sent in no_null_sentences:
+            tokens = _get_tokens(sent)
+            if len(tokens) > max_words_per_sent:
+                max_words_per_sent = len(tokens)
+            for tok in tokens:
+                unique_tokens.add(tok)
+
+        super().prepare_encoder(unique_tokens)
+
+        if self._combine == 'concat':
+            self._combine_fn = lambda vecs: _concat(vecs, max_words_per_sent)
+        elif self._combine == 'mean':
+            self._combine_fn = lambda vecs: _mean(vecs)
+        else:
+            self._unexpected_combine()
 
     def encode(self, column_data):
         no_null_sentences = (x if x is not None else '' for x in column_data)
         output = []
-        for sent in map(Sentence, no_null_sentences):
-            if len(sent) > 0:
-                encoded_words = super().encode(list(tok.text for tok in sent.tokens))
-            else:
-                encoded_words = super().encode([''])
+        for sent in no_null_sentences:
+            tokens = _get_tokens(sent)
+            encoded_words = super().encode(tokens)
             encoded_sent = self._combine_fn(encoded_words)
             output.append(encoded_sent)
         return output
 
     def decode(self, vectors):
-        raise NotImplementedError
+        if self._combine == 'concat':
+            raise NotImplementedError
+        elif self._combine == 'mean':
+            raise ValueError('decode doesn\'t work with combine = mean')
+        else:
+            self._unexpected_combine()
