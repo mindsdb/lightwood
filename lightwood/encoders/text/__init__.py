9ee0096c25aaee3d08989418636b32652d7a3bec
maximlopin <wzzhz13@gmail.com>
2020-07-05 23:15:41 +0300

    sum vectors

--- lightwood/encoders/text/__init__.py
+++ lightwood/encoders/text/__init__.py
@@ -2,7 +2,7 @@ from lightwood.encoders.text.rnn import RnnEncoder
 from lightwood.encoders.text.infersent import InferSentEncoder
 from lightwood.encoders.text.distilbert import DistilBertEncoder
 from lightwood.encoders.text.tfidf import TfidfEncoder
-from lightwood.encoders.text.short import ShortTextEncoder
+from lightwood.encoders.text.short import TextAutoEncoder
 
 #default = TfidfEncoder
 default = DistilBertEncoder
