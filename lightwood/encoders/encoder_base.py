b9c433ca64bc14c708f92352417a71f69990da71
maximlopin <wzzhz13@gmail.com>
2020-07-13 14:49:09 +0300

    base encoder

---
+++ lightwood/encoders/encoder_base.py
@@ -0,0 +1,19 @@
+
+class EncoderBase:
+    """Base class for all encoders"""
+    def __init__(self, is_target=False):
+        self.is_target = is_target
+
+    def prepare_encoder(self, priming_data):
+        """Can be overridden"""
+        pass
+
+    def encode(self, column_data):
+        raise NotImplementedError
+
+    def decoder(self, encoded_data):
+        raise NotImplementedError
+
+    def to(self, device, available_devices):
+        """Can be overridden"""
+        return self
