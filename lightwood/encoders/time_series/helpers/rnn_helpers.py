0f502ea4a4c8f3b78e93fa2eb3c8dd90d3052253
Patricio Cerda Mardini <pcerdam@uc.cl>
2020-07-14 17:01:30 -0400

    added challenge submission

--- lightwood/encoders/time_series/helpers/rnn_helpers.py
+++ lightwood/encoders/time_series/helpers/rnn_helpers.py
@@ -2,38 +2,63 @@ import logging
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
-import numpy as np
 
 
-def tensor_from_series(series, device):
-    if type(series) != type([]):
-        series = str(series).split(' ')
+def tensor_from_series(series, device, n_dims=None, pad_value=0.0):
+    """
+    :param series: list of lists, corresponds to time series: [[x1_1, ..., x1_n], [x2_1, ..., x2_n], ...]
+                   the series is zero-padded on each axis so that all dimensions have equal length
+    :param device: computing device that PyTorch backend uses
+    :param n_dims: if not None, will zero-pad dimensions until series_dimensions = n_dims
+    :param pad_value: value to pad each dimension in the time series, if needed
+    :return: series as a tensor ready for model consumption, shape (1, ts_length, n_dims)
+    """
+    if isinstance(series[0], torch.Tensor):
+        return series[0]
 
     float_series = []
-    for ele in series:
-        try:
-            float_series.append(float(ele))
-        except:
-            logging.warning(f'Weird element encountered in timeseries: {ele} !')
-            float_series.append(0)
+    for dimn in series:
+        dimn_series = []
+        if isinstance(dimn[0], str):
+            dimn = [i for i in dimn[0].split(" ")]
+        for ele in dimn:
+            try:
+                dimn_series.append(float(ele))
+            except Exception as _:
+                logging.warning(f'Weird element encountered in timeseries: {ele} !')
+                dimn_series.append(0)
+        float_series.append(dimn_series)
 
-    return torch.tensor(float_series, dtype=torch.float, device=device).view(-1, 1, 1, 1).float()
+    # timestep padding
+    timesteps = max([len(dimn) for dimn in float_series])
+    for i in range(len(float_series)):
+        for _ in range(max(0, timesteps - len(float_series[i]))):
+            float_series[i].append(pad_value)
+
+    # dimension padding
+    if n_dims:
+        for _ in range(max(0, n_dims - len(float_series))):
+            float_series.append([pad_value] * timesteps)
+    else:
+        n_dims = len(float_series)
+
+    tensor = torch.tensor(float_series, dtype=torch.float, device=device).T.view(-1, timesteps, n_dims).float()
+    return tensor
 
 
 class DecoderRNNNumerical(nn.Module):
-    def __init__(self, hidden_size, output_size):
+    def __init__(self, hidden_size, output_size, dropout=0.2):
         super(DecoderRNNNumerical, self).__init__()
         self.hidden_size = hidden_size
         self.in_activation = nn.ReLU()
+        self.dropout = nn.Dropout(dropout)
         self.gru = nn.GRU(output_size, hidden_size)
         self.out = nn.Linear(hidden_size, output_size)
-
 
     def forward(self, input, hidden):
         output = self.in_activation(input.float())
         output, hidden = self.gru(output, hidden)
+        output = self.dropout(output)
         output = self.out(output)
         return output, hidden
 
@@ -41,19 +66,17 @@ class DecoderRNNNumerical(nn.Module):
         return torch.zeros(1, 1, self.hidden_size, device=device)
 
 
-
 class EncoderRNNNumerical(nn.Module):
-    def __init__(self,  hidden_size):
-        input_size = 1
+    def __init__(self,  input_size, hidden_size, dropout=0.2):
         super(EncoderRNNNumerical, self).__init__()
         self.hidden_size = hidden_size
-
-        self.gru = nn.GRU(input_size, hidden_size)
+        self.dropout = nn.Dropout(dropout)
+        self.gru = nn.GRU(input_size, hidden_size, batch_first=True)
         self.out = nn.Linear(hidden_size, input_size)
 
     def forward(self, input, hidden):
-
         output, hidden = self.gru(input, hidden)
+        output = self.dropout(output)
         output = self.out(output)
         return output, hidden
 
--- lightwood/encoders/time_series/rnn.py
+++ lightwood/encoders/time_series/rnn.py
@@ -1,125 +1,172 @@
 from __future__ import unicode_literals, print_function, division
 
-from lightwood.encoders.time_series.helpers.rnn_helpers import *
-from lightwood.config.config import CONFIG
-from lightwood.helpers.device import get_devices
+import random
 
+import numpy as np
 import torch
 import torch.nn as nn
 from torch import optim
-import numpy as np
+
+from lightwood.encoders.time_series.helpers.rnn_helpers import *
+from lightwood.helpers.device import get_devices
 
 
 class RnnEncoder:
+    def __init__(self, encoded_vector_size=4, train_iters=75000, stop_on_error=0.8, learning_rate=0.01,
+                 is_target=False, ts_n_dims=1, dropout=0.2):
+        self.device, _ = get_devices()
 
-    def __init__(self, encoded_vector_size=4, train_iters=75000, stop_on_error=0.8, learning_rate=0.01, is_target=False):
         self._stop_on_error = stop_on_error
         self._learning_rate = learning_rate
         self._encoded_vector_size = encoded_vector_size
         self._train_iters = train_iters
         self._pytorch_wrapper = torch.FloatTensor
-        self._encoder = None
+        self._encoder = EncoderRNNNumerical(input_size=ts_n_dims, hidden_size=self._encoded_vector_size,
+                                            dropout=dropout).to(self.device)
+        self._decoder = DecoderRNNNumerical(output_size=ts_n_dims, hidden_size=self._encoded_vector_size,
+                                            dropout=dropout).to(self.device)
+        self.parameters = list(self._encoder.parameters()) + list(self._decoder.parameters())
+        self.optimizer = optim.Adam(self.parameters, lr=self._learning_rate)
+        self.criterion = nn.MSELoss()
         self._prepared = False
-
-        self.device, _ = get_devices()
+        self._n_dims = ts_n_dims  # expected dimensionality of time series
+        self._max_length = 20  # static decoding length
+        self._sos = 0.0  # start of sequence for decoding
+        self._eos = 0.0  # end of input sequence -- padding value for batches
 
     def to(self, device, available_devices):
         self.device = device
         self._encoder = self._encoder.to(self.device)
         return self
-    
-    def prepare_encoder(self, priming_data, feedback_hoop_function = None):
+
+    def prepare_encoder(self, priming_data, feedback_hoop_function=None, batch_size=1):
         """
         The usual, run this on the initial training data for the encoder
-        :param priming_data: a list of lists [[time_series], ...]
-        :param feedback_hoop_function: [if you wan to get feedback on the training process]
+        :param priming_data: a list of (self._n_dims)-dimensional time series [[dim1_data], ...]
+        :param feedback_hoop_function: [if you want to get feedback on the training process]
+        :param batch_size
         :return:
         """
         if self._prepared:
             raise Exception('You can only call "prepare_encoder" once for a given encoder.')
 
-        self._encoder = EncoderRNNNumerical(hidden_size=self._encoded_vector_size).to(self.device)
-        optimizer = optim.Adam(self._encoder.parameters(), lr=self._learning_rate)
-        criterion = nn.MSELoss()
-
         self._encoder.train()
         for i in range(self._train_iters):
             average_loss = 0
-            for data_point in priming_data:
-                data_tensor = tensor_from_series(data_point, self.device)
-                loss = 0
+            data_idx = 0
 
-                optimizer.zero_grad()
+            while data_idx < len(priming_data):
+
+                # batch building
+                data_points = priming_data[data_idx:min(data_idx + batch_size, len(priming_data))]
+                batch = []
+                for dp in data_points:
+                    data_tensor = tensor_from_series(dp, self.device, n_dims=self._n_dims, pad_value=self._eos)
+                    batch.append(data_tensor)
+
+                # shape: (batch_size, timesteps, n_dims)
+                batch = torch.cat(batch, dim=0).to(self.device)
+                data_idx += batch_size
+
+                # setup loss and optimizer
+                steps = batch.shape[1]
+                loss = 0
+                self.optimizer.zero_grad()
+
+                # encode
                 encoder_hidden = self._encoder.initHidden(self.device)
-                next_tensor = data_tensor[0]
-                for tensor_i in range(len(data_tensor)-1):
+                next_tensor = data_tensor[:, 0, :].unsqueeze(dim=1)  # initial input
+
+                for tensor_i in range(steps - 1):
                     rand = np.random.randint(2)
-                    # teach from forward as well as from known tensor alteratively
+                    # teach from forward as well as from known tensor alternatively
                     if rand == 1:
-                        next_tensor, encoder_hidden = self._encoder.forward(data_tensor[tensor_i] , encoder_hidden)
+                        next_tensor, encoder_hidden = self._encoder.forward(
+                            data_tensor[:, tensor_i, :].unsqueeze(dim=1),
+                            encoder_hidden)
                     else:
                         next_tensor, encoder_hidden = self._encoder.forward(next_tensor.detach(), encoder_hidden)
-                    loss += criterion(next_tensor, data_tensor[tensor_i+1])
 
+                    loss += self.criterion(next_tensor, data_tensor[:, tensor_i + 1, :].unsqueeze(dim=1))
 
-                loss = loss
+                # decode
+                decoder_hidden = encoder_hidden
+                sos_vector = torch.full((batch.shape[0], 1, batch.shape[2]), self._sos,
+                                        dtype=torch.float32).to(self.device)
+                next_tensor = torch.clone(sos_vector)
+                tensor_target = torch.cat([sos_vector, batch], dim=1)
+
+                for tensor_i in range(steps - 1):
+                    rand = np.random.randint(2)
+                    # teach from forward as well as from known tensor alternatively
+                    if rand == 1:
+                        next_tensor, decoder_hidden = self._decoder.forward(
+                            tensor_target[:, tensor_i, :].unsqueeze(dim=1),
+                            decoder_hidden)
+                    else:
+                        next_tensor, decoder_hidden = self._decoder.forward(next_tensor.detach(), decoder_hidden)
+
+                    loss += self.criterion(next_tensor, tensor_target[:, tensor_i + 1, :].unsqueeze(dim=1))
+
                 average_loss += int(loss)
                 loss.backward()
-                optimizer.step()
+                self.optimizer.step()
 
-            average_loss = average_loss/len(priming_data)
+            average_loss = average_loss / len(priming_data)
 
             if average_loss < self._stop_on_error:
                 break
             if feedback_hoop_function is not None:
-                feedback_hoop_function("epoch [{epoch_n}/{total}] average_loss = {average_loss}".format(average_loss=average_loss, epoch_n=i+1, total=self._train_iters))
+                feedback_hoop_function("epoch [{epoch_n}/{total}] average_loss = {average_loss}".format(
+                    epoch_n=i + 1,
+                    total=self._train_iters,
+                    average_loss=round(average_loss, 2)))
 
         self._prepared = True
 
-    def _encode_one(self, data, initial_hidden = None, return_next_value = False):
+    def _encode_one(self, data, initial_hidden=None, return_next_value=False):
         """
         This method encodes one single row of serial data
-        :param data: a string representing a list of values separate by space, for example: `1 2 3 4` or a list [1, 2, 3, 4]
+        :param data: multidimensional time series as list of lists [[dim1_data], [dim2_data], ...]
+                     (dim_data: string with format "x11, x12, ... x1n")
         :param initial_hidden: if you want to encode from an initial hidden state other than 0s
-        :param return_next_value:  if you to return the next value in the time series too
+        :param return_next_value:  if you want to return the next value in the time series too
 
-        :return:  either encoded_value or encoded_value, next_value
+        :return:  either encoded_value or (encoded_value, next_value)
         """
         self._encoder.eval()
         with torch.no_grad():
-
-            data_tensor = tensor_from_series(data, self.device)
-
+            data_tensor = tensor_from_series(data, self.device, n_dims=self._n_dims, pad_value=self._eos)
+            steps = data_tensor.shape[1]
             encoder_hidden = self._encoder.initHidden(self.device)
             encoder_hidden = encoder_hidden if initial_hidden is None else initial_hidden
-            if type(encoder_hidden) is list:
-                encoder_hidden = torch.Tensor([[encoder_hidden]], device=device)
+
             next_tensor = None
-            for tensor_i in range(len(data_tensor)):
-                next_tensor, encoder_hidden = self._encoder.forward(data_tensor[tensor_i], encoder_hidden)
+            for tensor_i in range(steps):
+                next_tensor, encoder_hidden = self._encoder.forward(data_tensor[:, tensor_i, :].unsqueeze(dim=0),
+                                                                    encoder_hidden)
 
         if return_next_value:
             return encoder_hidden, next_tensor
         else:
             return encoder_hidden
 
-
-    def encode(self, column_data, get_next_count = None):
+    def encode(self, column_data, get_next_count=None):
         """
         Encode a list of time series data
-        :param column_data: a list of lists [ [ts_data_point_list1], ...] to encode
-        :param get_next_count: default None, but you can pass a number X and it will return the X following predictions on the series for each ts_data_point in column_data
-        :return: a list of encoded values or if get_next_count !=0 two lists encoded_values, projected_numbers
+        :param column_data: a list of (self._n_dims)-dimensional time series [[dim1_data], ...] to encode
+        :param get_next_count: default None, but you can pass a number X and it will return the X following predictions
+                               on the series for each ts_data_point in column_data
+        :return: a list of encoded time series or if get_next_count !=0 two lists (encoded_values, projected_numbers)
         """
 
         if not self._prepared:
             raise Exception('You need to call "prepare_encoder" before calling "encode" or "decode".')
 
-
-        ret = []
-        next = []
-
         for val in column_data:
+            ret = []
+            next = []
+
             if get_next_count is None:
                 encoded = self._encode_one(val)
             else:
@@ -132,14 +179,13 @@ class RnnEncoder:
                 next_i = []
 
                 for j in range(get_next_count):
-
                     hidden, next_reading = self._encode_one(vector, initial_hidden=hidden, return_next_value=True)
                     vector = [next_reading]
                     if j == 0:
                         encoded = hidden
-                    next_i += [next_reading]
+                    next_i += [next_reading.cpu()]
 
-                next += [next_i[0][0].cpu()]
+                next += [torch.stack(next_i)]
 
             ret += [encoded[0][0].cpu()]
 
@@ -148,26 +194,71 @@ class RnnEncoder:
         else:
             return self._pytorch_wrapper(torch.stack(ret)), self._pytorch_wrapper(torch.stack(next))
 
+    def _decode_one(self, hidden):
+        """
+        Decodes a single time series from its encoded representation.
+        :param hidden: time series embedded representation tensor, with size self._encoded_vector_size
+        :return: decoded time series list
+        """
+        self._decoder.eval()
+        with torch.no_grad():
+            ret = []
+            next_tensor = torch.full((1, 1, self._n_dims), self._sos, dtype=torch.float32).to(self.device)
+            for _ in range(self._max_length):
+                next_tensor, hidden = self._decoder.forward(next_tensor, hidden)
+                ret.append(next_tensor)
+            return torch.stack(ret)
+
+    def decode(self, encoded_data):
+        """
+        Decode a list of embedded multidimensional time series
+        :param encoded_data: a list of embeddings [ e1, e2, ...] to be decoded into time series
+        :return: a list of reconstructed time series
+        """
+        if not self._prepared:
+            raise Exception('You need to call "prepare_encoder" before calling "encode" or "decode".')
+
+        ret = []
+        for _, val in enumerate(encoded_data):
+            hidden = torch.unsqueeze(torch.unsqueeze(val, dim=0), dim=0).to(self.device)
+            reconstruction = self._decode_one(hidden).cpu().squeeze().T.tolist()
+            ret += [reconstruction]
+
+        return self._pytorch_wrapper(ret)
 
 
 # only run the test if this file is called from debugger
 if __name__ == "__main__":
-    series = []
-    for j in range(100):
-        length = 3 + np.random.randint(14)
-        skip = 1+np.random.randint(4)
+    from lightwood.encoders.time_series.helpers.test_ts import train_and_eval
 
-        start = np.random.randint(30)
-        vec = [start+j*skip for j in range(length)]
+    # set log, fix random seed for reproducibility
+    logging.basicConfig(level=logging.DEBUG)
+    seed = 42
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if 'cuda' in str(get_devices()[0]):
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
 
-        series+=[' '.join([str(x) for x in vec])]
+    # test: overfit single multi dimensional time series
+    logging.info(" [Test] Testing multi-dimensional time series encoding")
+    series = [[['1 2 3 4 5 6'], ['2 3 4 5 6 7'], ['3 4 5 6 7 8'], ['4 5 6 7 8 9']]]
 
-    encoder = RnnEncoder(encoded_vector_size=3,train_iters=10)
-    encoder.prepare_encoder(series, feedback_hoop_function=lambda x:print(x))
+    data = 100 * series
+    n_dims = max([len(q) for q in data])
 
+    queries = [[['1 2 3'], ['2 3 4'], ['3 4 5'], ['4 5 6']]]
+    answers = [[4, 5, 6, 7]]
 
-    # test de decoder
-    init_vector = ['31 33 35 37', '1 2 3 4 5 6']
+    params = {'answers': answers,
+              'hidden_size': 10,
+              'batch_size': 1,
+              'dropout': 0.1,
+              'ts_n_dims': n_dims,
+              'train_iters': 15,
+              'margin': 0.25,  # error tolerance
+              'feedback_fn': lambda x: logging.info(x),
+              'pred_qty': 1}
 
-    print(encoder.encode(column_data=init_vector, get_next_count=2))
-    print(encoder.encode(column_data=init_vector))
+    train_and_eval(data, queries, params)
