232e253178d56b2ee815e7e1aebd40cbaafffa57
Patricio Cerda Mardini <pcerdam@uc.cl>
2020-07-14 17:01:30 -0400

    added challenge submission

---
+++ lightwood/encoders/time_series/helpers/test_ts.py
@@ -0,0 +1,64 @@
+import logging
+
+from lightwood.encoders.time_series.rnn import RnnEncoder
+
+
+def train_and_eval(data, queries, params):
+    """ Minimal training and testing suite to unit test time series encoder-decoder.
+    :param data: list of multi-dimensional time series [[data_dim1], ...] to train the model with.
+                 For now, data_dim is a string of integers/floats separated by white space (e.g. "1 2 3 4")
+    :param queries: list of multi-dimensional time series to encode, predict, and decode with.
+    :param params: dictionary with various configuration parameters.
+    :return:
+    """
+    forecasts, decoded = list(), list()
+    encoder = RnnEncoder(encoded_vector_size=params['hidden_size'],
+                         train_iters=params['train_iters'],
+                         ts_n_dims=params['ts_n_dims'],
+                         dropout=params['dropout'])
+    encoder.prepare_encoder(data, feedback_hoop_function=params['feedback_fn'], batch_size=params['batch_size'])
+
+    # predict and decode
+    for query in queries:
+        encoded_data, preds = encoder.encode([query], get_next_count=params['pred_qty'])
+        decoded_data = encoder.decode(encoded_data).tolist()[0]
+
+        # shape preds to (timesteps, n_dims)
+        preds = preds.squeeze()
+        if len(list(preds.shape)) == 1:
+            preds = preds.unsqueeze(dim=0)
+
+        forecasts.append(preds.tolist())
+        decoded.append(decoded_data)
+
+    # evaluate query error
+    for i in range(len(queries)):
+        logging.info("\t[Query {}]".format(i))
+        query = queries[i]
+        preds = forecasts[i]
+        dec = [elt[0:len(query[0][0].split(" "))] for elt in decoded[i]]  # truncate to original query length
+        answers = params['answers'][i]
+        aggregate = [preds, dec]
+
+        # check prediction
+        pred_test = True
+        for answer, pred in zip(answers, preds[-1]):  # compare last predicted timestep with answer
+            if abs(pred - answer) > params['margin']:
+                pred_test = False
+                break
+
+        # check reconstruction
+        dec_test = True
+        for q, v in zip(query, dec):
+            fq = map(float, q[0].split(" "))
+            for p, t in zip(fq, v):
+                if abs(p - t) > params['margin']:
+                    dec_test = False
+                    break
+
+        tests = [pred_test, dec_test]
+        tests_info = ['Prediction for query [{}]:'.format(query), 'Reconstruction for query [{}]: '.format(query)]
+
+        for info, value, passed in zip(tests_info, aggregate, tests):
+            logging.info("{}[{}] {}\n{}{}".format("\t" * 2, "Pass" if passed else "Fail", info, "\t" * 6,
+                                                  [[round(elt, 2) for elt in dimn] for dimn in value]))
