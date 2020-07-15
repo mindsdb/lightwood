53a6c56eca8285a4c67b01b63c4fa30140ddad40
Patricio Cerda Mardini <pcerdam@uc.cl>
2020-07-15 16:52:17 -0400

    fix docstrings

--- lightwood/encoders/time_series/helpers/test_ts.py
+++ lightwood/encoders/time_series/helpers/test_ts.py
@@ -1,12 +1,14 @@
 import logging
-from lightwood.encoders.time_series.rnn import RnnEncoder
+import torch
 
 
 def test_ts(encoder, queries, answers, params):
     """ Minimal testing suite for time series encoder-decoder.
     :param encoder: RnnEncoder instance to test
     :param queries: list of multi-dimensional time series to encode, predict, and decode with
-                    [[[data_ts1_dim1], [data_ts1_dim2]], ...]
+                    [[[data_ts1_dim_1], [data_ts1_dim_2]], ...] with data_ts_dim_i a string
+    :param answers: list of correct predictions for each query [[ans_query_1], ...], where
+                    ans_query_i is a list of numbers
     :param params: dictionary with configuration parameters
     :return:
     """
@@ -15,13 +17,12 @@ def test_ts(encoder, queries, answers, params):
     # predict and decode
     for query in queries:
         encoded_data, preds = encoder.encode([query], get_next_count=params['pred_qty'])
-        decoded_data = encoder.decode(encoded_data).tolist()[0]
+        decoded_data = encoder.decode(encoded_data).tolist()
 
-        # shape preds to (timesteps, n_dims)
-        preds = preds.squeeze()
-        if len(list(preds.shape)) == 1:
-            preds = preds.unsqueeze(dim=0)
+        if params['ts_n_dims'] > 1:
+            decoded_data = decoded_data[0]
 
+        preds = torch.reshape(preds, (1, params['ts_n_dims']))
         forecasts.append(preds.tolist())
         decoded.append(decoded_data)
 
