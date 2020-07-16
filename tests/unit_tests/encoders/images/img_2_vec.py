4c3f49cac1c94e8574c39e276520cee89a2f3b89
George3d6 <george@cerebralab.com>
2020-07-17 02:44:25 +0300

    simplified image encoder and added some more proper testing, supporting PNG and grayscale

--- tests/unit_tests/encoders/images/img_2_vec.py
+++ tests/unit_tests/encoders/images/img_2_vec.py
@@ -6,12 +6,16 @@ class TestDataSource(unittest.TestCase):
 
     def test_encoding(self):
         # Just some randoms url from imgur's frontpage
+        # @TODO: See how this behaves for SVGs
         images = ['https://i.imgur.com/PznpPOY.png', 'https://i.imgur.com/B08g7Vk.jpg', 'https://i.imgur.com/WGnlMgh.jpg']
 
         encoder = Img2VecEncoder()
         encoder.prepare_encoder([])
 
         ret = encoder.encode(images)
+
+        assert len(ret.shape) == 2
+        assert ret.shape[0] == len(images)
 
 if __name__ == '__main__':
     unittest.main(failfast=True)
