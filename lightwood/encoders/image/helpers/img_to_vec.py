280b914b7e2d8bd9fdbf477e79beabffd891cb90
George3d6 <george@cerebralab.com>
2020-07-19 02:20:28 +0300

    stashing

--- lightwood/encoders/image/helpers/img_to_vec.py
+++ lightwood/encoders/image/helpers/img_to_vec.py
@@ -16,7 +16,7 @@ class ChannelPoolAdaptiveAvg1d(torch.nn.AdaptiveAvgPool1d):
 
 class Img2Vec(nn.Module):
 
-    def __init__(self, model, layer='default', layer_output_size=512):
+    def __init__(self, model, layer='default'):
         """ Img2Vec
         :param cuda: If set to True, will run forward pass on GPU
         :param model: String name of requested model
@@ -26,11 +26,12 @@ class Img2Vec(nn.Module):
         super(Img2Vec, self).__init__()
 
         self.device, _ = get_devices()
-        self.layer_output_size = layer_output_size
         self.model_name = model
 
-        self.model = self._get_model_and_layer(model, layer)
-        self.model = self.model.to(self.device).train()
+        self.model_name = 'resnet-18' # Debugging
+
+        self.model = self._get_model(layer)
+        self.model = self.model.to(self.device)
 
 
     def to(self, device, available_devices):
@@ -51,22 +52,22 @@ class Img2Vec(nn.Module):
             return embedding[0, :, 0, 0]
 
 
-    def _get_model_and_layer(self, model_name, layer):
+    def _get_model(self, layer):
         """ Internal method for getting layer from model
         :param model_name: model name such as 'resnet-18'
         :param layer: layer as a string for resnet-18 or int for alexnet
         :returns: pytorch model, selected layer
         """
 
-        if model_name == 'resnet-18':
+        if self.model_name == 'resnet-18':
             self.layer_output_size = 512
             model = torch.nn.Sequential(*list(models.resnet18(pretrained=True).children())[:-1])
 
-        elif model_name == 'resnext-50-small':
+        elif self.model_name == 'resnext-50-small':
             self.layer_output_size = 512
             model = torch.nn.Sequential(*list(models.resnext50_32x4d(pretrained=True).children())[:-1] , ChannelPoolAdaptiveAvg1d(output_size=512))
 
-        elif model_name == 'resnext-50':
+        elif self.model_name == 'resnext-50':
             self.layer_output_size = 2048
             model = torch.nn.Sequential(*list(models.resnext50_32x4d(pretrained=True).children())[:-1])
 
