d250c122e6cf2efcfd7fd8ceb8180cf2d6c641f7
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2020-07-21 11:28:57 -0400

    tests now pass + cleanup

--- tests/unit_tests/encoders/time_series/test_rnn.py
+++ tests/unit_tests/encoders/time_series/test_rnn.py
@@ -9,99 +9,58 @@ from lightwood.encoders.time_series.helpers.rnn_helpers import tensor_from_serie
 
 class TestRnnEncoder(unittest.TestCase):
 
-
     def test_padding(self):
         series = [['1', '2', '3 '], ['2', '3'], ['3', '4', '5', '6'], [' 4', '5', '6']]
         target = [[1.0, 2.0, 3.0, 4.0, 0.0], [2.0, 3.0, 4.0, 5.0, 0.0], [3.0, 0.0, 5.0, 6.0, 0.0]]
         result = tensor_from_series(series, get_devices()[0], n_dims=5, pad_value=0.0, max_len=3).tolist()[0]
-        assert result == target
+        self.assertEqual(result, target)
 
     def test_overfit_multidimensional(self):
         series = [[['1', '2', '3', '4', '5', '6'], ['2', '3', '4', '5', '6', '7'],
                    ['3', '4', '5', '6', '7', '8'], ['4', '5', '6', '7', '8', '9']]]
         data = 20 * series
         n_dims = max([len(q) for q in data])
+        timesteps = max([len(q[0]) for q in data])
+        batch_size = 1
 
-        params = {'max_ts': 6,
-                  'hidden_size': 10,
-                  'batch_size': 1,
-                  'dropout': 0.1,
-                  'ts_n_dims': n_dims,
-                  'train_iters': 400,
-                  'margin': 0.05,  # error tolerance
-                  'feedback_fn': lambda x: print(x),
-                  'pred_qty': 1}
-
-        encoder = RnnEncoder(encoded_vector_size=params['hidden_size'], train_iters=params['train_iters'],
-                             ts_n_dims=params['ts_n_dims'], max_timesteps=params['max_ts'])
-
-        encoder.prepare_encoder(data, feedback_hoop_function=params['feedback_fn'], batch_size=params['batch_size'])
+        encoder = RnnEncoder(encoded_vector_size=10, train_iters=400, ts_n_dims=n_dims, max_timesteps=timesteps)
+        encoder.prepare_encoder(data, feedback_hoop_function=lambda x: print(x), batch_size=batch_size)
         encoded = encoder.encode(data)
-        decoded = encoder.decode(encoded)
+        decoded = encoder.decode(encoded, steps=timesteps)
 
         equal = 0
         unequal = 0
-        assert len(data) == len(decoded)
-        assert len(data[0]) == len(decoded[0])
+        self.assertEqual(len(data), len(decoded))
+        self.assertEqual(len(data[0]), len(decoded[0]))
         for i in range(len(data[0])):
-            # Doesn't work, shouldn't it ?
-            #assert len(data[0][i]) == len(decoded[0][i])
+            self.assertEqual(len(data[0][i]), len(decoded[0][i]))
             for n in range(len(data[0][i])):
                 if int(decoded[0][i][n]) == int(data[0][i][n]):
                     equal += 1
                 else:
                     unequal += 1
+
         print(f'Decoder got {equal} correct and {unequal} incorrect')
-        # Not much else we can do here, it trains slowly, it's the nature of the type of network and travis is really slow
-        assert equal > unequal
+        # Not much else we can do here, trains slowly, it's the nature of the type of network and travis is really slow
+        self.assertGreater(equal, unequal)
 
-        queries = [[['1', '2', '3'], ['2', '3', '4'], ['3', '4', '5'], ['4', '5', '6']]]
-        answers = [[4, 5, 6, 7]]
+        query = [[['1', '2', '3'], ['2', '3', '4'], ['3', '4', '5'], ['4', '5', '6']]]
+        answer = [4, 5, 6, 7]
+        error_margin = 1
+        encoded_data, preds = encoder.encode(query, get_next_count=1)
+        decoded_data = encoder.decode(encoded_data, steps=len(query[0][0])).tolist()
 
-        forecasts, decoded = list(), list()
+        # check prediction
+        preds = torch.reshape(preds, (1, n_dims)).tolist()[-1]
+        for ans, pred in zip(answer, preds):
+            self.assertGreater(error_margin, abs(pred - ans))
 
-        # predict and decode
-        for query in queries:
-            encoded_data, preds = encoder.encode([query], get_next_count=params['pred_qty'])
-            decoded_data = encoder.decode(encoded_data).tolist()
+        # check reconstruction
+        float_query = [list(map(float, q)) for q in query[0]]
+        for qry, dec in zip(float_query, decoded_data[0]):
+            for truth, pred in zip(qry, dec):
+                self.assertGreater(error_margin, abs(truth - pred))
 
-            if params['ts_n_dims'] > 1:
-                decoded_data = decoded_data[0]
-
-            preds = torch.reshape(preds, (1, params['ts_n_dims']))
-            forecasts.append(preds.tolist())
-            decoded.append(decoded_data)
-
-        # evaluate query error
-        for i in range(len(queries)):
-            query = queries[i]
-            preds = forecasts[i]
-            dec = [elt[0:len(query[0][0].split(" "))] for elt in decoded[i]]  # truncate to original query length
-            ans = answers[i]
-            aggregate = [preds, dec]
-
-            # check prediction
-            pred_test = True
-            for answer, pred in zip(ans, preds[-1]):  # compare last predicted timestep with answer
-                if abs(pred - answer) > params['margin']:
-                    pred_test = False
-                    break
-
-            # check reconstruction
-            dec_test = True
-            for q, v in zip(query, dec):
-                fq = map(float, q[0].split(" "))
-                for p, t in zip(fq, v):
-                    if abs(p - t) > params['margin']:
-                        dec_test = False
-                        break
-
-            tests = [pred_test, dec_test]
-            tests_info = ['Prediction for query [{}]:'.format(query), 'Reconstruction for query [{}]: '.format(query)]
-
-            for info, value, passed in zip(tests_info, aggregate, tests):
-                print("{}[{}] {}\n{}{}".format("\t" * 2, "Pass" if passed else "Fail", info, "\t" * 6,
-                                                      [[round(elt, 2) for elt in dimn] for dimn in value]))
 
 if __name__ == '__main__':
     unittest.main(failfast=True)
