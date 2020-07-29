bb8d8ba6259a89e9514b8d6f7dc64cda30a7cdab
George3d6 <george@cerebralab.com>
2020-07-29 20:20:35 +0300

    finished vocab encoder

--- lightwood/encoders/text/vocab.py
+++ lightwood/encoders/text/vocab.py
@@ -11,7 +11,6 @@ class VocabularyEncoder(BaseEncoder):
         self._tokenizer_class = DistilBertTokenizer
         self._pretrained_model_name = 'distilbert-base-uncased'
         self._max_len = None
-        self._pytorch_wrapper = torch.FloatTensor
 
     def prepare_encoder(self, priming_data):
         self._max_len = max([len(x) for x in priming_data])
@@ -22,8 +21,7 @@ class VocabularyEncoder(BaseEncoder):
         vec = []
         for text in column_data:
             encoded = self._tokenizer.encode(text[:self._max_len], add_special_tokens=True)
-            encoded = torch.tensor(encoded + [self._pad_id * (self._max_len - len(encoded))])
-            print(encoded)
+            encoded = torch.tensor(encoded + [self._pad_id] * (self._max_len - len(encoded)))
             vec.append(encoded)
         return torch.stack(vec)
 
