e5a16c511bac36c6b6cfbe00c715d6dd5902f8d2
George3d6 <george@cerebralab.com>
2020-08-03 12:39:28 +0300

    test renaming and added empty vocab encoder test

---
+++ tests/unit_tests/encoders/text/test_vocab.py
@@ -0,0 +1,10 @@
+import unittest
+
+from lightwood.encoders.text.short import VocabularyEncoder
+
+
+class TestVocabularyEncoder(unittest.TestCase):
+
+
+if __name__ == '__main__':
+    unittest.main()
