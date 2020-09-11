87f698e0dae7aa103cbb0e4d7c80ee4abe69ed89
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2020-09-11 18:16:01 -0300

    input normalization v1

--- lightwood/encoders/time_series/helpers/rnn_helpers.py
+++ lightwood/encoders/time_series/helpers/rnn_helpers.py
@@ -2,23 +2,14 @@ import logging
 
 import torch
 import torch.nn as nn
+import numpy as np
 
 
-def tensor_from_series(series, device, n_dims, pad_value, max_len):
-    """
-    :param series: list of lists, corresponds to time series: [[x1_1, ..., x1_n], [x2_1, ..., x2_n], ...]
-                   the series is zero-padded on each axis so that all dimensions have equal length
-    :param device: computing device that PyTorch backend uses
-    :param n_dims: will zero-pad dimensions until series_dimensions == n_dims
-    :param pad_value: value to pad each dimension in the time series, if needed
-    :param max_len: length to pad or truncate each time_series
-    :return: series as a tensor ready for model consumption, shape (1, ts_length, n_dims)
-    """
+def float_matrix_from_strlist(series):
     # edge case for when series == "dim 1 data"
-    if type(series) != type([]):
+    if not isinstance(series, list):
         series = [str(series).split(' ')]
 
-    # conversion to float
     float_series = []
     for dimn in series:
         dimn_series = []
@@ -29,6 +20,21 @@ def tensor_from_series(series, device, n_dims, pad_value, max_len):
                 logging.warning(f'Weird element encountered in timeseries: {ele} !')
                 dimn_series.append(0)
         float_series.append(dimn_series)
+    return float_series
+
+
+def tensor_from_series(series, device, n_dims, pad_value, max_len, normalizer=None):
+    """
+    :param series: list of lists, corresponds to time series: [[x1_1, ..., x1_n], [x2_1, ..., x2_n], ...]
+                   the series is zero-padded on each axis so that all dimensions have equal length
+    :param device: computing device that PyTorch backend uses
+    :param n_dims: will zero-pad dimensions until series_dimensions == n_dims
+    :param pad_value: value to pad each dimension in the time series, if needed
+    :param max_len: length to pad or truncate each time_series
+    :return: series as a tensor ready for model consumption, shape (1, ts_length, n_dims)
+    """
+    # conversion to float
+    float_series = float_matrix_from_strlist(series)
 
     # timestep padding and truncating
     for i in range(len(float_series)):
@@ -40,6 +46,9 @@ def tensor_from_series(series, device, n_dims, pad_value, max_len):
     for _ in range(max(0, n_dims - len(float_series))):
         float_series.append([pad_value] * max_len)
 
+    # normalize and transpose
+    if normalizer:
+        float_series = normalizer.transform(np.array(float_series))
     tensor = torch.transpose(torch.tensor(float_series, dtype=torch.float, device=device), 0, 1)
 
     # add batch dimension
@@ -82,3 +91,34 @@ class EncoderRNNNumerical(nn.Module):
 
     def initHidden(self, device):
         return torch.zeros(1, 1, self.hidden_size, device=device)
+
+
+class TanhNormalizer:
+    """ Ref: https://stackoverflow.com/questions/43061120/tanh-estimator-normalization-in-python """
+    def __init__(self):
+        self.mu = None
+        self.std = None
+
+    def fit(self, x):
+        if isinstance(x, list) and isinstance(x[0], str):
+            x = [float_matrix_from_strlist(i) for i in x]
+
+        self.mu = np.mean(x, dtype=np.float64)
+        self.std = np.std(x, dtype=np.float64)  # might .mul() by constant for better generalization
+
+    def transform(self, x):
+        output = 0.5 * (np.tanh(0.01 * (x - self.mu) / self.std) + 1)
+        eps = np.finfo(float).eps
+        output = np.where(output == 0, eps, output)  # bound encodings == 0 or 1 to avoid error with arctanh
+        output = np.where(output == 1, 1.0-eps, output)
+        return output
+
+    def fit_transform(self, x):
+        self.fit(x)
+        return self.transform(x)
+
+    def inverse_transform(self, y):
+        return self.mu + (100 * self.std * np.arctanh(2*y - 1))
+
+    def inverse_transform_tensor(self, y):
+        return self.mu + (100 * self.std * torch.atanh(2*y - 1))
