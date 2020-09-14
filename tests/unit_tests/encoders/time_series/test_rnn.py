545c3297137f3c808b1d84d5878b3f9f0f979518
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2020-09-14 16:38:45 -0300

    added single dimensional test

--- tests/unit_tests/encoders/time_series/test_rnn.py
+++ tests/unit_tests/encoders/time_series/test_rnn.py
@@ -25,11 +25,60 @@ class TestRnnEncoder(unittest.TestCase):
         reconstructed = normalizer.inverse_transform(normalizer.fit_transform(data))
         self.assertTrue(np.allclose(data, reconstructed, atol=0.1))
 
+    def test_overfit_singledimensional(self):
+        series = ['1 2 3 4 5', '2 3 4 5 6', '3 4 5 6 7', '4 5 6 7 8']  # format given by native
+        data = series * 20
+        batch_size = 32
+        timesteps = 5
+        n_dims = 1
+
+        encoder = RnnEncoder(encoded_vector_size=10, train_iters=100, ts_n_dims=n_dims)
+        encoder.prepare_encoder(data, feedback_hoop_function=lambda x: print(x), batch_size=batch_size)
+        encoded = encoder.encode(data)
+        decoded = encoder.decode(encoded, steps=timesteps).tolist()
+
+        unequal = 0
+        equal = 0
+        data = float_matrix_from_strlist(data)
+
+        # [print([round(dd,2) for dd in d]) for d in decoded]
+        # [print(d) for d in data]
+
+        self.assertEqual(len(data), len(decoded))
+        self.assertEqual(len(data[0]), len(decoded[0]))
+        for i in range(len(data)):
+            for n in range(timesteps):
+                if int(decoded[i][n]) == int(data[i][n]):
+                    equal += 1
+                else:
+                    unequal += 1
+
+        print(f'Decoder got {equal} correct and {unequal} incorrect')
+        # Not much else we can do here, trains slowly, it's the nature of the type of network and travis is really slow
+        self.assertGreater(equal, unequal)
+
+        query = ['1 2 3', '2 3 4', '3 4 5', '4 5 6']
+        answer = [4, 5, 6, 7]
+        error_margin = 1
+        encoded_data, preds = encoder.encode(query, get_next_count=1)
+        decoded_data = encoder.decode(encoded_data, steps=len(query[0][0])).tolist()
+
+        # check prediction
+        preds = preds.squeeze().tolist()
+        for ans, pred in zip(answer, preds):
+            self.assertGreater(error_margin, abs(pred - ans))
+
+        # check reconstruction
+        float_query = float_matrix_from_strlist(query)
+        for qry, dec in zip(float_query, decoded_data):
+            for truth, pred in zip(qry, dec):
+                self.assertGreater(error_margin, abs(truth - pred))
+
     def test_overfit_multidimensional(self):
         pass
         # series = [[['1', '2', '3', '4', '5', '6'], ['2', '3', '4', '5', '6', '7'],
         #            ['3', '4', '5', '6', '7', '8'], ['4', '5', '6', '7', '8', '9']]]
-        # data = 20 * series
+        # data = series * 20
         # n_dims = max([len(q) for q in data])
         # timesteps = max([len(q[0]) for q in data])
         # batch_size = 1
@@ -38,7 +87,6 @@ class TestRnnEncoder(unittest.TestCase):
         # encoder.prepare_encoder(data, feedback_hoop_function=lambda x: print(x), batch_size=batch_size)
         # encoded = encoder.encode(data)
         # decoded = encoder.decode(encoded, steps=timesteps)
-
         # equal = 0
         # unequal = 0
         # self.assertEqual(len(data), len(decoded))
@@ -50,22 +98,18 @@ class TestRnnEncoder(unittest.TestCase):
         #             equal += 1
         #         else:
         #             unequal += 1
-
         # print(f'Decoder got {equal} correct and {unequal} incorrect')
         # # Not much else we can do here, trains slowly, it's the nature of the type of network and travis is really slow
         # self.assertGreater(equal, unequal)
-
         # query = [[['1', '2', '3'], ['2', '3', '4'], ['3', '4', '5'], ['4', '5', '6']]]
         # answer = [4, 5, 6, 7]
         # error_margin = 1
         # encoded_data, preds = encoder.encode(query, get_next_count=1)
         # decoded_data = encoder.decode(encoded_data, steps=len(query[0][0])).tolist()
-
         # # check prediction
         # preds = torch.reshape(preds, (1, n_dims)).tolist()[-1]
         # for ans, pred in zip(answer, preds):
         #     self.assertGreater(error_margin, abs(pred - ans))
-
         # # check reconstruction
         # float_query = [list(map(float, q)) for q in query[0]]
         # for qry, dec in zip(float_query, decoded_data[0]):
