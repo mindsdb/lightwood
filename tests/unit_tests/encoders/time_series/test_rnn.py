d2aa2f18eb9b68f46b131830c26ed8701d411b56
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2020-09-15 17:24:20 -0300

    unified tests

--- tests/unit_tests/encoders/time_series/test_rnn.py
+++ tests/unit_tests/encoders/time_series/test_rnn.py
@@ -1,7 +1,5 @@
 import unittest
 
-import torch
-
 from lightwood.helpers.device import get_devices
 from lightwood.encoders.time_series import RnnEncoder
 from lightwood.encoders.time_series.helpers.rnn_helpers import *
@@ -25,92 +23,68 @@ class TestRnnEncoder(unittest.TestCase):
         reconstructed = normalizer.inverse_transform(normalizer.fit_transform(data))
         self.assertTrue(np.allclose(data, reconstructed, atol=0.1))
 
-    def test_overfit_singledimensional(self):
-        series = [[[1, 2, 3, 4, 5]], [[2, 3, 4, 5, 6]], [[3, 4, 5, 6, 7]], [[4, 5, 6, 7, 8]]]
-        data = series * 20
-        batch_size = 1
-        n_dims = max([len(q) for q in data])
-        timesteps = max([len(q[0]) for q in data])
+    def test_overfit(self):
+        single_dim_ts = [[[1, 2, 3, 4, 5]],
+                     [[2, 3, 4, 5, 6]],
+                     [[3, 4, 5, 6, 7]],
+                     [[4, 5, 6, 7, 8]]]
 
-        encoder = RnnEncoder(encoded_vector_size=15, train_iters=50, ts_n_dims=n_dims)
-        encoder.prepare_encoder(data, feedback_hoop_function=lambda x: print(x), batch_size=batch_size)
-        encoded = encoder.encode(data)
-        decoded = encoder.decode(encoded, steps=timesteps).tolist()
+        multi_dim_ts = [[[1, 2, 3, 4, 5, 6],
+                         [2, 3, 4, 5, 6, 7],
+                         [3, 4, 5, 6, 7, 8],
+                         [4, 5, 6, 7, 8, 9]]]
 
-        unequal = 0
-        equal = 0
+        single_qry_ans = ([[[1, 2, 3]], [[2, 3, 4]], [[3, 4, 5]], [[4, 5, 6]]],  # query
+                          [4, 5, 6, 7])                                          # answer
 
-        self.assertEqual(len(data), len(decoded))
-        self.assertEqual(len(data[0]), len(decoded[0]))
-        for i in range(len(data)):
-            for n in range(timesteps):
-                if round(decoded[i][0][n], 0) == round(data[i][0][n], 0):
-                    equal += 1
-                else:
-                    unequal += 1
+        multi_qry_ans = ([[[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6]]],  # query
+                             [4, 5, 6, 7])                                # answer
 
-        print(f'Decoder got {equal} correct and {unequal} incorrect')
-        # Not much else we can do here, trains slowly, it's the nature of the type of network and travis is really slow
-        self.assertGreater(equal, unequal)
+        for series, example in zip([single_dim_ts, multi_dim_ts], [single_qry_ans, multi_qry_ans]):
 
-        query = [[[1, 2, 3]], [[2, 3, 4]], [[3, 4, 5]], [[4, 5, 6]]]
-        answer = [4, 5, 6, 7]
-        error_margin = 1
-        encoded_data, preds = encoder.encode(query, get_next_count=1)
-        decoded_data = encoder.decode(encoded_data, steps=4).squeeze(1).tolist()
+            data = series * 100
+            n_dims = max([len(q) for q in data])
+            timesteps = max([len(q[0]) for q in data])
+            batch_size = 1
 
-        # check prediction
-        preds = preds.squeeze().tolist()
+            encoder = RnnEncoder(encoded_vector_size=15, train_iters=10, ts_n_dims=n_dims)
+            encoder.prepare_encoder(data, feedback_hoop_function=lambda x: print(x), batch_size=batch_size)
+            encoded = encoder.encode(data)
+            decoded = encoder.decode(encoded, steps=timesteps).tolist()
 
-        for ans, pred in zip(answer, preds):
-            self.assertGreater(error_margin, abs(pred - ans))
+            equal = 0
+            unequal = 0
+            self.assertEqual(len(data), len(decoded))
+            self.assertEqual(len(data[0]), len(decoded[0]))
+            self.assertEqual(len(data[0][0]), len(decoded[0][0]))
 
-        # check reconstruction
-        for qry, dec in zip(query[0], decoded_data):
-            for truth, pred in zip(qry, dec):
-                self.assertGreater(error_margin, abs(truth - pred))
+            for i in range(len(data)):
+                for d in range(n_dims):
+                    for t in range(timesteps):
+                        if round(decoded[i][d][t], 0) == round(data[i][d][t], 0):
+                            equal += 1
+                        else:
+                            unequal += 1
 
-    def test_overfit_multidimensional(self):
-        series = [[[1, 2, 3, 4, 5, 6], [2, 3, 4, 5, 6, 7], [3, 4, 5, 6, 7, 8], [4, 5, 6, 7, 8, 9]]]
-        data = 20 * series
-        n_dims = max([len(q) for q in data])
-        timesteps = max([len(q[0]) for q in data])
-        batch_size = 1
+            print(f'Decoder got {equal} correct and {unequal} incorrect')
+            self.assertGreaterEqual(equal, unequal)
 
-        encoder = RnnEncoder(encoded_vector_size=10, train_iters=150, ts_n_dims=n_dims)
-        encoder.prepare_encoder(data, feedback_hoop_function=lambda x: print(x), batch_size=batch_size)
-        encoded = encoder.encode(data)
-        decoded = encoder.decode(encoded, steps=timesteps).tolist()
+            error_margin = 1
+            query, answer = example
+            encoded_data, preds = encoder.encode(query, get_next_count=1)
+            decoded_data = encoder.decode(encoded_data, steps=len(query[0][0])).tolist()
 
-        equal = 0
-        unequal = 0
-        self.assertEqual(len(data), len(decoded))
-        self.assertEqual(len(data[0]), len(decoded[0]))
-        for i in range(len(data[0])):
-            self.assertEqual(len(data[0][i]), len(decoded[0][i]))
-            for n in range(len(data[0][i])):
-                if round(decoded[0][i][n], 0) == round(data[0][i][n], 0):
-                    equal += 1
-                else:
-                    unequal += 1
+            # check prediction
+            if len(data[0]) > 1:
+                preds = torch.reshape(preds, (1, n_dims)).tolist()[-1]
+            else:
+                preds = preds.squeeze().tolist()
 
-        print(f'Decoder got {equal} correct and {unequal} incorrect')
-        # Not much else we can do here, trains slowly, it's the nature of the type of network and travis is really slow
-        self.assertGreaterEqual(equal, unequal)
+            for ans, pred in zip(answer, preds):
+                self.assertGreater(error_margin, abs(pred - ans))
 
-        query = [[[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6]]]
-        answer = [4, 5, 6, 7]
-        error_margin = 1
-        encoded_data, preds = encoder.encode(query, get_next_count=1)
-        decoded_data = encoder.decode(encoded_data, steps=len(query[0][0])).tolist()
-
-        # check prediction
-        preds = torch.reshape(preds, (1, n_dims)).tolist()[-1]
-        for ans, pred in zip(answer, preds):
-            self.assertGreater(error_margin, abs(pred - ans))
-
-        # check reconstruction
-        float_query = [list(map(float, q)) for q in query[0]]
-        for qry, dec in zip(float_query, decoded_data[0]):
-            for truth, pred in zip(qry, dec):
-                self.assertGreater(error_margin, abs(truth - pred))
+            # check reconstruction
+            float_query = [list(map(float, q)) for q in query[0]]
+            for qry, dec in zip(float_query, decoded_data[0]):
+                for truth, pred in zip(qry, dec):
+                    self.assertGreater(error_margin, abs(truth - pred))
