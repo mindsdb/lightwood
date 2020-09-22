d39d453842160fa670e7c3b0108bf07b0b12b3ae
George3d6 <george@cerebralab.com>
2020-09-22 12:34:02 +0300

    remove cesium and tsfresh

--- lightwood/encoders/time_series/__init__.py
+++ lightwood/encoders/time_series/__init__.py
@@ -1,7 +1,11 @@
-from lightwood.encoders.time_series.ts_fresh_ts import TsFreshTsEncoder
 from lightwood.encoders.time_series.rnn import RnnEncoder
 
 # Optional encoders
+try:
+    from lightwood.encoders.time_series.ts_fresh_ts import TsFreshTsEncoder
+except:
+    pass
+
 try:
     from lightwood.encoders.time_series.cesium_ts import CesiumTsEncoder
     CesiumTsEncoder = CesiumTsEncoder
