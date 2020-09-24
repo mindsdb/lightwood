f884cc9f81dc4e4c6690b338add424af42ac5f05
George3d6 <george@cerebralab.com>
2020-09-24 12:40:09 +0300

    clearing forward net

--- lightwood/api/predictor.py
+++ lightwood/api/predictor.py
@@ -219,7 +219,7 @@ class Predictor:
             # Null out certain object we don't want to store
             if hasattr(self._mixer, '_nonpersistent'):
                 self._mixer._nonpersistent = {}
-            self._mixer.net.self._foward_net = None
+            self._mixer.net._foward_net = None
 
             # Dump everything relevant to cpu before saving
             self.convert_to_device("cpu")
