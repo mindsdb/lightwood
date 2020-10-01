73c22d2abba3313d242eca8403d41d9aa4910173
maximlopin <wzzhz13@gmail.com>
2020-10-01 15:56:36 +0300

    return boost mixer

--- lightwood/mixers/__init__.py
+++ lightwood/mixers/__init__.py
@@ -1,4 +1,4 @@
 from lightwood.mixers.base_mixer import BaseMixer
 from lightwood.mixers.nn import NnMixer
-#from lightwood.mixers.boost import BoostMixer
+from lightwood.mixers.boost import BoostMixer
 from lightwood.mixers.sklearn_mixer import SklearnMixer
