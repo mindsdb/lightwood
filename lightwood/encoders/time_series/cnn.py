f59898644524c479c31620122aa0d415a3d6adba
walexand3r <wadh5182@eng.ox.ac.uk>
2020-11-10 21:39:33 +0000

    Added docs in cnn.py

--- lightwood/encoders/time_series/cnn.py
+++ lightwood/encoders/time_series/cnn.py
@@ -8,8 +8,44 @@ from lightwood.helpers.device import get_devices
 from lightwood.encoders.encoder_base import BaseEncoder
 
 """ 
+Causal 1D CAE using dilated convolutions loosely based on: https://arxiv.org/pdf/1611.05267.pdf
+https://arxiv.org/pdf/1803.01271.pdf used for initial parameters as similar task. 
+Overall idea is similar to a simplified WaveNet. 
+
+Encoder is made up of a series of Temporal Convolution blocks - typical ResNet units with 
+zero padding of front of tensor to ensure causality.
+Residual connection 1x1 convolved to ensure it matches shape of block output
+Dilation d*i at block i to increase receptive field
+Maxpool halves sequence length after each block
+Unable to use pretrained standard ResNet as it requires square input and is not causal.
+
+Size of network depends on 'blocks' variable (list):
+length = number of temporal convolution blocks, 
+each element refers to number of filters applied during each convolution
+
+Shape of encoded tensor:
+(blocks[-1] , original max sequence length / 2^(len(blocks)))
+Hence user can alter output size depending on application by tuning blocks variable
+Fully connected layers could instead be used at ouput as in 
+https://github.com/hsinyilin19/ResNetVAE, to set encoded length to desired value more easily.
+
+Decoder works in reverse, dilation decreases with each block, interpolate/ upsample is used to 
+restore to original length.
+
 For now, AE encodes input and just aims to reproduce it. 
 This could be adapted to produce n future predictions by training with a sliding window method 
+
+
+In conclusion, this simple model has its merits.
+the temporal dilated convolutions are necessary, and being able to scale depth and 
+complexity of the model depending on the task is useful.
+However, this process requires significant trial and error to ensure encoded output
+is sensible shape etc. It could make more sense to have a large chunk at the start 
+which purely extracts features with limited downsampling, then adjusting its output down 
+to required latent size using fully connected layers. Making use of a deeper ResNet 
+structure (current model has only 8 convolutional layers in encoder compared to 18 in
+ResNet-18) is an option which would not require much adjustment. A complete WaveNet is 
+another option but has not been applied as an autoencoder.
 """
 
 class CnnEncoder(BaseEncoder):
@@ -17,14 +53,14 @@ class CnnEncoder(BaseEncoder):
                  is_target=False, ts_n_dims=1):
         super().__init__(is_target)
         self.device, _ = get_devices()
-        self._n_dims = ts_n_dims  # expected dimensionality of time series
+        self._n_dims = ts_n_dims     # expected dimensionality of time series
         self._max_ts_length = 0
-        self._sos = 0.0  # start of sequence for decoding
-        self._eos = 0.0  # end of input sequence -- padding value for batches
+        self._sos = 0.0              # start of sequence for decoding
+        self._eos = 0.0              # end of input sequence -- padding value for batches
         self._stop_on_error = stop_on_error
         self._learning_rate = learning_rate
         self._kernel_size = kernel_size
-        self._blocks = blocks # user defined array giving nodes in each layer (last element = encoded size)
+        self._blocks = blocks        # user defined array giving nodes in each layer (last element = encoded size)
         self._batch_size = batch_size
         self._train_epochs = train_epochs
         self._pytorch_wrapper = torch.FloatTensor
