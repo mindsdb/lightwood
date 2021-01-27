82fd4cdf217c1e8f4214888a994bc418ace7b2fa
Jorge Torres <jorge.torres.maldonado@gmail.com>
2021-01-27 08:30:53 -0800

    lightgbm mixer

---
+++ lightwood/mixers/lightgbm.py
@@ -0,0 +1,78 @@
+import numpy as np
+import lightgbm
+import torch
+import logging
+
+from lightwood.constants.lightwood import COLUMN_DATA_TYPES
+from lightwood.mixers import BaseMixer
+
+
+class LightGBMMixer(BaseMixer):
+    def __init__(self):
+        super().__init__()
+        self.models = {}
+
+    def _fit(self, train_ds, test_ds=None):
+        """
+        :param train_ds: DataSource
+        :param test_ds: DataSource
+        """
+
+        data = {
+            'train': {'ds': train_ds, 'data': None, 'label_data': {}},
+            'test': {'ds': test_ds, 'data': None, 'label_data': {}}
+        }
+
+
+        for dtype in data:
+            cols = data[dtype]['ds'].input_feature_names
+            out_cols = data[dtype]['ds'].output_feature_names
+            for col_name in cols:
+                if data[dtype]['data'] is None:
+                    data[dtype]['data'] = data[dtype]['ds'].get_encoded_column_data(col_name)
+                else:
+                    data[dtype]['data'] = torch.cat((data[dtype]['data'], data[dtype]['ds'].get_encoded_column_data(col_name)), 1)
+            data[dtype]['data'] = data[dtype]['data'].tolist()
+            for col_name in out_cols:
+                data[dtype]['label_data'][col_name] = data[dtype]['ds'].get_column_original_data(col_name)
+
+        out_cols = train_ds.output_feature_names
+        for col_name in out_cols:
+            train_data = lightgbm.Dataset(data['train']['data'], label = data['train']['data'][col_name])
+            validate_data = lightgbm.Dataset(data['test']['data'], label=data['test']['data'][col_name])
+            dtype = next(item for item in train_ds.output_features if item["name"] == col_name)['type']
+            if dtype not in ['numeric', 'categorical']:
+                logging.info('cannot support {dtype} in lightgbm'.format(dtype=dtype))
+                continue
+            else:
+                objective =  'regression' if dtype == 'numeric' else 'multiclass'
+            param = {'objective': objective}
+
+            num_round = 10
+            bst = lightgbm.train(param, train_data, num_round, valid_sets=validate_data)
+            self.models[col_name] = bst
+
+
+    def _predict(self, when_data_source, include_extra_data=False):
+        """
+        :param when_data_source: DataSource
+        :param include_extra_data: bool
+        """
+
+
+        cols = when_data_source.input_feature_names
+        out_cols = when_data_source.output_feature_names
+        data = None
+        for col_name in cols:
+            if data is None:
+                data = when_data_source.get_encoded_column_data(col_name)
+            else:
+                data = torch.cat((data, when_data_source.get_encoded_column_data(col_name)), 1)
+        data = data.tolist()
+
+
+        data = lightgbm.Dataset(data)
+
+        ypred = {col_name: self.models[col_name].predict(data) for col_name in out_cols}
+
+        return ypred
