24b6a35f96e8239adebdb3edc11b642fa86bce0c
George3d6 <george@cerebralab.com>
2021-01-28 19:49:48 +0000

    added try catching for mixer imports and remove two unused mixers

--- lightwood/mixers/__init__.py
+++ lightwood/mixers/__init__.py
@@ -1,5 +1,18 @@
-from lightwood.mixers.base_mixer import BaseMixer
-from lightwood.mixers.nn import NnMixer
-from lightwood.mixers.boost import BoostMixer
-from lightwood.mixers.sklearn_mixer import SklearnMixer
-from lightwood.mixers.lightgbm import LightGBMMixer
+try:
+    from lightwood.mixers.nn import NnMixer
+except Exception as e:
+    print(e)
+    NnMixer = None
+    
+try:
+    from lightwood.mixers.lightgbm import LightGBMMixer
+except Exception as e:
+    print(e)
+    LightGBMMixer = None
+
+# @TODO: Bellow mixers not ready for prime time yet
+# BoostMixer should probably go away in favor of LightGBMMixer
+# SklearnMixer has potential to be an auto-sklearn like thing, but nobody has the time to work on it
+# On hold til we have better benchmarks and more time
+#from lightwood.mixers.boost import BoostMixer
+#from lightwood.mixers.sklearn_mixer import SklearnMixer
