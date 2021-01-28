2c7405174170f085c70bda9083fc4de86ce8b73d
George3d6 <george@cerebralab.com>
2021-01-28 20:34:17 +0000

    debugging

--- lightwood/mixers/__init__.py
+++ lightwood/mixers/__init__.py
@@ -1,9 +1,11 @@
+from lightwood.mixers.base_mixer import BaseMixer
+
 try:
     from lightwood.mixers.nn import NnMixer
 except Exception as e:
     print(e)
     NnMixer = None
-    
+
 try:
     from lightwood.mixers.lightgbm import LightGBMMixer
 except Exception as e:
@@ -14,5 +16,12 @@ except Exception as e:
 # BoostMixer should probably go away in favor of LightGBMMixer
 # SklearnMixer has potential to be an auto-sklearn like thing, but nobody has the time to work on it
 # On hold til we have better benchmarks and more time
-#from lightwood.mixers.boost import BoostMixer
-#from lightwood.mixers.sklearn_mixer import SklearnMixer
+try:
+    from lightwood.mixers.boost import BoostMixer
+except Exception as e:
+    BoostMixer = None
+
+try:
+    from lightwood.mixers.sklearn_mixer import SklearnMixer
+except Exception as e:
+    SklearnMixer = None
