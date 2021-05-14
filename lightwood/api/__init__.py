18c8f1eb97049d46206d2232e7e76bbf2a8b8567
George3d6 <george@cerebralab.com>
2021-05-14 15:11:13 +0100

    added more structure

--- lightwood/api/__init__.py
+++ lightwood/api/__init__.py
@@ -1,3 +1,4 @@
 from lightwood.api.dtype import dtype
-from lightwood.api.types import LightwoodConfig, Output, Feature
+from lightwood.api.types import LightwoodConfig, Output, Feature, TypeInformation, StatisticalAnalysis
 from lightwood.api.generate_predictor import generate_predictor
+from lightwood.api.generate_config import generate_config
---
+++ lightwood/api/generate_config.py
@@ -0,0 +1,5 @@
+from lightwood.api import LightwoodConfig, TypeInformation, StatisticalAnalysis
+
+
+def generate_config(type_information: TypeInformation, statistical_analysis: StatisticalAnalysis) -> LightwoodConfig:
+    pass
