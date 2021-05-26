7c20f7c7194d4c24aa8d164ad2f8f0c7557abe53
George3d6 <george@cerebralab.com>
2021-05-26 13:03:25 +0100

    merged

--- lightwood/api/encode.py
+++ lightwood/api/encode.py
@@ -3,11 +3,13 @@ import pandas as pd
 from lightwood.encoder.base import BaseEncoder
 from lightwood.data.encoded_ds import EncodedDs, ConcatedEncodedDs
 
+
 def encode(encoders: List[BaseEncoder], folds: List[pd.DataFrame], target: str) -> List[EncodedDs]:
     encoded_ds_arr: List[EncodedDs] = []
     for fold in folds:
         encoded_ds_arr.append(EncodedDs(encoders, fold, target))
     return encoded_ds_arr
+
 
 def concat(encoded_ds_arr: List[EncodedDs]):
     return ConcatedEncodedDs(encoded_ds_arr)
