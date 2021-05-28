627cee22d2f5de0b73e1b3532ec70d81ede763cd
George3d6 <george@cerebralab.com>
2021-05-28 14:09:33 +0300

    done implementing fitting for lightGBM

--- lightwood/model/lightgbm.py
+++ lightwood/model/lightgbm.py
@@ -1,13 +1,14 @@
 from lightwood.data.encoded_ds import ConcatedEncodedDs, EncodedDs
 from lightwood.api.types import LightwoodConfig
+from lightwooda.api import dtype
 from typing import List, Set
 import numpy as np
-import optuna.integration.lightgbm as lgb
+import optuna.integration.lightgbm as optunna_lightgbm
 import lightgbm
 import optuna
 import torch
 import time
-
+from lightwood.helpers.log import log
 from lightwood.helpers.device import get_devices
 from sklearn.preprocessing import OrdinalEncoder
 from lightwood.helpers.log import log
@@ -65,108 +66,73 @@ class LightGBM(BaseModel):
 
         input_col_arr = list(self.lightwood_config.features.keys())
         output_col = self.lightwood_config.output.name
-        for input_col in input_col_arr:
-            
+        output_dtype = self.lightwood_config.output.data_dtype
 
-        # Order is important here
-        for subset_name in ['train','test']:
-            cols = data[subset_name]['ds'].input_feature_names
-            out_cols = data[subset_name]['ds'].output_feature_names
-            for col_name in cols:
+        for subset_name in ['train', 'test']:
+            for input_col in input_col_arr:
                 if data[subset_name]['data'] is None:
-                    data[subset_name]['data'] = data[subset_name]['ds'].get_encoded_column_data(col_name).to(self.device)
+                    data[subset_name]['data'] = data[subset_name]['ds'].get_encoded_column_data(input_col).to(self.device)
                 else:
-                    enc_col = data[subset_name]['ds'].get_encoded_column_data(col_name)
+                    enc_col = data[subset_name]['ds'].get_encoded_column_data(input_col)
                     data[subset_name]['data'] = torch.cat((data[subset_name]['data'], enc_col.to(self.device)), 1)
             data[subset_name]['data'] = data[subset_name]['data'].tolist()
-            for col_name in out_cols:
-                label_data = data[subset_name]['ds'].get_column_original_data(col_name)
-                if next(item for item in train_ds.output_features if item["name"] == col_name)['type'] == COLUMN_DATA_TYPES.CATEGORICAL:
-                    if subset_name == 'train':
-                        self.ord_encs[col_name] = OrdinalEncoder()
-                        self.label_sets[col_name] = set(label_data)
-                        self.label_sets[col_name].add('__mdb_unknown_cat')
-                        self.ord_encs[col_name].fit(np.array(list(self.label_sets[col_name])).reshape(-1, 1))
 
-                    label_data = [x if x in self.label_sets[col_name] else '__mdb_unknown_cat' for x in label_data]
-                    label_data = self.ord_encs[col_name].transform(np.array(label_data).reshape(-1, 1)).flatten()
+            label_data = data[subset_name]['ds'].get_column_original_data(output_col)
 
-                data[subset_name]['label_data'][col_name] = label_data
+            if output_dtype == dtype.categorical:
+                if subset_name == 'train':
+                    self.ordinal_encoder = OrdinalEncoder()
+                    self.label_set = set(label_data)
+                    self.label_set.add('__mdb_unknown_cat')
+                    self.ordinal_encoder.fit(np.array(list(self.label_set)).reshape(-1, 1))
 
-        for col_name in train_ds.output_feature_names:
-            dtype = next(item for item in train_ds.output_features if item["name"] == col_name)['type']
-            if dtype not in [COLUMN_DATA_TYPES.NUMERIC, COLUMN_DATA_TYPES.CATEGORICAL]:
-                log.info('cannot support {dtype} in lightgbm'.format(dtype=dtype))
-                continue
-            else:
-                objective = 'regression' if dtype == COLUMN_DATA_TYPES.NUMERIC else 'multiclass'
-                metric = 'l2' if dtype == COLUMN_DATA_TYPES.NUMERIC else 'multi_logloss'
+                label_data = [x if x in self.label_set else '__mdb_unknown_cat' for x in label_data]
+                label_data = self.ordinal_encoder.transform(np.array(label_data).reshape(-1, 1)).flatten()
 
-            params = {'objective': objective,
-                      'metric': metric,
-                      'verbose': -1,
-                      'lambda_l1': 0.1,
-                      'lambda_l2': 0.1,
-                      'force_row_wise': True,
-                      'device_type': self.device_str
-                      }
-            if objective == 'multiclass':
-                self.all_classes = self.ord_encs[col_name].categories_[0]
-                params['num_class'] = self.all_classes.size
+            data[subset_name]['label_data'] = label_data
 
-            num_iterations = 50
-            kwargs = {}
+        if output_dtype not in (dtype.categorical, dtype.integer, dtype.float):
+            log.info(f'Lightgbm mixer not supported for type: {output_dtype}')
+        else:
+            objective = 'regression' if output_dtype in (dtype.integer, dtype.float) else 'multiclass'
+            metric = 'l2' if output_dtype in (dtype.integer, dtype.float) else 'multi_logloss'
 
-            if self.stop_training_after_seconds is not None:
-                train_data = lightgbm.Dataset(data['train']['data'], label=data['train']['label_data'][col_name])
-                validate_data = lightgbm.Dataset(data['test']['data'], label=data['test']['label_data'][col_name])
-                start = time.time()
-                params['num_iterations'] = 1
-                bst = lightgbm.train(params, train_data, valid_sets=validate_data, verbose_eval=False)
-                end = time.time()
-                seconds_for_one_iteration = max(0.1, end - start)
-                log.info(f'A single GBM itteration takes {seconds_for_one_iteration} seconds')
-                max_itt = int(self.stop_training_after_seconds / seconds_for_one_iteration)
-                num_iterations = max(1, min(num_iterations, max_itt))
-                # Turn on grid search if training doesn't take too long using it
-                if max_itt >= num_iterations and seconds_for_one_iteration < 10:
-                    self.grid_search = True
-                    kwargs['time_budget'] = self.stop_training_after_seconds
+        params = {
+            'objective': objective,
+            'metric': metric,
+            'verbose': -1,
+            'lambda_l1': 0.1,
+            'lambda_l2': 0.1,
+            'force_row_wise': True,
+            'device_type': self.device_str
+        }
 
-            train_data = lightgbm.Dataset(data['train']['data'], label=data['train']['label_data'][col_name])
-            validate_data = lightgbm.Dataset(data['test']['data'], label=data['test']['label_data'][col_name])
-            model = lgb if self.grid_search else lightgbm
-            log.info(f'Training GBM ({model}) with {num_iterations} iterations given {self.stop_training_after_seconds} seconds constraint')
-            params['num_iterations'] = num_iterations
-            bst = model.train(params, train_data, valid_sets=validate_data, verbose_eval=False, **kwargs)
+        if objective == 'multiclass':
+            self.all_classes = self.ordinal_encoder.categories_[0]
+            params['num_class'] = self.all_classes.size
 
-            self.models[col_name] = bst
+        num_iterations = 50
+        kwargs = {}
 
-    def _predict(self, when_data_source, include_extra_data=False):
-        """
-        :param when_data_source: DataSource
-        :param include_extra_data: bool
-        """
-        data = None
-        for col_name in when_data_source.input_feature_names:
-            if data is None:
-                data = when_data_source.get_encoded_column_data(col_name).to(self.device)
-            else:
-                data = torch.cat((data, when_data_source.get_encoded_column_data(col_name).to(self.device)), 1)
-        data = data.tolist()
+        if self.stop_training_after_seconds is not None:
+            train_data = lightgbm.Dataset(data['train']['data'], label=data['train']['label_data'])
+            validate_data = lightgbm.Dataset(data['test']['data'], label=data['test']['label_data'])
+            start = time.time()
+            params['num_iterations'] = 1
+            bst = lightgbm.train(params, train_data, valid_sets=validate_data, verbose_eval=False)
+            end = time.time()
+            seconds_for_one_iteration = max(0.1, end - start)
+            log.info(f'A single GBM itteration takes {seconds_for_one_iteration} seconds')
+            max_itt = int(self.lightwood_config.problem_definition.time_per_model / seconds_for_one_iteration)
+            num_iterations = max(1, min(num_iterations, max_itt))
+            # Turn on grid search if training doesn't take too long using it
+            if max_itt >= num_iterations and seconds_for_one_iteration < 10:
+                model_generator = optunna_lightgbm if self.grid_search else lightgbm
+                kwargs['time_budget'] = self.lightwood_config.problem_definition.time_per_model
 
-        ypred = {}
-        for col_name in when_data_source.output_feature_names:
-            col_config = [conf for conf in when_data_source.config['output_features'] if conf['name'] == col_name][0]
-            col_preds = self.models[col_name].predict(data)
-            ypred[col_name] = {}
-            if col_name in self.ord_encs:
-                ypred[col_name]['class_distribution'] = list(col_preds)
-                ypred[col_name]['class_labels'] = {i: cls for i, cls in enumerate(self.all_classes)}
-                col_preds = self.ord_encs[col_name].inverse_transform(np.argmax(col_preds, axis=1).reshape(-1, 1)).flatten()
-            if col_config.get('encoder_attrs', False):
-                if col_config['encoder_attrs'].get('positive_domain', False):
-                    col_preds = col_preds.clip(0)
-            ypred[col_name]['predictions'] = list(col_preds)
-
-        return ypred
+        train_data = lightgbm.Dataset(data['train']['data'], label=data['train']['label_data'])
+        validate_data = lightgbm.Dataset(data['test']['data'], label=data['test']['label_data'])
+        
+        log.info(f'Training GBM ({model_generator}) with {num_iterations} iterations given {self.stop_training_after_seconds} seconds constraint')
+        params['num_iterations'] = num_iterations
+        self.model = model_generator.train(params, train_data, valid_sets=validate_data, verbose_eval=False, **kwargs)   
