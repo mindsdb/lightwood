735c7cfe3b54acb3dfb124383b2f37d04439f5ea
George3d6 <george@cerebralab.com>
2021-06-02 17:28:35 +0300

    better templating

--- lightwood/data/cleaner.py
+++ lightwood/data/cleaner.py
@@ -1,5 +1,6 @@
+from typing import Dict
 from lightwood.api.dtype import dtype
-from lightwood.api.types import LightwoodConfig
+from lightwood.api.types import Feature, LightwoodConfig, Output
 from lightwood.helpers.log import log
 from dateutil.parser import parse as parse_dt
 from mindsdb_datasources import DataSource
@@ -11,20 +12,21 @@ def clean_value(element: object, data_dtype: str):
         element = parse_dt(element)
     return element
 
-def cleaner(data: DataSource, lightwood_config: LightwoodConfig) -> pd.DataFrame:
-    for col_data in [*lightwood_config.features.values(), lightwood_config.output]:
+
+def cleaner(data: DataSource, dtype_dict: Dict[str, str], pct_invalid: int) -> pd.DataFrame:
+    for name, data_dtype in dtype_dict:
         new_data = []
-        for element in data.df[col_data.name]:
+        for element in data.df[name]:
             try:
-                new_data.append(clean_value(element, col_data.data_dtype))
+                new_data.append(clean_value(element, data_dtype))
             except Exception as e:
                 new_data.append(None)
-                log.warning(f'Unable to parse elemnt: {element} or type {col_data.data_dtype} from column {col_data.name}. Excetpion: {e}')
+                log.warning(f'Unable to parse elemnt: {element} or type {data_dtype} from column {name}. Excetpion: {e}')
 
         pct_invalid = 100 * (len(new_data) - len([x for x in new_data if x is not None])) / len(new_data)
         
-        if pct_invalid > lightwood_config.problem_definition.pct_invalid:
-            err = f'Too many ({pct_invalid}%) invalid values in column {col_data.name} of type {col_data.data_dtype}'
+        if pct_invalid > pct_invalid:
+            err = f'Too many ({pct_invalid}%) invalid values in column {name} of type {data_dtype}'
             log.error(err)
             raise Exception(err)
 
