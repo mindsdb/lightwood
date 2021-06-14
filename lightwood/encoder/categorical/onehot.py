fixed various issues around nn mixer and weighting

lightwood/encoder/categorical/onehot.py
@@ -44,8 +44,8 @@ class OneHotEncoder(BaseEncoder):
             self._lang.removeWord(word_to_remove)
         
         if self.is_target:
-            self.index_weights = [None] * len(self._lang.n_words)
-            self.index_weights[0] = np.mean(self.class_distribution.values())
+            self.index_weights = [None] * self._lang.n_words
+            self.index_weights[0] = np.mean(list(self.class_distribution.values()))
             for word in set(priming_data):
                 self.index_weights[self._lang.word2index[word]] = 1 / self.class_distribution[word]
             self.index_weights = torch.Tensor(self.index_weights)

lightwood/model/neural.py
@@ -31,9 +31,9 @@ class Neural(BaseModel):
         self.timeseries_settings = timeseries_settings
         self.target_encoder = target_encoder
 
-    def _select_criterion(self, target_encoder) -> torch.nn.Module:
+    def _select_criterion(self) -> torch.nn.Module:
         if self.dtype_dict[self.target] in (dtype.categorical, dtype.binary):
-            criterion = TransformCrossEntropyLoss(weight=target_encoder.index_weights)
+            criterion = TransformCrossEntropyLoss(weight=self.target_encoder.index_weights.to(self.model.device))
         elif self.dtype_dict[self.target] in (dtype.tags):
             criterion = nn.BCEWithLogitsLoss()
         elif self.dtype_dict[self.target] in (dtype.integer, dtype.float) and self.timeseries_settings.is_timeseries:
@@ -92,7 +92,7 @@ class Neural(BaseModel):
             output_size=len(train_ds[0][1])
         )
         
-        criterion = self._select_criterion(train_ds.encoders[self.target])
+        criterion = self._select_criterion()
         optimizer = self._select_optimizer()
 
         started = time.time()