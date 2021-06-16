abfca0c9280a7208c2006142ea90a747eb019279
George3d6 <george@cerebralab.com>
2021-06-17 02:15:29 +0300

    added some integration tests

--- lightwood/__init__.py
+++ lightwood/__init__.py
@@ -7,10 +7,12 @@ from lightwood.api import (
     StatisticalAnalysis,
     generate_predictor,
     generate_config,
-    encode
+    encode,
+    make_predictor,
+    analyze_dataset
 )
 import lightwood.data as data
 from lightwood.data import infer_types, statistical_analysis
 
 
-__all__ = ['data', 'infer_types', 'statistical_analysis', 'dtype', 'LightwoodConfig', 'Output', 'Feature', 'TypeInformation', 'StatisticalAnalysis', 'generate_predictor', 'generate_config', 'encode']
+__all__ = ['data', 'infer_types', 'statistical_analysis', 'dtype', 'LightwoodConfig', 'Output', 'Feature', 'TypeInformation', 'StatisticalAnalysis', 'generate_predictor', 'generate_config', 'encode', 'make_predictor', 'analyze_dataset']
---
+++ lightwood/api/high_level.py
@@ -0,0 +1,35 @@
+from numpy import DataSource
+from lightwood.api.types import DataAnalysis, ProblemDefinition
+import importlib
+from lightwood import generate_predictor
+import lightwood
+
+
+def make_predictor(datasource: DataSource, problem_definition_dict: dict):
+    predictor_class_str = generate_predictor(ProblemDefinition.from_dict(problem_definition_dict), datasource.df)
+
+    with open('dynamic_predictor.py', 'w') as fp:
+        fp.write(predictor_class_str)
+
+    predictor_class = importlib.import_module('dynamic_predictor').Predictor
+    print('Class was evaluated successfully')
+
+    predictor = predictor_class()
+    print('Class initialized successfully')
+
+    return predictor
+
+
+def analyze_dataset(datasource: DataSource, problem_definition_dict: dict = None):
+    if problem_definition_dict is None:
+        problem_definition_dict = {}
+    problem_definition = ProblemDefinition.from_dict(problem_definition_dict)
+
+    df = datasource.df
+    type_information = lightwood.data.infer_types(df, problem_definition.pct_invalid)
+    statistical_analysis = lightwood.data.statistical_analysis(df, type_information, problem_definition)
+
+    return DataAnalysis(
+        type_information=type_information,
+        statistical_analysis=statistical_analysis
+    )
