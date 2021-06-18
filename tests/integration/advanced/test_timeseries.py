0bf4322b894a315213ba0a5352cbebcde32aff97
George3d6 <george@cerebralab.com>
2021-06-18 21:28:15 +0300

    added advnaced tests template

---
+++ tests/integration/advanced/test_timeseries.py
@@ -0,0 +1,7 @@
+from tests.integration.helpers import ClickhouseTest, break_dataset
+from lightwood.api import make_predictor
+
+
+class TestTimeseries(ClickhouseTest):
+    def test_timeseries(self):
+        pass
