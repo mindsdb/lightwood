d366425b6a65ec7307cdf60fff912ca4e9a0378e
George3d6 <george@cerebralab.com>
2021-06-18 21:28:15 +0300

    added advnaced tests template

---
+++ tests/integration/advanced/test_text_input.py
@@ -0,0 +1,7 @@
+from tests.integration.helpers import ClickhouseTest, break_dataset
+from lightwood.api import make_predictor
+
+
+class TestTextInput(ClickhouseTest):
+    def test_text_input(self):
+        pass
