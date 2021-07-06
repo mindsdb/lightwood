74a69a3995de090cd3781f9a51ed6cdce8706567
George3d6 <george@cerebralab.com>
2021-07-07 02:57:06 +0300

    encoder parallelization with pool

--- lightwood/helpers/parallelism.py
+++ lightwood/helpers/parallelism.py
@@ -10,11 +10,7 @@ def get_nr_procs(df=None):
         return 1
     else:
         available_mem = psutil.virtual_memory().available
-        try:
-            import mindsdb_worker # noqa
-            max_per_proc_usage = 0.2 * pow(10, 9)
-        except Exception:
-            max_per_proc_usage = 0.5 * pow(10, 9)
+        max_per_proc_usage = 0.2 * pow(10, 9)
 
         if df is not None:
             max_per_proc_usage += df.memory_usage(index=True, deep=True).sum()
@@ -36,14 +32,12 @@ def mut_method_call(object_dict: Dict[str, tuple]) -> Dict[str, object]:
     manager = mp.Manager()
     return_dict = manager.dict()
 
-    jobs = []
+    nr_procs = get_nr_procs()
+    pool = mp.Pool(processes=nr_procs)
     for name, data in object_dict.items():
-        p = mp.Process(target=run_mut_method, args=(data[0], data[1], data[2], name, return_dict))
-        jobs.append(p)
-        p.start()
-
-    for proc in jobs:
-        proc.join()
+        pool.apply_async(target=run_mut_method, args=(data[0], data[1], data[2], name, return_dict))
+    pool.close()
+    pool.join()
 
     for identifier in return_dict:
         if return_dict[identifier] == False:
