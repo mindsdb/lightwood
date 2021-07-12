7d59df4f4d6a592d5d2b15caa6c04e1535557386
George3d6 <george@cerebralab.com>
2021-07-12 20:10:28 +0300

    added sapwn ctx for multiprocessing tests

--- lightwood/__about__.py
+++ lightwood/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'lightwood'
 __package_name__ = 'lightwood'
-__version__ = '0.90.0'
+__version__ = '0.91.0'
 __description__ = "Lightwood is a toolkit for automatic machine learning model building"
 __email__ = "community@mindsdb.com"
 __author__ = 'MindsDB Inc'
