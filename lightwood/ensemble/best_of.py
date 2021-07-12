f4f7ac206f23ca03b26f8616b7d1bc8a492aa550
George3d6 <george@cerebralab.com>
2021-07-12 18:27:47 +0300

    removed pointless functionality from ensemble

--- lightwood/ensemble/best_of.py
+++ lightwood/ensemble/best_of.py
@@ -1,17 +1,17 @@
 from typing import List
 from lightwood.model.base import BaseModel
 import pandas as pd
-from lightwood.data.encoded_ds import EncodedDs
+from lightwood.data.encoded_ds import EncodedDs, ConcatedEncodedDs
 from lightwood.ensemble.base import BaseEnsemble
 
 
 class BestOf(BaseEnsemble):
     best_index: int
 
-    def __init__(self, models: List[BaseModel], data: EncodedDs) -> None:
-        super().__init__(models, data)
+    def __init__(self, models: List[BaseModel]) -> None:
+        super().__init__(models)
         # @TODO: Need some shared accuracy functionality to determine model selection here
         self.best_index = 0
 
-    def __call__(self, ds: EncodedDs) -> pd.DataFrame:
-        return self.models[self.best_index](ds)
+    def __call__(self, ds_arr: List[EncodedDs]) -> pd.DataFrame:
+        return self.models[self.best_index](ConcatedEncodedDs(ds_arr))
