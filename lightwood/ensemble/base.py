a1d3a6480bbc66f41221b33bc6c13d2ab92ecbb3
George3d6 <george@cerebralab.com>
2021-07-12 18:29:19 +0300

    removed pointless functionality from ensemble

--- lightwood/ensemble/base.py
+++ lightwood/ensemble/base.py
@@ -5,9 +5,11 @@ from lightwood.data.encoded_ds import EncodedDs
 
 
 class BaseEnsemble:
+    data: List[EncodedDs]
     models: List[BaseModel]
 
-    def __init__(self, models: List[BaseModel]) -> None:
+    def __init__(self, models: List[BaseModel], data: List[EncodedDs]) -> None:
+        self.data = data
         self.models = models
         
     def __call__(self, ds: EncodedDs) -> pd.DataFrame:
