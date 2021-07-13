b9a4b3354cd73b378c7e22ee8f7e47ae8ce7d957
George3d6 <george@cerebralab.com>
2021-07-13 19:14:48 +0300

    added early stopping to model validation loop

--- lightwood/__about__.py
+++ lightwood/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'lightwood'
 __package_name__ = 'lightwood'
-__version__ = '0.95.0'
+__version__ = '0.93.0'
 __description__ = "Lightwood is a toolkit for automatic machine learning model building"
 __email__ = "community@mindsdb.com"
 __author__ = 'MindsDB Inc'
