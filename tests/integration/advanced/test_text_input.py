053da5ef5d0e5bb00ead33eb7dfbf5ce0c5239c8
George3d6 <george@cerebralab.com>
2021-07-17 00:56:49 +0300

    Made the PretrainedLangEncoder prepare with a new and more reduced encoded_output_data format and streamlined some of the logic while at it

--- tests/integration/advanced/test_text_input.py
+++ tests/integration/advanced/test_text_input.py
@@ -10,11 +10,13 @@ class TestText(unittest.TestCase):
         predictor = predictor_from_problem(datasource.df, ProblemDefinition.from_dict({'target': 'Label'}))
         predictor.learn(datasource.df)
         predictions = predictor.predict(datasource.df)
-        print(predictions[0:100])
+        for x in predictions['prediction']:
+            assert x is not None
 
     def test_1_train_and_predict_model(self):
         datasource = FileDS('tests/data/wine_reviews_binary_sample.csv')
         predictor = predictor_from_problem(datasource.df, ProblemDefinition.from_dict({'target': 'label'}))
         predictor.learn(datasource.df)
         predictions = predictor.predict(datasource.df)
-        print(predictions[0:100])
+        for x in predictions['prediction']:
+            assert x is not None
