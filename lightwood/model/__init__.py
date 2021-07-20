7c3ec1865a93fc0628838607deeb0ec5b4d2e566
Patricio Cerda Mardini <pcerdam@live.com>
2021-07-20 11:53:56 -0400

    sktime API working

--- lightwood/model/__init__.py
+++ lightwood/model/__init__.py
@@ -3,6 +3,7 @@ from lightwood.model.neural import Neural
 from lightwood.model.ts_neural import TsNeural
 from lightwood.model.lightgbm import LightGBM
 from lightwood.model.lightgbm_array import LightGBMArray
+from lightwood.model.sktime import SkTime
 
 
-__all__ = ['BaseModel', 'Neural', 'TsNeural', 'LightGBM', 'LightGBMArray']
+__all__ = ['BaseModel', 'Neural', 'TsNeural', 'LightGBM', 'LightGBMArray', 'SkTime']
