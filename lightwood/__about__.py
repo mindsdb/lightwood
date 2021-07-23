de241105a4f71fe1ffefce9a0efbe075432b7185
George3d6 <george@cerebralab.com>
2021-07-23 21:00:52 +0300

    v bump for benchmarking logs

--- lightwood/__about__.py
+++ lightwood/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'lightwood'
 __package_name__ = 'lightwood'
-__version__ = '0.200.0'
+__version__ = '0.102.0'
 __description__ = "Lightwood is a toolkit for automatic machine learning model building"
 __email__ = "community@mindsdb.com"
 __author__ = 'MindsDB Inc'
