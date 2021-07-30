6daffb30ef4e7a976afa6707ef108b71fd710af5
Patricio Cerda Mardini <pcerdam@live.com>
2021-07-30 11:23:49 -0400

    predict_common_body fix

--- lightwood/api/json_ai.py
+++ lightwood/api/json_ai.py
@@ -535,6 +535,7 @@ data = {call(json_ai.cleaner, json_ai)}
 {ts_transform_code}
 
 encoded_ds = lightwood.encode(self.encoders, data, self.target)
+encoded_data = encoded_ds.get_encoded_data(include_target=False)
 """
     predict_common_body = align(predict_common_body, 2)
 
