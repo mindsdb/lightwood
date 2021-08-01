989fcf94027ab7cb8d7a22d4cfcc13531b60668b
George3d6 <george@cerebralab.com>
2021-08-01 20:08:59 +0300

    fixed for timeseries

--- lightwood/encoder/type_encoder_maps/Categorical.py
+++ lightwood/encoder/type_encoder_maps/Categorical.py
@@ -1,2 +1,4 @@
 from lightwood.encoder.categorical.onehot import OneHotEncoder
 from lightwood.encoder.categorical.autoencoder import CategoricalAutoEncoder
+from lightwood.encoder.time_series.rnn import TimeSeriesEncoder
+from lightwood.encoder.time_series.plain import TimeSeriesPlainEncoder
--- lightwood/encoder/type_encoder_maps/DateTime.py
+++ lightwood/encoder/type_encoder_maps/DateTime.py
@@ -1,1 +1,3 @@
 from lightwood.encoder.datetime.datetime import DatetimeEncoder
+from lightwood.encoder.time_series.rnn import TimeSeriesEncoder
+from lightwood.encoder.time_series.plain import TimeSeriesPlainEncoder
