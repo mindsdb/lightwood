1e50b308d6e5bfd6e55a2ac8f66426156c008c60
Patricio Cerda Mardini <pcerdam@live.com>
2021-08-03 10:41:37 -0400

    .encode() -> List[EncodedDs]

--- lightwood/api/encode.py
+++ lightwood/api/encode.py
@@ -1,12 +1,12 @@
-from typing import List, Union
+from typing import List
 import pandas as pd
 from lightwood.encoder.base import BaseEncoder
 from lightwood.data.encoded_ds import EncodedDs
 
 
-def encode(encoders: List[BaseEncoder], folds: List[pd.DataFrame], target: str) -> Union[EncodedDs, List[EncodedDs]]:
+def encode(encoders: List[BaseEncoder], folds: List[pd.DataFrame], target: str) -> List[EncodedDs]:
     if isinstance(folds, pd.DataFrame):
-        return EncodedDs(encoders, folds, target)
+        folds = [folds]
 
     encoded_ds_arr: List[EncodedDs] = []
     for fold in folds:
