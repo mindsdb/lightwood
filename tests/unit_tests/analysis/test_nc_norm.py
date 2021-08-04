ec754c1c8853c0d1b145c5636102da87b62d717d
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2021-08-04 18:26:37 -0400

    fix

--- tests/unit_tests/analysis/test_nc_norm.py
+++ tests/unit_tests/analysis/test_nc_norm.py
@@ -11,7 +11,7 @@ class TestNcNormalizer(unittest.TestCase):
         truths = np.array([1, 2, 3, 4, 5, 0])
         bounds = [0.5, 1.5]
 
-        labels = Normalizer.compute_numerical_labels(Normalizer, preds, truths, bounds)
+        labels = Normalizer.compute_numerical_labels(preds, truths, bounds)
         self.assertTrue(np.min(labels) >= bounds[0])
         self.assertTrue(np.max(labels) <= bounds[1])
         self.assertFalse(np.isnan(labels).any())
@@ -21,7 +21,7 @@ class TestNcNormalizer(unittest.TestCase):
         preds = np.array([[0.01, 0.99], [1, 0], [0.99, 0.1], [0, 1]])
         truths = np.array([[0, 1], [0, 1], [1, 0], [1, 0]])
 
-        labels = Normalizer.compute_categorical_labels(Normalizer, preds, truths)
+        labels = Normalizer.compute_categorical_labels(preds, truths)
         self.assertTrue(np.min(labels) >= 0)
         self.assertFalse(np.isnan(labels).any())
         self.assertFalse(np.isinf(labels).any())
