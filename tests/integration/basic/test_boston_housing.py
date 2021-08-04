5a353a63be1261dfb0082a840630091b35734986
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2021-08-04 18:36:06 -0400

    checks in regression test

--- tests/integration/basic/test_boston_housing.py
+++ tests/integration/basic/test_boston_housing.py
@@ -1,6 +1,10 @@
-from lightwood.api.types import ProblemDefinition
-import pandas as pd
+import json
 import unittest
+import pandas as pd
+from sklearn.metrics import r2_score
+
+from lightwood.api.types import ProblemDefinition
+from lightwood.api.high_level import json_ai_from_problem
 
 
 class TestBasic(unittest.TestCase):
@@ -8,13 +12,15 @@ class TestBasic(unittest.TestCase):
         from lightwood.api.high_level import predictor_from_problem
 
         df = pd.read_csv('tests/data/boston.csv')
-        
-        import json
-        from lightwood.api.high_level import json_ai_from_problem
-        json.dump(json_ai_from_problem(df, ProblemDefinition.from_dict({'target': 'MEDV', 'time_aim': 200})).to_dict(), open('AI2.json', 'w'))
+        target = 'MEDV'
 
-        predictor = predictor_from_problem(df, ProblemDefinition.from_dict({'target': 'MEDV', 'time_aim': 200}))
+        json.dump(json_ai_from_problem(df, ProblemDefinition.from_dict({'target': target, 'time_aim': 200})).to_dict(), open('AI2.json', 'w'))
+
+        predictor = predictor_from_problem(df, ProblemDefinition.from_dict({'target': target, 'time_aim': 200}))
         predictor.learn(df)
-
         predictions = predictor.predict(df)
-        print(predictions[0:100])
+
+        # sanity checks
+        self.assertTrue(r2_score(df[target], predictions['prediction']) > 0.8)
+        self.assertTrue(all([0 <= p <= 1 for p in predictions['confidence']]))
+        self.assertTrue(all([p['lower'] <= p['prediction'] <= p['upper'] for _, p in predictions.iterrows()]))
