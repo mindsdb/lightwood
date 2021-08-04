88cf070c23d36b8394506219d30608f293c189d9
George3d6 <george@cerebralab.com>
2021-08-05 02:11:44 +0300

    structural fixes

---
+++ dev/README.md
@@ -0,0 +1,10 @@
+# Contributor guide
+
+## Setting up vscode environment
+
+* Install and enable setting sync using github account (if you use multiple machines)
+* Install pylance (for types) and make sure to disable pyright
+* Go to `Python > Lint: Enabled` and disable everything *but* falke8
+* Set `python.linting.flake8Path` to the full path to flake8 (which falke8)
+* Set `Python › Formatting: Provider` to autopep8
+* Install live share and live share whiteboard
--- tests/unit_tests/data/test_infer_types.py
+++ tests/unit_tests/data/test_infer_types.py
@@ -4,39 +4,42 @@ from lightwood.api import dtype
 import unittest
 from itertools import cycle
 import random
-import unittest
-from unittest import mock
-from uuid import uuid4
 from datetime import datetime, timedelta
 import numpy as np
 import pandas as pd
-from lightwood import dtype
 from tests.utils.data_generation import (
     test_column_types,
     generate_short_sentences,
     generate_rich_sentences,
-    VOCAB
+    VOCAB,
 )
+import math
 
 
+def f():
+    a = {1: 3, 5: 2}
+    print(124124124, 12412412412412, 124124124, 12412412412412, 124124124, 12412412412412, 124124124, 12412412412412, 124124124, 12412412412412, 124124124, 12412412412412, 124124124, 12412412412412, 124124124, 12412412412412, 124124124, 12412412412412)
+    
 class TestInferTypes(unittest.TestCase):
     def test_infer_types_on_home_rentlas(self):
-        datasource = FileDS('https://raw.githubusercontent.com/mindsdb/mindsdb-examples/master/classics/home_rentals/dataset/train.csv')
+        datasource = FileDS(
+            "https://raw.githubusercontent.com/mindsdb/mindsdb-examples/master/classics/home_rentals/dataset/train.csv"
+        )
         type_information = infer_types(datasource.df, pct_invalid=0)
 
-        self.assertTrue(type_information.dtypes['number_of_rooms'] == dtype.categorical)
-        self.assertTrue(type_information.dtypes['number_of_bathrooms'] == dtype.binary)
-        self.assertTrue(type_information.dtypes['sqft'] == dtype.integer)
-        self.assertTrue(type_information.dtypes['location'] == dtype.categorical)
-        self.assertTrue(type_information.dtypes['days_on_market'] == dtype.integer)
-        self.assertTrue(type_information.dtypes['initial_price'] == dtype.integer)
-        self.assertTrue(type_information.dtypes['neighborhood'] == dtype.categorical)
-        # This has a .0 after every price, so we shoiuld detect it as float
-        self.assertTrue(type_information.dtypes['rental_price'] == dtype.float)
+        self.assertTrue(type_information.dtypes["number_of_rooms"] == dtype.categorical)
+        self.assertTrue(type_information.dtypes["number_of_bathrooms"] == dtype.binary)
+        self.assertTrue(type_information.dtypes["sqft"] == dtype.integer)
+        self.assertTrue(type_information.dtypes["location"] == dtype.categorical)
+        self.assertTrue(type_information.dtypes["days_on_market"] == dtype.integer)
+        self.assertTrue(type_information.dtypes["initial_price"] == dtype.integer)
+        self.assertTrue(type_information.dtypes["neighborhood"] == dtype.categorical)
+        # This has a .0 after every price, so we should detect it as float
+        self.assertTrue(type_information.dtypes["rental_price"] == dtype.float)
 
         # There should be no ambiguity in the type detection for this dataset
         for k in type_information.additional_info:
-            self.assertTrue(len(type_information.additional_info[k]['dtype_dist']) == 1)
+            self.assertTrue(len(type_information.additional_info[k]["dtype_dist"]) == 1)
 
         # None of the columns are identifiers
         for k in type_information.identifiers:
@@ -45,7 +48,7 @@ class TestInferTypes(unittest.TestCase):
     def test_type_deduction(self):
         n_points = 100
 
-        # Apparently for n_category_values = 10 it doesnt work
+        # Apparently for n_category_values = 10 it doesn't work
         n_category_values = 4
         categories_cycle = cycle(range(n_category_values))
         n_multilabel_category_values = 25
@@ -53,25 +56,46 @@ class TestInferTypes(unittest.TestCase):
             random.choices(VOCAB[0:20], k=n_multilabel_category_values)
         )
 
-        df = pd.DataFrame({
-            'numeric_int': [x % 10 for x in list(range(n_points))],
-            'numeric_float': np.linspace(0, n_points, n_points),
-            'date_timestamp': [(datetime.now() - timedelta(minutes=int(i))).isoformat() for i in range(n_points)],
-            'date_date': [(datetime.now() - timedelta(days=i)).strftime('%Y-%m-%d') for i in range(n_points)],
-            'categorical_str': [f'category_{next(categories_cycle)}' for i in range(n_points)],
-            'categorical_int': [next(categories_cycle) for i in range(n_points)],
-            'categorical_binary': [0, 1] * (n_points//2),
-            'sequential_array': [f"1,2,3,4,5,{i}" for i in range(n_points)],
-            'multiple_categories_array_str': [",".join([f'{next(multiple_categories_str_cycle)}' for j in range(random.randint(1,6))]) for i in range(n_points)],
-            'short_text': generate_short_sentences(n_points),
-            'rich_text': generate_rich_sentences(n_points)
-        })
+        df = pd.DataFrame(
+            {
+                "numeric_int": [x % 10 for x in list(range(n_points))],
+                "numeric_float": np.linspace(0, n_points, n_points),
+                "date_timestamp": [
+                    (datetime.now() - timedelta(minutes=int(i))).isoformat()
+                    for i in range(n_points)
+                ],
+                "date_date": [
+                    (datetime.now() - timedelta(days=i)).strftime("%Y-%m-%d")
+                    for i in range(n_points)
+                ],
+                "categorical_str": [
+                    f"category_{next(categories_cycle)}" for i in range(n_points)
+                ],
+                "categorical_int": [next(categories_cycle) for i in range(n_points)],
+                "categorical_binary": [0, 1] * (n_points // 2),
+                "sequential_array": [f"1,2,3,4,5,{i}" for i in range(n_points)],
+                "multiple_categories_array_str": [
+                    ",".join(
+                        [
+                            f"{next(multiple_categories_str_cycle)}"
+                            for j in range(random.randint(1, 6))
+                        ]
+                    )
+                    for i in range(n_points)
+                ],
+                "short_text": generate_short_sentences(n_points),
+                "rich_text": generate_rich_sentences(n_points),
+            }
+        )
 
         type_information = infer_types(df, pct_invalid=0)
         for col_name in df.columns:
             expected_type = test_column_types[col_name]
-            print(f'Got {type_information.dtypes[col_name]} | Expected: {expected_type}')
+            print(
+                f"Got {type_information.dtypes[col_name]} | Expected: {expected_type}"
+            )
             assert type_information.dtypes[col_name] == expected_type
+
 
 '''
     def test_deduce_foreign_key(self):
@@ -287,4 +311,4 @@ class TestInferTypes(unittest.TestCase):
 
         assert predictor.transaction.lmd['stats_v2']['datetime']['typing']['data_type'] == DATA_TYPES.DATE
         assert predictor.transaction.lmd['stats_v2']['datetime']['typing']['data_subtype'] == DATA_SUBTYPES.TIMESTAMP
-''' 
+'''
