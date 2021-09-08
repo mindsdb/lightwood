392569b27b2b1d4eb51b6c60bee159bcebfebff1
George3d6 <george@cerebralab.com>
2021-09-08 15:25:23 +0300

    removed unused method

--- lightwood/analysis/nc/wrappers.py
+++ lightwood/analysis/nc/wrappers.py
@@ -19,25 +19,6 @@ def clear_icp_state(icp):
         icp.normalizer.model = None
 
 
-def restore_icp_state(col, hmd, session):
-    icps = hmd['icp'][col]
-    try:
-        predictor = session.transaction.model_backend.predictor
-    except AttributeError:
-        model_path = os.path.join(CONFIG.MINDSDB_STORAGE_PATH, hmd['name'], 'lightwood_data')
-        predictor = Predictor(load_from_path=model_path)
-
-    for group, icp in icps.items():
-        if group not in ['__mdb_groups', '__mdb_group_keys']:
-            icp.nc_function.model.model = predictor
-
-    # restore model in normalizer
-    for group, icp in icps.items():
-        if group not in ['__mdb_groups', '__mdb_group_keys']:
-            if icp.nc_function.normalizer is not None:
-                icp.nc_function.normalizer.model = icp.nc_function.model.model
-
-
 class ConformalRegressorAdapter(RegressorAdapter):
     def __init__(self, model, fit_params=None):
         super(ConformalRegressorAdapter, self).__init__(model, fit_params)
