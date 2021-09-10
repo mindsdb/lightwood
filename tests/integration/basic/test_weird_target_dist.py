62e0ee21227be6cf7def99e12b8a3c67438aeccc
George3d6 <george@cerebralab.com>
2021-09-10 18:10:59 +0300

    fix: fixed bug where nonconformist failed if a new cateogorical target appeared in the validationdata

--- tests/integration/basic/test_weird_target_dist.py
+++ tests/integration/basic/test_weird_target_dist.py
@@ -5,9 +5,10 @@ from lightwood.api.types import ProblemDefinition
 
 
 class TestBasic(unittest.TestCase):
-    def test_0_predict_file_flow(self):
+    def test_0_unkown_cateogires_in_test(self):
         from lightwood.api.high_level import predictor_from_problem
 
+        # The target will be cateogircal and there will be a bunch of values in all datasets (train/dev/validation) that were not present in the others
         df = pd.DataFrame({
             'target': [1 for _ in range(500)] + [f'{i}cat' for i in range(100)],
             'y': [i for i in range(600)]
