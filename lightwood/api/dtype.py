eb519c9617b14e19de62598cb03a785d142318f5
Natasha Seelam <nseelam1@gmail.com>
2021-09-13 16:53:23 -0400

    docs: Adds clarifying comments to dtype. Changes order of the vars but no other edits.

--- lightwood/api/dtype.py
+++ lightwood/api/dtype.py
@@ -1,17 +1,33 @@
 class dtype():
-    binary = 'binary'
+    """
+    Definitions of all data types currently supported.
+    """
+    # Numerical type data
     integer = 'integer'
     float = 'float'
-    date = 'date'
-    datetime = 'datetime'
+
+    # Categorical type data
+    binary = 'binary'
     categorical = 'categorical'
     tags = 'tags'
+
+    # Dates and Times (time-series)
+    date = 'date'
+    datetime = 'datetime'
+
+    # Text
+    short_text = 'short_text'
+    rich_text = 'rich_text'
+
+    # Complex Data types
     image = 'image'
     audio = 'audio'
     video = 'video'
+
+    # Series/Sequences
     array = 'array'
-    short_text = 'short_text'
-    rich_text = 'rich_text'
+    
+    # Misc (Unk/NaNs)
     empty = 'empty'
     invalid = 'invalid'
     quantity = 'quantity'
