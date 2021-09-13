c5cec192afbd4cc5e6352c5e2364331d43fb5fef
George3d6 <george@cerebralab.com>
2021-09-13 18:59:09 +0300

    feat: escpaing weird column names, i.e. those containig  and ", tried to do the same for { and }, but apparently not needed\ntest: added test for column names containing , ", { and }

--- tests/integration/basic/test_boston_housing.py
+++ tests/integration/basic/test_boston_housing.py
@@ -10,7 +10,10 @@ class TestBasic(unittest.TestCase):
         from lightwood.api.high_level import predictor_from_problem
 
         df = pd.read_csv('tests/data/boston.csv')[:500]
+        # Mess with the names to also test if lightwood can deal /w weird names
         df = df.rename(columns={df.columns[1]: f'\'{df.columns[1]}\''})
+        df = df.rename(columns={df.columns[2]: f'\'{df.columns[2]}}}'})
+        df = df.rename(columns={df.columns[3]: f'{{{df.columns[3]}\"'})
         target = 'MEDV'
 
         predictor = predictor_from_problem(df, ProblemDefinition.from_dict({'target': target, 'time_aim': 200}))
