4454f474e66f50c4f8cedfccc992882f9e5ad7f3
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2021-09-14 17:46:55 -0300

    refactor: rename folds to subsets

--- lightwood/data/splitter.py
+++ lightwood/data/splitter.py
@@ -8,41 +8,41 @@ from lightwood.api.types import TimeseriesSettings
 
 def splitter(data: pd.DataFrame, k: int, tss: TimeseriesSettings) -> List[pd.DataFrame]:
     """
-    Splits a dataframe into k equally-sized folds.
+    Splits a dataframe into k equally-sized subsets.
     """
     if not tss.is_timeseries:
         # shuffle
         data = data.sample(frac=1).reset_index(drop=True)
 
         # split
-        folds = np.array_split(data, k)
+        subsets = np.array_split(data, k)
 
     else:
         if not tss.group_by:
-            folds = np.array_split(data, k)
+            subsets = np.array_split(data, k)
         else:
             gcols = tss.group_by
-            folds = grouped_ts_splitter(data, k, gcols)
+            subsets = grouped_ts_splitter(data, k, gcols)
 
-    return folds
+    return subsets
 
 
 def grouped_ts_splitter(data: pd.DataFrame, k: int, gcols: List[str]):
     """
     Splitter for grouped time series tasks, where there is a set of `gcols` columns by which data is grouped.
-    Each group yields a different time series, and the splitter generates `k` folds from `data`,
+    Each group yields a different time series, and the splitter generates `k` subsets from `data`,
     with equally-sized sub-series for each group.
     """
     all_group_combinations = list(product(*[data[gcol].unique() for gcol in gcols]))
-    folds = [pd.DataFrame() for _ in range(k)]
+    subsets = [pd.DataFrame() for _ in range(k)]
     for group in all_group_combinations:
         subframe = data
         for idx, gcol in enumerate(gcols):
             subframe = subframe[subframe[gcol] == group[idx]]
 
-        subfolds = np.array_split(subframe, k)
+        subset = np.array_split(subframe, k)
 
         for i in range(k):
-            folds[i] = pd.concat([folds[i], subfolds[i]])
+            subsets[i] = pd.concat([subsets[i], subset[i]])
 
-    return folds
+    return subsets
