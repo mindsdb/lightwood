ee9e78bbbb9b4cbf54c5f1ce7c7b34ae15e4a134
George3d6 <george@cerebralab.com>
2021-09-15 16:56:20 +0300

    fixed by merge

--- tests/integration/basic/test_airline.py
+++ tests/integration/basic/test_airline.py
@@ -1,6 +1,6 @@
 import unittest
 import pandas as pd
-from sklearn.metrics import r2_score
+from sklearn.metrics import accuracy_score
 
 from lightwood.api.types import ProblemDefinition
 
@@ -9,7 +9,7 @@ class TestBasic(unittest.TestCase):
     def test_0_predict_file_flow(self):
         from lightwood.api.high_level import predictor_from_problem
 
-        df = pd.read_csv('tests/data/airline_sentiment.csv')[:50]
+        df = pd.read_csv('tests/data/airline_sentiment.csv')[:500]
         target = 'airline_sentiment'
 
         predictor = predictor_from_problem(df, ProblemDefinition.from_dict({'target': target, 'time_aim': 80}))
@@ -17,6 +17,6 @@ class TestBasic(unittest.TestCase):
         predictions = predictor.predict(df)
 
         # sanity checks
-        self.assertTrue(r2_score(df[target], predictions['prediction']) > 0.8)
+        self.assertTrue(accuracy_score(df[target], predictions['prediction']) > 0.8)
         self.assertTrue(all([0 <= p <= 1 for p in predictions['confidence']]))
         self.assertTrue(all([p['lower'] <= p['prediction'] <= p['upper'] for _, p in predictions.iterrows()]))
