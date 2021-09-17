ff6200a6c799827efac0e874ed133219b22bb57c
George3d6 <george@cerebralab.com>
2021-09-17 16:48:13 +0300

    better test docs

--- tests/integration/basic/test_weird_target_dist.py
+++ tests/integration/basic/test_weird_target_dist.py
@@ -5,6 +5,16 @@ from lightwood.api.types import ProblemDefinition
 
 class TestBasic(unittest.TestCase):
     def test_0_unkown_cateogires_in_test(self):
+        """This test is meant to check if the expaliner can handle a  validation set\
+            with values that it has never seen as part of the training set.
+            In order to achieve this, we generate a dataset with a target that contains 600\
+            values, out of which 100 are distinct. It's basically certain at least 1 of these\
+            distinct value will be in train and at least one in validate (more likely 90/10 split)
+            So if training and predicting goes well, this validates that explainer works with\
+            new values in the validation set *and* with values in train that never occur in validation.
+            Note: This doesn't check how good the confidence is, it'll likely be very bad, we just care\
+            that it doesn't crash.
+        """
         from lightwood.api.high_level import predictor_from_problem
 
         # The target will be cateogircal and there will be a bunch of values
@@ -15,6 +25,6 @@ class TestBasic(unittest.TestCase):
         })
         target = 'target'
 
-        predictor = predictor_from_problem(df, ProblemDefinition.from_dict({'target': target, 'time_aim': 200}))
+        predictor = predictor_from_problem(df, ProblemDefinition.from_dict({'target': target, 'time_aim': 60}))
         predictor.learn(df)
         predictor.predict(df)
