b369ca2fa56100f12233c3e7e1fee91e38508846
George3d6 <george@cerebralab.com>
2021-09-18 17:04:12 +0300

    fix: removed array encoder  from ts encoder section

--- lightwood/encoder/time_series/__init__.py
+++ lightwood/encoder/time_series/__init__.py
@@ -1,5 +1,3 @@
 from lightwood.encoder.time_series.rnn import TimeSeriesEncoder
-from lightwood.encoder.array.array import ArrayEncoder
 
-
-__all__ = ['TimeSeriesEncoder', 'ArrayEncoder']
+__all__ = ['TimeSeriesEncoder']
