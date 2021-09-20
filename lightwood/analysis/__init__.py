b75da405b584600e80f13a4a0b9f63608c5f8445
Patricio Cerda Mardini <pcerdam@live.com>
2021-09-20 17:52:41 -0300

    refactor: renamed model_analyzer to analyze

--- lightwood/analysis/__init__.py
+++ lightwood/analysis/__init__.py
@@ -1,4 +1,4 @@
-from lightwood.analysis.model_analyzer import model_analyzer
+from lightwood.analysis.analyze import model_analyzer
 from lightwood.analysis.explain import explain
 
 __all__ = ['model_analyzer', 'explain']
