602924893e464e051ddf082e2bc280b690ec2e89
George3d6 <george@cerebralab.com>
2021-09-22 19:58:48 +0300

    fix: from double to single quotes\ndocs: added comments to high_level

--- lightwood/api/types.py
+++ lightwood/api/types.py
@@ -123,15 +123,12 @@ class TypeInformation:
     """
     For a dataset, provides information on columns types, how they're used, and any other potential identifiers.
 
-    TypeInformation is generated within ``data.infer_types``, where small samples of each column are evaluated in a \
-    custom framework to understand what kind of data type the model is. The user may override data types, but it is \
-    recommended to do so within a JSON-AI config file.
+    TypeInformation is generated within ``data.infer_types``, where small samples of each column are evaluated in a custom framework to understand what kind of data type the model is. The user may override data types, but it is recommended to do so within a JSON-AI config file.
 
     :param dtypes: For each column's name, the associated data type inferred.
     :param additional_info: Any possible sub-categories or additional descriptive information.
-    :param identifiers: Columns within the dataset highly suspected of being identifiers or IDs. These do not contain\
-     informatic value, therefore will be ignored in subsequent training/analysis procedures unless manually indicated.
-    """
+    :param identifiers: Columns within the dataset highly suspected of being identifiers or IDs. These do not contain informatic value, therefore will be ignored in subsequent training/analysis procedures unless manually indicated.
+    """ # noqa
 
     dtypes: Dict[str, str]
     additional_info: Dict[str, object]
