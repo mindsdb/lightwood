0a2775275100f105e14d4a12c96fee16e0b53f8d
george <george@cerebralab.com>
2021-10-04 21:47:02 +0100

    fix: dependency passing still not working

--- lightwood/encoder/time_series/rnn.py
+++ lightwood/encoder/time_series/rnn.py
@@ -156,7 +156,8 @@ class TimeSeriesEncoder(BaseEncoder):
         :param batch_size
         """
         priming_data = pd.concat([train_priming_data, dev_priming_data])
-        priming_data = priming_data.values
+        priming_data = list(priming_data.values)
+        
         if self._prepared:
             raise Exception('You can only call "prepare" once for a given encoder.')
         else:
