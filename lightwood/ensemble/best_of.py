d07f63139f2bd7dbda0f3ac5735651fecd211f76
george <george@cerebralab.com>
2021-10-07 14:49:38 +0100

    fix: handling nan and inf and None and other weird accuracy score values in the BestOf ensemble

--- lightwood/ensemble/best_of.py
+++ lightwood/ensemble/best_of.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 
 from lightwood.helpers.log import log
+from lightwood.helpers.numeric import can_be_nan_numeric
 from lightwood.mixer.base import BaseMixer
 from lightwood.ensemble.base import BaseEnsemble
 from lightwood.data.encoded_ds import EncodedDs
@@ -27,6 +28,12 @@ class BestOf(BaseEnsemble):
                 ts_analysis=ts_analysis
             )
             avg_score = np.mean(list(score_dict.values()))
+            log.info(f'Mixer: {type(mixer).__name__} got accuracy: {avg_score}')
+
+            if can_be_nan_numeric(avg_score):
+                avg_score = -pow(2, 63)
+                log.warning(f'Change the accuracy of mixer {type(mixer).__name__} to valid value: {avg_score}')
+            
             score_list.append(avg_score)
 
         self.indexes_by_accuracy = list(reversed(np.array(score_list).argsort()))
