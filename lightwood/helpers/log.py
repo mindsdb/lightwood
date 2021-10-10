05f4a066eaaa8bedcd6302d47ae96fe9a8009bb8
Andrea Cognolato <andrecogno@hotmail.it>
2021-10-10 13:24:34 +0300

    Use LIGHTWOOD_LOG environment variable to set logging level

--- lightwood/helpers/log.py
+++ lightwood/helpers/log.py
@@ -6,7 +6,8 @@ def initialize_log():
     pid = os.getpid()
     logging.basicConfig()
     log = logging.getLogger(f'lightwood-{pid}')
-    log.setLevel(logging.DEBUG)
+    log_level = os.environ.get('LIGHTWOOD_LOG', 'DEBUG')
+    log.setLevel(log_level)
     return log
 
 
