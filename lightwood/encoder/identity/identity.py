af2fbeb734514203f6f75fc5b065ce96775a09bd
LyndonFan <lyndon0808@gmail.com>
2021-10-13 00:09:26 +0100

    add doc string and types

--- lightwood/encoder/identity/identity.py
+++ lightwood/encoder/identity/identity.py
@@ -4,6 +4,20 @@ from lightwood.encoder.base import BaseEncoder
 
 
 class IdentityEncoder(BaseEncoder):
+    """
+    The identity encoder which does not change the data during encoding.
+
+    Due to the restrictions of torch.Tensor, the encoding function only accepts types:
+
+    - (nested) lists of numbers
+    - np.ndarray of numbers, or
+    - torch.Tensor
+
+    Nonetypes are not supported.
+
+    The decode function takes in a torch.Tensor and converts it to a list of numbers.
+
+    """  # noqa
 
     def __init__(self, is_target=False) -> None:
         super().__init__(is_target)
@@ -12,10 +26,10 @@ class IdentityEncoder(BaseEncoder):
     def prepare(self, priming_data) -> None:
         self._prepared = True
 
-    def encode(self, column_data) -> torch.Tensor:
+    def encode(self, column_data: Union[torch.Tensor, np.ndarray, List[object]]) -> torch.Tensor:
         if type(column_data) == torch.Tensor:
             return column_data
         return torch.Tensor(column_data)
 
-    def decode(self, encoded_data) -> List[object]:
+    def decode(self, encoded_data: torch.Tensor) -> List[object]:
         return encoded_data.tolist()
