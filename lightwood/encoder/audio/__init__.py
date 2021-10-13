4e3943a6e3e21dd16926bd2f45ec1bb68ce0e368
Andrea Cognolato <andrecogno@hotmail.it>
2021-10-13 19:19:49 +0300

    Remove AmplitudeTsEncoder

--- lightwood/encoder/audio/__init__.py
+++ lightwood/encoder/audio/__init__.py
@@ -1,5 +1,4 @@
-from lightwood.encoder.audio.amplitude_ts import AmplitudeTsEncoder
 from lightwood.encoder.audio.mfcc import MFCCEncoder
 
 
-__all__ = ['AmplitudeTsEncoder', 'MFCCEncoder']
+__all__ = ['MFCCEncoder']
