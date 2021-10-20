f073a567e3117df240d902d68f8b9dbeddf07b4e
Andrea Cognolato <andrecogno@hotmail.it>
2021-10-20 12:15:32 +0300

    Implement MeanEnsemble

---
+++ tests/unit_tests/ensemble/test_mean_ensemble.py
@@ -0,0 +1,33 @@
+import unittest
+import pandas as pd
+from sklearn.metrics import r2_score
+
+from lightwood.api.high_level import code_from_json_ai, json_ai_from_problem, predictor_from_code
+from lightwood.api.types import ProblemDefinition
+
+
+class TestMeanEnsemble(unittest.TestCase):
+    def test_0_predict_file_flow(self):
+
+        df = pd.read_csv('tests/data/concrete_strength.csv')
+
+        target = 'concrete_strength'
+
+        json_ai = json_ai_from_problem(df, ProblemDefinition.from_dict({
+            'target': target,
+            'time_aim': 80
+        }))
+
+        json_ai.outputs[target].ensemble = {
+            'module': 'MeanEnsemble',
+            "args": {
+                'dtype_dict': '$dtype_dict'
+            }
+        }
+
+        code = code_from_json_ai(json_ai)
+        predictor = predictor_from_code(code)
+        predictor.learn(df)
+        predictions = predictor.predict(df)
+
+        self.assertTrue(r2_score(df[target], predictions['prediction']) > 0.5)
