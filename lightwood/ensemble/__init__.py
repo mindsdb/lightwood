1fac325c62fb985cefe1e8930c06700e0de4e4be
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2021-10-24 14:37:19 +0530

    error fix

--- lightwood/ensemble/__init__.py
+++ lightwood/ensemble/__init__.py
@@ -1,6 +1,7 @@
 from lightwood.ensemble.base import BaseEnsemble
 from lightwood.ensemble.best_of import BestOf
 from lightwood.ensemble.mean_ensemble import MeanEnsemble
+from lightwood.ensemble.weighted_mean_ensemble import WeightedMeanEnsemble
 
 
 __all__ = ['BaseEnsemble', 'BestOf', 'MeanEnsemble', 'WeightedMeanEnsemble']
