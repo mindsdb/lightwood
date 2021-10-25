47ed4ab06b42ebd3f2858b518845e947afaa10ad
Andrea Cognolato <andrecogno@hotmail.it>
2021-10-25 12:15:47 +0300

    Refactor test_shap_analysis.py

--- tests/integration/basic/test_shap_analysis.py
+++ tests/integration/basic/test_shap_analysis.py
@@ -1,15 +1,14 @@
 import unittest
 import pandas as pd
-from sklearn.metrics import accuracy_score
 
 from lightwood.api.high_level import ProblemDefinition, json_ai_from_problem
 from lightwood.api.high_level import code_from_json_ai, predictor_from_code
 
 
 class TestBasic(unittest.TestCase):
-    def test_0_predict_file_flow(self):
+    def test_0_shap_analysis(self):
 
-        df = pd.read_csv('tests/data/hdi.csv')[:500]
+        df = pd.read_csv('tests/data/hdi.csv')
         target = 'Development Index'
 
         pdef = ProblemDefinition.from_dict({'target': target, 'time_aim': 10})
@@ -24,8 +23,7 @@ class TestBasic(unittest.TestCase):
         predictor = predictor_from_code(code)
 
         predictor.learn(df)
-        predictions = predictor.predict(df)
+        predictor.predict(df)
 
-        self.assertTrue(accuracy_score(df[target], predictions['prediction'].astype(int)) > 0.5)
-        self.assertTrue(all([0 <= p <= 1 for p in predictions['confidence']]))
+        assert 'shap_explainer' in predictor.runtime_analyzer
 
