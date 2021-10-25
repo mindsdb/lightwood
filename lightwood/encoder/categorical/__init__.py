4a245f7cdb88764a35ecb86d575c5145ab60219b
Victory Nwani <vickywane@gmail.com>
2021-10-25 21:28:42 +0100

    feat: added prospector code analysis tool

--- lightwood/encoder/categorical/__init__.py
+++
@@ -1,5 +0,0 @@
-from lightwood.encoder.categorical.onehot import OneHotEncoder
-from lightwood.encoder.categorical.multihot import MultiHotEncoder
-from lightwood.encoder.categorical.autoencoder import CategoricalAutoEncoder
-
-__all__ = ['OneHotEncoder', 'MultiHotEncoder', 'CategoricalAutoEncoder']
--- lightwood/ensemble/__init__.py
+++
@@ -1,6 +0,0 @@
-from lightwood.ensemble.base import BaseEnsemble
-from lightwood.ensemble.best_of import BestOf
-from lightwood.ensemble.mean_ensemble import MeanEnsemble
-
-
-__all__ = ['BaseEnsemble', 'BestOf', 'MeanEnsemble']
