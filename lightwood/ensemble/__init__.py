38f5226e3faf324f4bcc6d54210c162cd7305916
Andrea Cognolato <andrecogno@hotmail.it>
2021-10-27 01:16:55 +0300

    Add ModeEnsemble and integration test

--- lightwood/ensemble/__init__.py
+++ lightwood/ensemble/__init__.py
@@ -1,6 +1,6 @@
 from lightwood.ensemble.base import BaseEnsemble
 from lightwood.ensemble.best_of import BestOf
 from lightwood.ensemble.mean_ensemble import MeanEnsemble
+from lightwood.ensemble.mode_ensemble import ModeEnsemble
 
-
-__all__ = ['BaseEnsemble', 'BestOf', 'MeanEnsemble']
+__all__ = ['BaseEnsemble', 'BestOf', 'MeanEnsemble', "ModeEnsemble"]
