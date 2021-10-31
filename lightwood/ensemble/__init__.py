aa746f81bd87f8c34202670767290c8ea4ff382a
ma1VAR3 <prajjwalgupta1465@gmail.com>
2021-10-31 18:49:30 +0530

    added median ensemble

--- lightwood/ensemble/__init__.py
+++ lightwood/ensemble/__init__.py
@@ -1,7 +1,8 @@
 from lightwood.ensemble.base import BaseEnsemble
 from lightwood.ensemble.best_of import BestOf
 from lightwood.ensemble.mean_ensemble import MeanEnsemble
+from lightwood.ensemble.median_ensemble import MedianEnsemble
 from lightwood.ensemble.mode_ensemble import ModeEnsemble
 from lightwood.ensemble.weighted_mean_ensemble import WeightedMeanEnsemble
 
-__all__ = ['BaseEnsemble', 'BestOf', 'MeanEnsemble', 'ModeEnsemble', 'WeightedMeanEnsemble']
+__all__ = ['BaseEnsemble', 'BestOf', 'MeanEnsemble','MedianEnsemble', 'ModeEnsemble', 'WeightedMeanEnsemble']
