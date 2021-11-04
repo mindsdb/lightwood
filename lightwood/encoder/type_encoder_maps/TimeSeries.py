d5987e1aa213d08d7817afead01c48818f52ba6b
george <george@cerebralab.com>
2021-11-04 17:50:34 +0000

    removed type encoder maps

--- lightwood/encoder/type_encoder_maps/TimeSeries.py
+++
@@ -1,5 +0,0 @@
-from lightwood.encoder.numeric.ts_array_numeric import TsArrayNumericEncoder
-from lightwood.encoder.time_series.rnn import TimeSeriesEncoder
-
-
-__all__ = ['TsArrayNumericEncoder', 'TimeSeriesEncoder']
