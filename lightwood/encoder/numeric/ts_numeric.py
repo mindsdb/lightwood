0592557bd10369d2f33980a98ba58c61183033a2
Patricio Cerda Mardini <pcerdam@live.com>
2021-11-04 18:36:35 -0500

    fix: uncomment exception raiser

--- lightwood/encoder/numeric/ts_numeric.py
+++ lightwood/encoder/numeric/ts_numeric.py
@@ -53,9 +53,7 @@ class TsNumericEncoder(NumericEncoder):
                     vector[0] = 1 if real < 0 and not self.positive_domain else 0
                     vector[1] = real / mean if mean != 0 else real
                 else:
-                    pass
-                    # This should raise an exception *once* we fix the TsEncoder such that this doesn't get feed `nan`
-                    # raise Exception(f'Can\'t encode target value: {real}')
+                    raise Exception(f'Can\'t encode target value: {real}')
 
             else:
                 vector = [0] * 3
