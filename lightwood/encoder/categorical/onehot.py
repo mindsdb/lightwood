fix: using target weight column in problem definition, lightgbhm handles weighting using datasets, onehotencoder has saner behavior

lightwood/encoder/categorical/onehot.py
@@ -11,7 +11,7 @@ UNCOMMON_TOKEN = 0
 
 class OneHotEncoder(BaseEncoder):
 
-    def __init__(self, is_target=False, target_class_distribution=None, handle_unknown='unknown_token'):
+    def __init__(self, is_target=False, target_weights=None, handle_unknown='unknown_token'):
         super().__init__(is_target)
         self._lang = None
         self.rev_map = {}
@@ -22,8 +22,14 @@ class OneHotEncoder(BaseEncoder):
             self.handle_unknown = handle_unknown
 
         if self.is_target:
-            self.target_class_distribution = target_class_distribution
-            self.index_weights = None
+            if self.handle_unknown != 'unknown_token':
+                raise ValueError(f'One Hot Encoders used for target encoding can only be used with `handle_unknown` \
+                                   set to `unknown_token`. The option: "{self.handle_unknown}" is not supported!')
+
+        self.target_weights = None
+        self.index_weights = None
+        if self.is_target:
+            self.target_weights = target_weights
 
     def prepare(self, priming_data, max_dimensions=20000):
         if self.is_prepared:
@@ -35,7 +41,7 @@ class OneHotEncoder(BaseEncoder):
             self._lang.index2word = {}
             self._lang.word2index = {}
             self._lang.n_words = 0
-        else:  # self.handle_unknown == "unknown_token"
+        elif self.handle_unknown == "unknown_token":
             priming_data = [x if x is not None else UNCOMMON_WORD for x in priming_data]
             self._lang.index2word = {UNCOMMON_TOKEN: UNCOMMON_WORD}
             self._lang.word2index = {UNCOMMON_WORD: UNCOMMON_TOKEN}
@@ -61,17 +67,16 @@ class OneHotEncoder(BaseEncoder):
 
             self._lang.removeWord(word_to_remove)
 
+        # Note: Is target assume that we are operating in "unknown_token" mode
         if self.is_target:
-            self.index_weights = [None] * self._lang.n_words
-            if self.target_class_distribution is not None:
-                self.index_weights[0] = np.mean(list(self.target_class_distribution.values()))
-            else:
-                self.index_weights[0] = 1
+            self.index_weights = [1] * self._lang.n_words
+            if self.target_weights is not None:
+                self.index_weights[0] = np.mean(list(self.target_weights.values()))
+                self.target_weights[UNCOMMON_WORD] = self.index_weights[0]
             for word in set(priming_data):
-                if self.target_class_distribution is not None:
-                    self.index_weights[self._lang.word2index[str(word)]] = 1 / self.target_class_distribution[word]
-                else:
-                    self.index_weights[self._lang.word2index[str(word)]] = 1
+                if self.target_weights is not None:
+                    self.index_weights[self._lang.word2index[str(word)]] = 1 / self.target_weights[word]
+
             self.index_weights = torch.Tensor(self.index_weights)
 
         self.output_size = self._lang.n_words