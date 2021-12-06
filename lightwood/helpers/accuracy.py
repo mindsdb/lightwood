2df50277041b2bc9dd7619b71419ca83683c31fb
george <george@cerebralab.com>
2021-12-05 20:53:35 -0500

    feat: added precision, recall and f1, since they require conversion and some assertions to work with the sklearn format

--- lightwood/helpers/accuracy.py
+++ lightwood/helpers/accuracy.py
@@ -1,4 +1,30 @@
+from typing import Iterable, List
 from sklearn.metrics import r2_score as sk_r2_score
+from sklearn.metrics import f1_score as sk_f1_score
+from sklearn.metrics import recall_score as sk_recall_score
+from sklearn.metrics import precision_score as sk_precision_score
+
+
+def to_binary(y: Iterable) -> List[int]:
+    try:
+        y = [int(x) for x in y]
+        assert len(set(y)) < 3
+        assert 1 in y
+        assert 0 in y
+    except Exception:
+        raise Exception('To use precision, recall or f1 please make sure your target consists only of 1s and 0s')
+
+
+def f1_score(y_true, y_pred) -> float:
+    return sk_f1_score(to_binary(y_true), to_binary(y_pred))
+
+
+def recall_score(y_true, y_pred) -> float:
+    return sk_recall_score(to_binary(y_true), to_binary(y_pred))
+
+
+def precision_score(y_true, y_pred) -> float:
+    return sk_precision_score(to_binary(y_true), to_binary(y_pred))
 
 
 def r2_score(y_true, y_pred) -> float:
