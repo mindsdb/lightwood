7b77839ea9608e09a3434957324647588733ae68
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2021-12-17 12:39:28 -0300

    better error increase fn

--- lightwood/helpers/ts.py
+++ lightwood/helpers/ts.py
@@ -31,13 +31,13 @@ def add_tn_conf_bounds(data: pd.DataFrame, tss_args: TimeseriesSettings):
     """
     Add confidence (and bounds if applicable) to t+n predictions, for n>1
     TODO: active research question: how to guarantee 1-e coverage for t+n, n>1
-    for now, we increase width by the square-rooted decreasing confidence -given its error rate- on each step.
+    For now, conservative approach is to increase width by the confidence times the log of the time step.
     """
     for col in ['confidence', 'lower', 'upper']:
         data[col] = data[col].astype(object)
 
     for idx, row in data.iterrows():
-        error_increase = [row['confidence']] + [(1 + float(1 - np.sqrt(row['confidence']**t)))
+        error_increase = [row['confidence']] + [row['confidence'] * np.log(np.e + t)
                                                 for t in range(1, tss_args.nr_predictions)]
         data['confidence'].iloc[idx] = [row['confidence'] for _ in range(tss_args.nr_predictions)]
 
