6e3d6368a04bf8516e68c1877b05631578cab4ae
andrew <elkin@raketa.im>
2022-03-31 17:32:58 +0300

    fix "too many indices for array: array is 1-dimensional, but 2 were indexed" error

--- lightwood/mixer/neural.py
+++ lightwood/mixer/neural.py
@@ -354,7 +354,9 @@ class Neural(BaseMixer):
             ydf = pd.DataFrame({'prediction': decoded_predictions})
 
             if args.predict_proba and self.supports_proba:
-                raw_predictions = np.array(all_probs).squeeze()
+                # remove 2th dimention
+                raw_predictions = np.array(all_probs)[:, 0, :]
+
                 for idx, label in enumerate(rev_map.values()):
                     ydf[f'__mdb_proba_{label}'] = raw_predictions[:, idx]
 
