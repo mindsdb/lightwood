fix 858, switch to scipy.special.softmax

lightwood/encoder/categorical/binary.py
@@ -1,10 +1,12 @@
+from copy import deepcopy as dc
+from typing import Dict, List, Iterable, Tuple
+
 import torch
 import numpy as np
+from scipy.special import softmax
+
 from lightwood.encoder.base import BaseEncoder
 from lightwood.helpers.constants import _UNCOMMON_WORD
-from copy import deepcopy as dc
-
-from typing import Dict, List, Iterable, Tuple
 
 
 class BinaryEncoder(BaseEncoder):
@@ -157,11 +159,8 @@ class BinaryEncoder(BaseEncoder):
     @staticmethod
     def _norm_vec(vec: List[float]):
         """
-        Given a vector, normalizes so that the sum of elements is 1.
+        Given a vector, normalizes so that the sum of elements is 1, using softmax.
 
         :param vec: Assigned weights for each category
         """  # noqa
-        total = sum(vec)
-        if total == 0:
-            return vec
-        return [i / total for i in vec]
+        return softmax(vec)