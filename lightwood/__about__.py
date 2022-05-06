3ddf8ab0afd44442dd0854c3016fd9f15e7b8ad1
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2022-05-06 14:08:03 +0200

    Update __about__.py
    version bump -> 22.5.1.0

--- lightwood/__about__.py
+++ lightwood/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'lightwood'
 __package_name__ = 'lightwood'
-__version__ = '22.4.4.0'
+__version__ = '22.5.1.0'
 __description__ = "Lightwood is a toolkit for automatic machine learning model building"
 __email__ = "community@mindsdb.com"
 __author__ = 'MindsDB Inc'
