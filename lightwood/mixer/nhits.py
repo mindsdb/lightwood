1f8bb341adc12aeee96c6714d45cb39abb3ad2a9
Patricio Cerda Mardini <pcerdam@live.com>
2022-06-03 19:44:59 +0100

    feat: add pretrained retrieval and usage

--- lightwood/mixer/nhits.py
+++ lightwood/mixer/nhits.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 from hyperopt import hp
 import neuralforecast as nf
+from neuralforecast.models.mqnhits.mqnhits import MQNHITS
+from neuralforecast.models.nhits.nhits import NHITS
 
 from lightwood.helpers.log import log
 from lightwood.mixer.base import BaseMixer
@@ -40,8 +42,8 @@ class NHitsMixer(BaseMixer):
         'n_windows': 32,
         'lr_decay': 0.5,
         'lr_decay_step_size': 2,
-        'max_epochs': None,  # no limit for now
-        'max_steps': None,
+        'max_epochs': 50,
+        'max_steps': 1000,
         'early_stop_patience': 20,
         'eval_freq': 500,
         'batch_normalization': False,
@@ -85,6 +87,16 @@ class NHitsMixer(BaseMixer):
         self.target = target
         self.config = NHitsMixer.default_config.copy()
 
+        # pretraining info
+        self.pretrained = True  # todo: modifiable from JsonAI, plus option to finetune!
+        self.base_url = 'https://nixtla-public.s3.amazonaws.com/transfer/pretrained_models/'
+        self.model_names = {
+           'hourly': 'nhits_m4_hourly.ckpt',  # hourly (non-tiny)
+           'daily': 'nhits_m4_daily.ckpt',   # daily
+           'monthly': 'nhits_m4_monthyl.ckpt',  # monthly
+           'yearly': 'nhits_m4_hourly.ckpt',  # yearly
+        }
+
         self.ts_analysis = ts_analysis
         self.horizon = horizon
         self.grouped_by = ['__default'] if not ts_analysis['tss'].group_by else ts_analysis['tss'].group_by
@@ -119,25 +131,34 @@ class NHitsMixer(BaseMixer):
 
         # train the model
         n_time_out = self.horizon
-        self.model = nf.auto.NHITS(horizon=n_time_out)
-        self.model.space['max_steps'] = hp.choice('max_steps', [5e4])
-        # self.model.space['max_epochs'] = hp.choice('max_epochs', [-1, 10])
-        # 4 & 4 works... theory is both need to be smaller than length of val and/or test idxs... 8&8 fails?
-        self.model.space['n_time_in'] = hp.choice('n_time_in', [self.ts_analysis['tss'].window-1])
-        self.model.space['n_time_out'] = hp.choice('n_time_out', [self.horizon])
-        self.model.space['n_windows'] = hp.choice('n_windows', [1])
-        self.model.fit(Y_df=Y_df,
-                       X_df=None,       # Exogenous variables
-                       S_df=None,       # Static variables
-                       hyperopt_steps=5,
-                       n_ts_val=n_ts_val,
-                       n_ts_test=n_ts_test,
-                       results_dir='./results/autonhits',  # TODO: rm/change this to /tmp/lightwood/autonhits or similar
-                       save_trials=False,
-                       loss_function_val=nf.losses.numpy.mae,
-                       loss_functions_test={'mse': nf.losses.numpy.mae},
-                       return_test_forecast=False,
-                       verbose=False)  # True
+        if self.pretrained:
+            model_name = self.model_names.get(self.ts_analysis['sample_freqs']['__default'], None)
+            model_name = self.model_names['hourly'] if model_name is None else model_name
+            ckpt_url = self.base_url + model_name
+            self.model = MQNHITS.load_from_checkpoint(ckpt_url)  # TODO use this when not pretraining for consistency
+
+            # TODO: if self.finetune: ...
+        else:
+            self.model = nf.auto.NHITS(horizon=n_time_out)
+            self.model.space['max_steps'] = hp.choice('max_steps', [5e4])
+            self.model.space['max_epochs'] = hp.choice('max_epochs', [50])
+            # self.model.space['max_epochs'] = hp.choice('max_epochs', [-1, 10])
+            # 4 & 4 works... theory is both need to be smaller than length of val and/or test idxs... 8&8 fails?
+            self.model.space['n_time_in'] = hp.choice('n_time_in', [self.ts_analysis['tss'].window-1])
+            self.model.space['n_time_out'] = hp.choice('n_time_out', [self.horizon])
+            self.model.space['n_windows'] = hp.choice('n_windows', [1])
+            self.model.fit(Y_df=Y_df,
+                           X_df=None,       # Exogenous variables
+                           S_df=None,       # Static variables
+                           hyperopt_steps=5,
+                           n_ts_val=n_ts_val,
+                           n_ts_test=n_ts_test,
+                           results_dir='./results/autonhits',  # TODO: rm/change this to /tmp/lightwood/autonhits or similar
+                           save_trials=False,
+                           loss_function_val=nf.losses.numpy.mae,
+                           loss_functions_test={'mse': nf.losses.numpy.mae},
+                           return_test_forecast=False,
+                           verbose=True)  # False
 
     def partial_fit(self, train_data: EncodedDs, dev_data: EncodedDs) -> None:
         """
@@ -165,10 +186,10 @@ class NHitsMixer(BaseMixer):
                            dtype=object)
 
         input_df = self._make_initial_df(ds.data_frame)  # TODO make it so that it's horizon worth of data in each row
+        pred_col = 'y' if isinstance(self.model, NHITS) else 'y_50'
         for i in range(input_df.shape[0]):
-            ydf.iloc[i]['prediction'] = self.model.forecast(
-                Y_df=input_df.iloc[i:i + 1]
-            )['y'].tolist()
+            fcst = self.model.forecast(Y_df=input_df.iloc[i:i + 1])
+            ydf.iloc[i]['prediction'] = fcst[pred_col].tolist()[:self.horizon]
 
         return ydf[['prediction']]
 
