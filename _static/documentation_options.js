87e73a51ddf10bb3e74b9d200434c85a29f2a65d
paxcema <pcerdam@live.com>
2022-10-12 21:32:21 +0000

    Rebuilt the docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '22.9.1.0',
+    VERSION: '22.9.1.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
