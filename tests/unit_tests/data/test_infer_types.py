9c7fcd818a2d5c035e68f4689eec6d3f0a03c2c7
Patricio Cerda Mardini <pcerdam@live.com>
2022-10-19 01:11:37 +0300

    remove infer_types test

--- tests/unit_tests/data/test_infer_types.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from lightwood.api.dtype import dtype
-from lightwood.data.infer_types import type_check_date
-
-
-class TestTypeInference(unittest.TestCase):
-    def test_0_type_check_dates(self):
-        self.assertEqual(type_check_date('31/12/2010'), dtype.date)
