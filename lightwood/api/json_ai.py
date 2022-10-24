504f304ed0ec43e7dd5eb70d205281211302a59e
adripo <26493496+adripo@users.noreply.github.com>
2022-10-24 19:35:54 +0200

    fix: import directly from about

--- lightwood/api/json_ai.py
+++ lightwood/api/json_ai.py
@@ -11,7 +11,7 @@ from lightwood.api.types import (
 )
 import inspect
 from lightwood.helpers.log import log
-from lightwood import __version__ as lightwood_version
+from lightwood.__about__ import __version__ as lightwood_version
 
 
 # For custom modules, we create a module loader with necessary imports below
