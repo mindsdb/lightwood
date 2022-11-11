94071f178a41a968399427355669e706c2f4ea40
Patricio Cerda Mardini <pcerdam@live.com>
2022-11-11 19:41:47 +0100

    add mavg test

---
+++ tests/unit_tests/analysis/test_mavg.py
@@ -0,0 +1,13 @@
+import unittest
+# import pandas as pd
+
+from lightwood.analysis import Robbie
+# from lightwood.api.high_level import ProblemDefinition, json_ai_from_problem
+# from lightwood.api.high_level import code_from_json_ai, predictor_from_code
+
+
+class TestRobbie(unittest.TestCase):
+    def test_0_robbie_analysis(self):
+        if Robbie is None:
+            print('Skipping Robbie test when values is empty')
+            return
