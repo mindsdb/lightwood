e63e0c26b4df2e89b8b4de3e5dae06ff60b30162
sebastian tobon <setohe.09@gmail.com>
2022-12-02 14:45:04 -0500

    def explain created

--- lightwood/analysis/helpers/mavg.py
+++ lightwood/analysis/helpers/mavg.py
@@ -1,11 +1,14 @@
+from copy import deepcopy
 from typing import Optional, Tuple, Dict
+from types import SimpleNamespace
+
 import pandas as pd
-# import numpy as np
+import numpy as np
 
 # from sklearn.preprocessing import OrdinalEncoder
 
 from lightwood.analysis.base import BaseAnalysisBlock
-# from lightwood.helpers.log import log
+from lightwood.helpers.log import log
 # from lightwood.helpers.parallelism import get_nr_procs
 
 
@@ -23,4 +26,44 @@ class MAvg(BaseAnalysisBlock):
     def explain(self,
                 row_insights: pd.DataFrame,
                 global_insights: Dict[str, object], **kwargs) -> Tuple[pd.DataFrame, Dict[str, object]]:
+        """
+        df: contains the entire time series to analyze.
+        box_dim: width & height relative to the observed magnitudes in the series (height) and the amount of observations (width). 
+
+        The box is positioned at the end of the time series, and it alerts the user if the series "escapes" or "exits" the box via the y axis at any given point.
+
+        TODO: Automated box_dim param estimation. How??
+        """
+        # TODO: box_dim (User defined)
+        box_dim= (0.1, 0.1)
+
+        log.info('Anomaly detection...')
+        ns = SimpleNamespace(**kwargs)
+        df = deepcopy(ns.data)
+
+        x, y = box_dim
+
+        assert 0 <= x <= 1  # raise Exception in lightwood
+        assert 0 <= y <= 1
+
+        n_points = round(len(df) * x)
+        obs_magnitude = abs(df[ns.target].max() - df[ns.target].min())
+
+        ref_point = df[ns.target].iloc[-1]
+
+        ub = ref_point + (obs_magnitude / 2) * y
+        lb = ref_point - (obs_magnitude / 2) * y
+
+        sub_df = df[ns.target].iloc[-n_points:].values  # numpy array
+
+        flag = False
+
+        lower_filter = np.where(lb <= sub_df, True, False)
+        upper_filter = np.where(sub_df <= ub, True, False)
+
+        if np.all([lower_filter, upper_filter]):
+            flag = True
+
+        global_insights['mavg_flag'] = flag
+
         return row_insights, global_insights
