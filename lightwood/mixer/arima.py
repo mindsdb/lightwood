d18e8bd41888519486eafeda6dd51fa76a08535c
Patricio Cerda Mardini <patricio.mardini@mindsdb.com>
2023-02-03 12:29:39 -0300

    improved interface for AutoETS and AutoARIMA

--- lightwood/mixer/arima.py
+++ lightwood/mixer/arima.py
@@ -1,22 +1,129 @@
-from typing import Dict
+from typing import Dict, Optional
 
 from lightwood.mixer.sktime import SkTime
 
 
 class ARIMAMixer(SkTime):
-    def __init__(self,
-                 stop_after: float,
-                 target: str,
-                 dtype_dict: Dict[str, str],
-                 horizon: int,
-                 ts_analysis: Dict,
-                 model_path: str = 'statsforecast.StatsForecastAutoARIMA',
-                 auto_size: bool = True,
-                 sp: int = None,
-                 use_stl: bool = False
-                 ):
+    def __init__(
+            self,
+            stop_after: float,
+            target: str,
+            dtype_dict: Dict[str, str],
+            horizon: int,
+            ts_analysis: Dict,
+            model_path: str = 'statsforecast.StatsForecastAutoARIMA',
+            auto_size: bool = True,
+            sp: int = None,
+            use_stl: bool = False,
+            start_p: int = 2,
+            d: Optional[int] = None,
+            start_q: int = 2,
+            max_p: int = 5,
+            max_d: int = 2,
+            max_q: int = 5,
+            start_P: int = 1,
+            D: Optional[int] = None,
+            start_Q: int = 1,
+            max_P: int = 2,
+            max_D: int = 1,
+            max_Q: int = 2,
+            max_order: int = 5,
+            seasonal: bool = True,
+            stationary: bool = False,
+            information_criterion: str = 'aic',
+            alpha: float = 0.05,
+            test: str = 'kpss',
+            seasonal_test: str = 'ocsb',
+            stepwise: bool = True,
+            n_jobs: int = 1,
+            start_params: Optional[list] = None,
+            trend: Optional[str] = None,
+            method: str = 'lbfgs',
+            maxiter: int = 50,
+            offset_test_args: Optional[dict] = None,
+            seasonal_test_args: Optional[dict] = None,
+            suppress_warnings: bool = True,
+            error_action: str = 'warn',
+            trace: bool = False,
+            random: bool = False,
+            random_state: Optional[int] = None,
+            n_fits: int = 10,
+            out_of_sample_size: int = 0,
+            scoring: str = 'mse',
+            scoring_args: Optional[dict] = None,
+            with_intercept: bool = True,
+            update_pdq: bool = True,
+            time_varying_regression: bool = False,
+            enforce_stationarity: bool = True,
+            enforce_invertibility: bool = True,
+            simple_differencing: bool = False,
+            measurement_error: bool = False,
+            mle_regression: bool = True,
+            hamilton_representation: bool = False,
+            concentrate_scale: bool = False,
+    ):
+        """
+        Wrapper for SkTime's AutoARIMA interface.
+
+        :param stop_after: time budget in seconds
+        :param target: column containing target time series
+        :param dtype_dict: data types for each dataset column
+        :param horizon: forecast length
+        :param ts_analysis: lightwood-produced stats about input time series
+        :param auto_size: whether to filter out old data points if training split is bigger than a certain threshold (defined by the dataset sampling frequency). Enabled by default to avoid long training times in big datasets.
+        :param use_stl: Whether to use de-trenders and de-seasonalizers fitted in the timeseries analysis phase.
+
+        For the rest of the parameters, please refer to SkTime's documentation.
+        """  # noqa
         hyperparam_search = False
-        model_kwargs = {}
+        model_kwargs = {
+            'start_p': start_p,
+            'd': d,
+            'start_q': start_q,
+            'max_p': max_p,
+            'max_d': max_d,
+            'max_q': max_q,
+            'start_P': start_P,
+            'D': D,
+            'start_Q': start_Q,
+            'max_P': max_P,
+            'max_D': max_D,
+            'max_Q': max_Q,
+            'max_order': max_order,
+            'seasonal': seasonal,
+            'stationary': stationary,
+            'information_criterion': information_criterion,
+            'alpha': alpha,
+            'test': test,
+            'seasonal_test': seasonal_test,
+            'stepwise': stepwise,
+            'n_jobs': n_jobs,
+            'start_params': start_params,
+            'trend': trend,
+            'method': method,
+            'maxiter': maxiter,
+            'offset_test_args': offset_test_args,
+            'seasonal_test_args': seasonal_test_args,
+            'suppress_warnings': suppress_warnings,
+            'error_action': error_action,
+            'trace': trace,
+            'random': random,
+            'random_state': random_state,
+            'n_fits': n_fits,
+            'out_of_sample_size': out_of_sample_size,
+            'scoring': scoring,
+            'scoring_args': scoring_args,
+            'with_intercept': with_intercept,
+            'update_pdq': update_pdq,
+            'time_varying_regression': time_varying_regression,
+            'enforce_stationarity': enforce_stationarity,
+            'enforce_invertibility': enforce_invertibility,
+            'simple_differencing': simple_differencing,
+            'measurement_error': measurement_error,
+            'mle_regression': mle_regression,
+            'hamilton_representation': hamilton_representation,
+            'concentrate_scale': concentrate_scale,
+        }
         super().__init__(stop_after, target, dtype_dict, horizon, ts_analysis,
                          model_path, model_kwargs, auto_size, sp, hyperparam_search, use_stl)
         self.name = 'AutoARIMA'
--- lightwood/mixer/ets.py
+++ lightwood/mixer/ets.py
@@ -1,22 +1,76 @@
-from typing import Dict
+from typing import Dict, Optional
 
 from lightwood.mixer.sktime import SkTime
 
 
 class ETSMixer(SkTime):
-    def __init__(self,
-                 stop_after: float,
-                 target: str,
-                 dtype_dict: Dict[str, str],
-                 horizon: int,
-                 ts_analysis: Dict,
-                 model_path: str = 'ets.AutoETS',
-                 auto_size: bool = True,
-                 sp: int = None,
-                 use_stl: bool = True
-                 ):
+    def __init__(
+            self,
+            stop_after: float,
+            target: str,
+            dtype_dict: Dict[str, str],
+            horizon: int,
+            ts_analysis: Dict,
+            model_path: str = 'ets.AutoETS',
+            auto_size: bool = True,
+            sp: int = None,
+            use_stl: bool = True,
+            error: str = 'add',
+            trend: Optional[str] = None,
+            damped_trend: bool = False,
+            seasonal: Optional[str] = None,
+            initialization_method: str = 'estimated',
+            initial_level: Optional[float] = None,
+            initial_trend: Optional[float] = None,
+            initial_seasonal: Optional[list] = None,
+            bounds: Optional[dict] = None,
+            start_params: Optional[list] = None,
+            maxiter: int = 1000,
+            auto: bool = False,
+            information_criterion: str = 'aic',
+            allow_multiplicative_trend: bool = False,
+            restrict: bool = True,
+            additive_only: bool = False,
+            ignore_inf_ic: bool = True,
+            n_jobs: Optional[int] = None,
+            random_state: Optional[int] = None
+    ):
+        """
+        Wrapper for SkTime's AutoETS interface.
+
+        :param stop_after: time budget in seconds
+        :param target: column containing target time series
+        :param dtype_dict: data types for each dataset column
+        :param horizon: forecast length
+        :param ts_analysis: lightwood-produced stats about input time series
+        :param auto_size: whether to filter out old data points if training split is bigger than a certain threshold (defined by the dataset sampling frequency). Enabled by default to avoid long training times in big datasets.
+        :param use_stl: Whether to use de-trenders and de-seasonalizers fitted in the timeseries analysis phase.
+
+        For the rest of the parameters, please refer to SkTime's documentation.
+        """  # noqa
+
         hyperparam_search = False
-        model_kwargs = {}
+        model_kwargs = {
+            'error': error,
+            'trend': trend,
+            'damped_trend': damped_trend,
+            'seasonal': seasonal,
+            'initialization_method': initialization_method,
+            'initial_level': initial_level,
+            'initial_trend': initial_trend,
+            'initial_seasonal': initial_seasonal,
+            'bounds': bounds,
+            'start_params': start_params,
+            'maxiter': maxiter,
+            'auto': auto,
+            'information_criterion': information_criterion,
+            'allow_multiplicative_trend': allow_multiplicative_trend,
+            'restrict': restrict,
+            'additive_only': additive_only,
+            'ignore_inf_ic': ignore_inf_ic,
+            'n_jobs': n_jobs,
+            'random_state': random_state
+        }
         super().__init__(stop_after, target, dtype_dict, horizon, ts_analysis,
                          model_path, model_kwargs, auto_size, sp, hyperparam_search, use_stl)
         self.name = 'AutoETS'
