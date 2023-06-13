4e28856baa772073878e80cf753614467e3f5639
paxcema <pcerdam@live.com>
2023-06-13 00:19:14 +0000

    Rebuilt the docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '23.5.1.1',
+    VERSION: '23.6.2.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
