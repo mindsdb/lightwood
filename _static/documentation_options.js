8fd7d299539a06a14ccb93435e37c56bb7a6714a
StpMax <stpmax@yandex.ru>
2025-03-25 09:01:30 +0000

    Rebuilt the docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '24.12.3.0',
+    VERSION: '25.3.3.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
